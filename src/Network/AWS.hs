f87c91f5af32b8d9497c908015c4875189d4f0c8
Brendan Hay <brendan.g.hay@gmail.com>
2013-08-06 08:06:00 +0200

    Attempting to make things a little more readable

--- src/Network/AWS.hs
+++ src/Network/AWS.hs
@@ -52,13 +52,16 @@ withRegion reg aws = awsAuth <$> ask >>=
 -- FIXME: XHT -> Aeson
 send :: AWSRequest b a => a -> AWS ByteString
 send payload = do
-    SignedRequest{..} <- sign =<< request payload
-    liftIO . bracket (establishConnection rqUrl) closeConnection $ \conn -> do
+    rq <- sign =<< request payload
+    liftIO . bracket open closeConnection (deliver rq)
+  where
+    open = establishConnection rqUrl
+
+    deliver SignedRequest{..} conn = do
         sendRequest conn rqRequest $ maybe emptyBody inputStreamBody rqStream
         print rqRequest
         receiveResponse conn $ \_ inp ->
             fromMaybe "" <$> Streams.read inp
-
 --
 -- Internal
 --

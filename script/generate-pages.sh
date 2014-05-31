be8b723de3d24fe9ff5d23953623c0d5534d8467
Brendan Hay <brendan.g.hay@gmail.com>
2014-05-31 19:18:29 +0200

    Updating script

--- script/generate-pages.sh
+++ script/generate-pages.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-exec standalone-haddock \
- -o $HOME/Projects/aws-doc \
+set -e
+
+doc=$HOME/Projects/aws-doc
+
+standalone-haddock \
+ -o $doc \
  --package-db $HOME/Projects/aws/.cabal-sandbox/x86_64-osx-ghc-7.8.2-packages.conf.d \
  amazonka-types
+
+cd $doc
+
+git checkout gh-pages || true
+git add .
+git commit -am "Updating"
+git push origin gh-pages

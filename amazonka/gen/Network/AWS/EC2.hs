87d06a8a8a69fc4a044605e00ae5aac9f3e761f7
Brendan Hay <brendan.g.hay@gmail.com>
2014-07-26 13:44:49 +0200

    Adding EC2 versions

---
+++ amazonka/gen/Network/AWS/EC2.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Module      : Network.AWS.EC2
+-- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+module Network.AWS.EC2
+    ( module Network.AWS.EC2.V2013_10_15
+    ) where
+
+import Network.AWS.EC2.V2013_10_15
---
+++ amazonka/gen/Network/AWS/EC2/V2014_05_01.hs
@@ -0,0 +1,325 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Module      : Network.AWS.EC2.V2014_05_01
+-- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+module Network.AWS.EC2.V2014_05_01
+    ( module Network.AWS.EC2.V2014_05_01.AcceptVpcPeeringConnection
+    , module Network.AWS.EC2.V2014_05_01.AllocateAddress
+    , module Network.AWS.EC2.V2014_05_01.AssignPrivateIpAddresses
+    , module Network.AWS.EC2.V2014_05_01.AssociateAddress
+    , module Network.AWS.EC2.V2014_05_01.AssociateDhcpOptions
+    , module Network.AWS.EC2.V2014_05_01.AssociateRouteTable
+    , module Network.AWS.EC2.V2014_05_01.AttachInternetGateway
+    , module Network.AWS.EC2.V2014_05_01.AttachNetworkInterface
+    , module Network.AWS.EC2.V2014_05_01.AttachVolume
+    , module Network.AWS.EC2.V2014_05_01.AttachVpnGateway
+    , module Network.AWS.EC2.V2014_05_01.AuthorizeSecurityGroupEgress
+    , module Network.AWS.EC2.V2014_05_01.AuthorizeSecurityGroupIngress
+    , module Network.AWS.EC2.V2014_05_01.BundleInstance
+    , module Network.AWS.EC2.V2014_05_01.CancelBundleTask
+    , module Network.AWS.EC2.V2014_05_01.CancelConversionTask
+    , module Network.AWS.EC2.V2014_05_01.CancelExportTask
+    , module Network.AWS.EC2.V2014_05_01.CancelReservedInstancesListing
+    , module Network.AWS.EC2.V2014_05_01.CancelSpotInstanceRequests
+    , module Network.AWS.EC2.V2014_05_01.ConfirmProductInstance
+    , module Network.AWS.EC2.V2014_05_01.CopyImage
+    , module Network.AWS.EC2.V2014_05_01.CopySnapshot
+    , module Network.AWS.EC2.V2014_05_01.CreateCustomerGateway
+    , module Network.AWS.EC2.V2014_05_01.CreateDhcpOptions
+    , module Network.AWS.EC2.V2014_05_01.CreateImage
+    , module Network.AWS.EC2.V2014_05_01.CreateInstanceExportTask
+    , module Network.AWS.EC2.V2014_05_01.CreateInternetGateway
+    , module Network.AWS.EC2.V2014_05_01.CreateKeyPair
+    , module Network.AWS.EC2.V2014_05_01.CreateNetworkAcl
+    , module Network.AWS.EC2.V2014_05_01.CreateNetworkAclEntry
+    , module Network.AWS.EC2.V2014_05_01.CreateNetworkInterface
+    , module Network.AWS.EC2.V2014_05_01.CreatePlacementGroup
+    , module Network.AWS.EC2.V2014_05_01.CreateReservedInstancesListing
+    , module Network.AWS.EC2.V2014_05_01.CreateRoute
+    , module Network.AWS.EC2.V2014_05_01.CreateRouteTable
+    , module Network.AWS.EC2.V2014_05_01.CreateSecurityGroup
+    , module Network.AWS.EC2.V2014_05_01.CreateSnapshot
+    , module Network.AWS.EC2.V2014_05_01.CreateSpotDatafeedSubscription
+    , module Network.AWS.EC2.V2014_05_01.CreateSubnet
+    , module Network.AWS.EC2.V2014_05_01.CreateTags
+    , module Network.AWS.EC2.V2014_05_01.CreateVolume
+    , module Network.AWS.EC2.V2014_05_01.CreateVpc
+    , module Network.AWS.EC2.V2014_05_01.CreateVpcPeeringConnection
+    , module Network.AWS.EC2.V2014_05_01.CreateVpnConnection
+    , module Network.AWS.EC2.V2014_05_01.CreateVpnConnectionRoute
+    , module Network.AWS.EC2.V2014_05_01.CreateVpnGateway
+    , module Network.AWS.EC2.V2014_05_01.DeleteCustomerGateway
+    , module Network.AWS.EC2.V2014_05_01.DeleteDhcpOptions
+    , module Network.AWS.EC2.V2014_05_01.DeleteInternetGateway
+    , module Network.AWS.EC2.V2014_05_01.DeleteKeyPair
+    , module Network.AWS.EC2.V2014_05_01.DeleteNetworkAcl
+    , module Network.AWS.EC2.V2014_05_01.DeleteNetworkAclEntry
+    , module Network.AWS.EC2.V2014_05_01.DeleteNetworkInterface
+    , module Network.AWS.EC2.V2014_05_01.DeletePlacementGroup
+    , module Network.AWS.EC2.V2014_05_01.DeleteRoute
+    , module Network.AWS.EC2.V2014_05_01.DeleteRouteTable
+    , module Network.AWS.EC2.V2014_05_01.DeleteSecurityGroup
+    , module Network.AWS.EC2.V2014_05_01.DeleteSnapshot
+    , module Network.AWS.EC2.V2014_05_01.DeleteSpotDatafeedSubscription
+    , module Network.AWS.EC2.V2014_05_01.DeleteSubnet
+    , module Network.AWS.EC2.V2014_05_01.DeleteTags
+    , module Network.AWS.EC2.V2014_05_01.DeleteVolume
+    , module Network.AWS.EC2.V2014_05_01.DeleteVpc
+    , module Network.AWS.EC2.V2014_05_01.DeleteVpcPeeringConnection
+    , module Network.AWS.EC2.V2014_05_01.DeleteVpnConnection
+    , module Network.AWS.EC2.V2014_05_01.DeleteVpnConnectionRoute
+    , module Network.AWS.EC2.V2014_05_01.DeleteVpnGateway
+    , module Network.AWS.EC2.V2014_05_01.DeregisterImage
+    , module Network.AWS.EC2.V2014_05_01.DescribeAccountAttributes
+    , module Network.AWS.EC2.V2014_05_01.DescribeAddresses
+    , module Network.AWS.EC2.V2014_05_01.DescribeAvailabilityZones
+    , module Network.AWS.EC2.V2014_05_01.DescribeBundleTasks
+    , module Network.AWS.EC2.V2014_05_01.DescribeConversionTasks
+    , module Network.AWS.EC2.V2014_05_01.DescribeCustomerGateways
+    , module Network.AWS.EC2.V2014_05_01.DescribeDhcpOptions
+    , module Network.AWS.EC2.V2014_05_01.DescribeExportTasks
+    , module Network.AWS.EC2.V2014_05_01.DescribeImageAttribute
+    , module Network.AWS.EC2.V2014_05_01.DescribeImages
+    , module Network.AWS.EC2.V2014_05_01.DescribeInstanceAttribute
+    , module Network.AWS.EC2.V2014_05_01.DescribeInstanceStatus
+    , module Network.AWS.EC2.V2014_05_01.DescribeInstances
+    , module Network.AWS.EC2.V2014_05_01.DescribeInternetGateways
+    , module Network.AWS.EC2.V2014_05_01.DescribeKeyPairs
+    , module Network.AWS.EC2.V2014_05_01.DescribeNetworkAcls
+    , module Network.AWS.EC2.V2014_05_01.DescribeNetworkInterfaceAttribute
+    , module Network.AWS.EC2.V2014_05_01.DescribeNetworkInterfaces
+    , module Network.AWS.EC2.V2014_05_01.DescribePlacementGroups
+    , module Network.AWS.EC2.V2014_05_01.DescribeRegions
+    , module Network.AWS.EC2.V2014_05_01.DescribeReservedInstances
+    , module Network.AWS.EC2.V2014_05_01.DescribeReservedInstancesListings
+    , module Network.AWS.EC2.V2014_05_01.DescribeReservedInstancesModifications
+    , module Network.AWS.EC2.V2014_05_01.DescribeReservedInstancesOfferings
+    , module Network.AWS.EC2.V2014_05_01.DescribeRouteTables
+    , module Network.AWS.EC2.V2014_05_01.DescribeSecurityGroups
+    , module Network.AWS.EC2.V2014_05_01.DescribeSnapshotAttribute
+    , module Network.AWS.EC2.V2014_05_01.DescribeSnapshots
+    , module Network.AWS.EC2.V2014_05_01.DescribeSpotDatafeedSubscription
+    , module Network.AWS.EC2.V2014_05_01.DescribeSpotInstanceRequests
+    , module Network.AWS.EC2.V2014_05_01.DescribeSpotPriceHistory
+    , module Network.AWS.EC2.V2014_05_01.DescribeSubnets
+    , module Network.AWS.EC2.V2014_05_01.DescribeTags
+    , module Network.AWS.EC2.V2014_05_01.DescribeVolumeAttribute
+    , module Network.AWS.EC2.V2014_05_01.DescribeVolumeStatus
+    , module Network.AWS.EC2.V2014_05_01.DescribeVolumes
+    , module Network.AWS.EC2.V2014_05_01.DescribeVpcAttribute
+    , module Network.AWS.EC2.V2014_05_01.DescribeVpcPeeringConnections
+    , module Network.AWS.EC2.V2014_05_01.DescribeVpcs
+    , module Network.AWS.EC2.V2014_05_01.DescribeVpnConnections
+    , module Network.AWS.EC2.V2014_05_01.DescribeVpnGateways
+    , module Network.AWS.EC2.V2014_05_01.DetachInternetGateway
+    , module Network.AWS.EC2.V2014_05_01.DetachNetworkInterface
+    , module Network.AWS.EC2.V2014_05_01.DetachVolume
+    , module Network.AWS.EC2.V2014_05_01.DetachVpnGateway
+    , module Network.AWS.EC2.V2014_05_01.DisableVgwRoutePropagation
+    , module Network.AWS.EC2.V2014_05_01.DisassociateAddress
+    , module Network.AWS.EC2.V2014_05_01.DisassociateRouteTable
+    , module Network.AWS.EC2.V2014_05_01.EnableVgwRoutePropagation
+    , module Network.AWS.EC2.V2014_05_01.EnableVolumeIO
+    , module Network.AWS.EC2.V2014_05_01.GetConsoleOutput
+    , module Network.AWS.EC2.V2014_05_01.GetPasswordData
+    , module Network.AWS.EC2.V2014_05_01.ImportInstance
+    , module Network.AWS.EC2.V2014_05_01.ImportKeyPair
+    , module Network.AWS.EC2.V2014_05_01.ImportVolume
+    , module Network.AWS.EC2.V2014_05_01.Lenses
+    , module Network.AWS.EC2.V2014_05_01.ModifyImageAttribute
+    , module Network.AWS.EC2.V2014_05_01.ModifyInstanceAttribute
+    , module Network.AWS.EC2.V2014_05_01.ModifyNetworkInterfaceAttribute
+    , module Network.AWS.EC2.V2014_05_01.ModifyReservedInstances
+    , module Network.AWS.EC2.V2014_05_01.ModifySnapshotAttribute
+    , module Network.AWS.EC2.V2014_05_01.ModifyVolumeAttribute
+    , module Network.AWS.EC2.V2014_05_01.ModifyVpcAttribute
+    , module Network.AWS.EC2.V2014_05_01.MonitorInstances
+    , module Network.AWS.EC2.V2014_05_01.PurchaseReservedInstancesOffering
+    , module Network.AWS.EC2.V2014_05_01.RebootInstances
+    , module Network.AWS.EC2.V2014_05_01.RegisterImage
+    , module Network.AWS.EC2.V2014_05_01.RejectVpcPeeringConnection
+    , module Network.AWS.EC2.V2014_05_01.ReleaseAddress
+    , module Network.AWS.EC2.V2014_05_01.ReplaceNetworkAclAssociation
+    , module Network.AWS.EC2.V2014_05_01.ReplaceNetworkAclEntry
+    , module Network.AWS.EC2.V2014_05_01.ReplaceRoute
+    , module Network.AWS.EC2.V2014_05_01.ReplaceRouteTableAssociation
+    , module Network.AWS.EC2.V2014_05_01.ReportInstanceStatus
+    , module Network.AWS.EC2.V2014_05_01.RequestSpotInstances
+    , module Network.AWS.EC2.V2014_05_01.ResetImageAttribute
+    , module Network.AWS.EC2.V2014_05_01.ResetInstanceAttribute
+    , module Network.AWS.EC2.V2014_05_01.ResetNetworkInterfaceAttribute
+    , module Network.AWS.EC2.V2014_05_01.ResetSnapshotAttribute
+    , module Network.AWS.EC2.V2014_05_01.RevokeSecurityGroupEgress
+    , module Network.AWS.EC2.V2014_05_01.RevokeSecurityGroupIngress
+    , module Network.AWS.EC2.V2014_05_01.RunInstances
+    , module Network.AWS.EC2.V2014_05_01.StartInstances
+    , module Network.AWS.EC2.V2014_05_01.StopInstances
+    , module Network.AWS.EC2.V2014_05_01.TerminateInstances
+    , module Network.AWS.EC2.V2014_05_01.Types
+    , module Network.AWS.EC2.V2014_05_01.UnassignPrivateIpAddresses
+    , module Network.AWS.EC2.V2014_05_01.UnmonitorInstances
+    ) where
+
+import Network.AWS.EC2.V2014_05_01.AcceptVpcPeeringConnection
+import Network.AWS.EC2.V2014_05_01.AllocateAddress
+import Network.AWS.EC2.V2014_05_01.AssignPrivateIpAddresses
+import Network.AWS.EC2.V2014_05_01.AssociateAddress
+import Network.AWS.EC2.V2014_05_01.AssociateDhcpOptions
+import Network.AWS.EC2.V2014_05_01.AssociateRouteTable
+import Network.AWS.EC2.V2014_05_01.AttachInternetGateway
+import Network.AWS.EC2.V2014_05_01.AttachNetworkInterface
+import Network.AWS.EC2.V2014_05_01.AttachVolume
+import Network.AWS.EC2.V2014_05_01.AttachVpnGateway
+import Network.AWS.EC2.V2014_05_01.AuthorizeSecurityGroupEgress
+import Network.AWS.EC2.V2014_05_01.AuthorizeSecurityGroupIngress
+import Network.AWS.EC2.V2014_05_01.BundleInstance
+import Network.AWS.EC2.V2014_05_01.CancelBundleTask
+import Network.AWS.EC2.V2014_05_01.CancelConversionTask
+import Network.AWS.EC2.V2014_05_01.CancelExportTask
+import Network.AWS.EC2.V2014_05_01.CancelReservedInstancesListing
+import Network.AWS.EC2.V2014_05_01.CancelSpotInstanceRequests
+import Network.AWS.EC2.V2014_05_01.ConfirmProductInstance
+import Network.AWS.EC2.V2014_05_01.CopyImage
+import Network.AWS.EC2.V2014_05_01.CopySnapshot
+import Network.AWS.EC2.V2014_05_01.CreateCustomerGateway
+import Network.AWS.EC2.V2014_05_01.CreateDhcpOptions
+import Network.AWS.EC2.V2014_05_01.CreateImage
+import Network.AWS.EC2.V2014_05_01.CreateInstanceExportTask
+import Network.AWS.EC2.V2014_05_01.CreateInternetGateway
+import Network.AWS.EC2.V2014_05_01.CreateKeyPair
+import Network.AWS.EC2.V2014_05_01.CreateNetworkAcl
+import Network.AWS.EC2.V2014_05_01.CreateNetworkAclEntry
+import Network.AWS.EC2.V2014_05_01.CreateNetworkInterface
+import Network.AWS.EC2.V2014_05_01.CreatePlacementGroup
+import Network.AWS.EC2.V2014_05_01.CreateReservedInstancesListing
+import Network.AWS.EC2.V2014_05_01.CreateRoute
+import Network.AWS.EC2.V2014_05_01.CreateRouteTable
+import Network.AWS.EC2.V2014_05_01.CreateSecurityGroup
+import Network.AWS.EC2.V2014_05_01.CreateSnapshot
+import Network.AWS.EC2.V2014_05_01.CreateSpotDatafeedSubscription
+import Network.AWS.EC2.V2014_05_01.CreateSubnet
+import Network.AWS.EC2.V2014_05_01.CreateTags
+import Network.AWS.EC2.V2014_05_01.CreateVolume
+import Network.AWS.EC2.V2014_05_01.CreateVpc
+import Network.AWS.EC2.V2014_05_01.CreateVpcPeeringConnection
+import Network.AWS.EC2.V2014_05_01.CreateVpnConnection
+import Network.AWS.EC2.V2014_05_01.CreateVpnConnectionRoute
+import Network.AWS.EC2.V2014_05_01.CreateVpnGateway
+import Network.AWS.EC2.V2014_05_01.DeleteCustomerGateway
+import Network.AWS.EC2.V2014_05_01.DeleteDhcpOptions
+import Network.AWS.EC2.V2014_05_01.DeleteInternetGateway
+import Network.AWS.EC2.V2014_05_01.DeleteKeyPair
+import Network.AWS.EC2.V2014_05_01.DeleteNetworkAcl
+import Network.AWS.EC2.V2014_05_01.DeleteNetworkAclEntry
+import Network.AWS.EC2.V2014_05_01.DeleteNetworkInterface
+import Network.AWS.EC2.V2014_05_01.DeletePlacementGroup
+import Network.AWS.EC2.V2014_05_01.DeleteRoute
+import Network.AWS.EC2.V2014_05_01.DeleteRouteTable
+import Network.AWS.EC2.V2014_05_01.DeleteSecurityGroup
+import Network.AWS.EC2.V2014_05_01.DeleteSnapshot
+import Network.AWS.EC2.V2014_05_01.DeleteSpotDatafeedSubscription
+import Network.AWS.EC2.V2014_05_01.DeleteSubnet
+import Network.AWS.EC2.V2014_05_01.DeleteTags
+import Network.AWS.EC2.V2014_05_01.DeleteVolume
+import Network.AWS.EC2.V2014_05_01.DeleteVpc
+import Network.AWS.EC2.V2014_05_01.DeleteVpcPeeringConnection
+import Network.AWS.EC2.V2014_05_01.DeleteVpnConnection
+import Network.AWS.EC2.V2014_05_01.DeleteVpnConnectionRoute
+import Network.AWS.EC2.V2014_05_01.DeleteVpnGateway
+import Network.AWS.EC2.V2014_05_01.DeregisterImage
+import Network.AWS.EC2.V2014_05_01.DescribeAccountAttributes
+import Network.AWS.EC2.V2014_05_01.DescribeAddresses
+import Network.AWS.EC2.V2014_05_01.DescribeAvailabilityZones
+import Network.AWS.EC2.V2014_05_01.DescribeBundleTasks
+import Network.AWS.EC2.V2014_05_01.DescribeConversionTasks
+import Network.AWS.EC2.V2014_05_01.DescribeCustomerGateways
+import Network.AWS.EC2.V2014_05_01.DescribeDhcpOptions
+import Network.AWS.EC2.V2014_05_01.DescribeExportTasks
+import Network.AWS.EC2.V2014_05_01.DescribeImageAttribute
+import Network.AWS.EC2.V2014_05_01.DescribeImages
+import Network.AWS.EC2.V2014_05_01.DescribeInstanceAttribute
+import Network.AWS.EC2.V2014_05_01.DescribeInstanceStatus
+import Network.AWS.EC2.V2014_05_01.DescribeInstances
+import Network.AWS.EC2.V2014_05_01.DescribeInternetGateways
+import Network.AWS.EC2.V2014_05_01.DescribeKeyPairs
+import Network.AWS.EC2.V2014_05_01.DescribeNetworkAcls
+import Network.AWS.EC2.V2014_05_01.DescribeNetworkInterfaceAttribute
+import Network.AWS.EC2.V2014_05_01.DescribeNetworkInterfaces
+import Network.AWS.EC2.V2014_05_01.DescribePlacementGroups
+import Network.AWS.EC2.V2014_05_01.DescribeRegions
+import Network.AWS.EC2.V2014_05_01.DescribeReservedInstances
+import Network.AWS.EC2.V2014_05_01.DescribeReservedInstancesListings
+import Network.AWS.EC2.V2014_05_01.DescribeReservedInstancesModifications
+import Network.AWS.EC2.V2014_05_01.DescribeReservedInstancesOfferings
+import Network.AWS.EC2.V2014_05_01.DescribeRouteTables
+import Network.AWS.EC2.V2014_05_01.DescribeSecurityGroups
+import Network.AWS.EC2.V2014_05_01.DescribeSnapshotAttribute
+import Network.AWS.EC2.V2014_05_01.DescribeSnapshots
+import Network.AWS.EC2.V2014_05_01.DescribeSpotDatafeedSubscription
+import Network.AWS.EC2.V2014_05_01.DescribeSpotInstanceRequests
+import Network.AWS.EC2.V2014_05_01.DescribeSpotPriceHistory
+import Network.AWS.EC2.V2014_05_01.DescribeSubnets
+import Network.AWS.EC2.V2014_05_01.DescribeTags
+import Network.AWS.EC2.V2014_05_01.DescribeVolumeAttribute
+import Network.AWS.EC2.V2014_05_01.DescribeVolumeStatus
+import Network.AWS.EC2.V2014_05_01.DescribeVolumes
+import Network.AWS.EC2.V2014_05_01.DescribeVpcAttribute
+import Network.AWS.EC2.V2014_05_01.DescribeVpcPeeringConnections
+import Network.AWS.EC2.V2014_05_01.DescribeVpcs
+import Network.AWS.EC2.V2014_05_01.DescribeVpnConnections
+import Network.AWS.EC2.V2014_05_01.DescribeVpnGateways
+import Network.AWS.EC2.V2014_05_01.DetachInternetGateway
+import Network.AWS.EC2.V2014_05_01.DetachNetworkInterface
+import Network.AWS.EC2.V2014_05_01.DetachVolume
+import Network.AWS.EC2.V2014_05_01.DetachVpnGateway
+import Network.AWS.EC2.V2014_05_01.DisableVgwRoutePropagation
+import Network.AWS.EC2.V2014_05_01.DisassociateAddress
+import Network.AWS.EC2.V2014_05_01.DisassociateRouteTable
+import Network.AWS.EC2.V2014_05_01.EnableVgwRoutePropagation
+import Network.AWS.EC2.V2014_05_01.EnableVolumeIO
+import Network.AWS.EC2.V2014_05_01.GetConsoleOutput
+import Network.AWS.EC2.V2014_05_01.GetPasswordData
+import Network.AWS.EC2.V2014_05_01.ImportInstance
+import Network.AWS.EC2.V2014_05_01.ImportKeyPair
+import Network.AWS.EC2.V2014_05_01.ImportVolume
+import Network.AWS.EC2.V2014_05_01.Lenses
+import Network.AWS.EC2.V2014_05_01.ModifyImageAttribute
+import Network.AWS.EC2.V2014_05_01.ModifyInstanceAttribute
+import Network.AWS.EC2.V2014_05_01.ModifyNetworkInterfaceAttribute
+import Network.AWS.EC2.V2014_05_01.ModifyReservedInstances
+import Network.AWS.EC2.V2014_05_01.ModifySnapshotAttribute
+import Network.AWS.EC2.V2014_05_01.ModifyVolumeAttribute
+import Network.AWS.EC2.V2014_05_01.ModifyVpcAttribute
+import Network.AWS.EC2.V2014_05_01.MonitorInstances
+import Network.AWS.EC2.V2014_05_01.PurchaseReservedInstancesOffering
+import Network.AWS.EC2.V2014_05_01.RebootInstances
+import Network.AWS.EC2.V2014_05_01.RegisterImage
+import Network.AWS.EC2.V2014_05_01.RejectVpcPeeringConnection
+import Network.AWS.EC2.V2014_05_01.ReleaseAddress
+import Network.AWS.EC2.V2014_05_01.ReplaceNetworkAclAssociation
+import Network.AWS.EC2.V2014_05_01.ReplaceNetworkAclEntry
+import Network.AWS.EC2.V2014_05_01.ReplaceRoute
+import Network.AWS.EC2.V2014_05_01.ReplaceRouteTableAssociation
+import Network.AWS.EC2.V2014_05_01.ReportInstanceStatus
+import Network.AWS.EC2.V2014_05_01.RequestSpotInstances
+import Network.AWS.EC2.V2014_05_01.ResetImageAttribute
+import Network.AWS.EC2.V2014_05_01.ResetInstanceAttribute
+import Network.AWS.EC2.V2014_05_01.ResetNetworkInterfaceAttribute
+import Network.AWS.EC2.V2014_05_01.ResetSnapshotAttribute
+import Network.AWS.EC2.V2014_05_01.RevokeSecurityGroupEgress
+import Network.AWS.EC2.V2014_05_01.RevokeSecurityGroupIngress
+import Network.AWS.EC2.V2014_05_01.RunInstances
+import Network.AWS.EC2.V2014_05_01.StartInstances
+import Network.AWS.EC2.V2014_05_01.StopInstances
+import Network.AWS.EC2.V2014_05_01.TerminateInstances
+import Network.AWS.EC2.V2014_05_01.Types
+import Network.AWS.EC2.V2014_05_01.UnassignPrivateIpAddresses
+import Network.AWS.EC2.V2014_05_01.UnmonitorInstances

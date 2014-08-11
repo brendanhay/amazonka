6ca8425fcbf8c327c808706a58acfee296cf460a
Brendan Hay <brendan.g.hay@gmail.com>
2014-08-11 18:01:31 +0200

    Adding X-AMZ-Security-Token to presigned version 4 query requests

--- amazonka/src/Network/AWS/Signing/V4.hs
+++ amazonka/src/Network/AWS/Signing/V4.hs
@@ -76,13 +76,14 @@ instance AWSPresigner V4 where
       where
         out = finalise Nothing qry s a r rq l t
 
-        -- FIXME: add security token query param
         qry cs sh =
               pair "X-AMZ-Algorithm" algorithm
             . pair "X-AMZ-Credential" cs
             . pair "X-AMZ-Date" (LocaleTime l t :: ISO8601)
             . pair "X-AMZ-Expires" x
             . pair "X-AMZ-SignedHeaders" sh
+            . pair "X-AMZ-SignedHeaders" sh
+            . pair "X-AMZ-Security-Token" (toBS (_authToken a))
 
         auth = mappend "&X-AMZ-Signature=" . _mSignature
 

b6e51dfe8b8d6ab1c456faf9e36d99e8d63d0da7
Brendan Hay <brendan.g.hay@gmail.com>
2014-09-11 18:00:59 +0200

    Adding convenience functions for constructing request bodies

--- core/src/Network/AWS/Internal/Data/Body.hs
+++ core/src/Network/AWS/Internal/Data/Body.hs
@@ -13,17 +13,27 @@
 
 module Network.AWS.Internal.Data.Body where
 
+import           Control.Applicative
+import           Control.Monad.IO.Class
+import           Control.Monad.Morph
+import           Control.Monad.Trans.Resource
 import           Crypto.Hash
+import qualified Crypto.Hash.Conduit            as Conduit
 import           Data.Aeson
 import           Data.ByteString                (ByteString)
+import qualified Data.ByteString                as BS
 import qualified Data.ByteString.Lazy           as LBS
 import qualified Data.ByteString.Lazy.Char8     as LBS8
 import           Data.Conduit
+import qualified Data.Conduit.Binary            as Conduit
+import           Data.IORef
+import           Data.Int
 import           Data.Monoid
 import           Data.String
 import qualified Data.Text                      as Text
 import           Network.AWS.Internal.Data.Text
-import           Network.HTTP.Conduit
+import           Network.HTTP.Client
+import           System.IO
 
 data RsBody where
     RsBody :: Monad m => ResumableSource m ByteString -> RsBody
@@ -64,17 +74,33 @@ instance ToBody ByteString where
 instance ToBody Value where
     toBody = toBody . encode
 
--- sourceBody :: Digest SHA256 -> Int64 -> Source IO ByteString -> RqBody
--- sourceBody h n = RqBody h . RequestBodyStream n . sourcePopper
+sourceBody :: Digest SHA256 -> Int64 -> Source IO ByteString -> RqBody
+sourceBody h n = RqBody h . RequestBodyStream n . sourcePopper
 
--- sourceHandle :: Digest SHA256 -> Int64 -> Handle -> RqBody
--- sourceHandle h n = sourceBody h n . Conduit.sourceHandle
+sourceHandle :: Digest SHA256 -> Int64 -> Handle -> RqBody
+sourceHandle h n = sourceBody h n . Conduit.sourceHandle
 
--- sourceFile :: Digest SHA256 -> Int64 -> FilePath -> RqBody
--- sourceFile h n = sourceBody h n . hoist runResourceT . Conduit.sourceFile
+sourceFile :: Digest SHA256 -> Int64 -> FilePath -> RqBody
+sourceFile h n = sourceBody h n . hoist runResourceT . Conduit.sourceFile
 
--- sourceFileIO :: MonadIO m => FilePath ->  RqBody
--- sourceFileIO f = sourceFile
---     <$> runResourceT (Conduit.sourceFile f $$ Conduit.sinkHash)
---     <*> (fromIntegral <$> withBinaryFile f ReadMode hFileSize)
---     <*> pure f
+sourceFileIO :: MonadIO m => FilePath -> m RqBody
+sourceFileIO f = liftIO $ sourceFile
+    <$> runResourceT (Conduit.sourceFile f $$ Conduit.sinkHash)
+    <*> (fromIntegral <$> withBinaryFile f ReadMode hFileSize)
+    <*> pure f
+
+sourcePopper :: Source IO ByteString -> GivesPopper ()
+sourcePopper src f = do
+    (rsrc0, ()) <- src $$+ return ()
+    irsrc <- newIORef rsrc0
+    let popper :: IO ByteString
+        popper = do
+            rsrc <- readIORef irsrc
+            (rsrc', mres) <- rsrc $$++ await
+            writeIORef irsrc rsrc'
+            case mres of
+                Nothing -> return mempty
+                Just bs
+                    | BS.null bs -> popper
+                    | otherwise  -> return bs
+    f popper

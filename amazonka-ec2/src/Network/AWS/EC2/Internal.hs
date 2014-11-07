ffd53761bc8809f29d002e82956329afd2aa1c35
Brendan Hay <brendan.g.hay@gmail.com>
2014-11-07 17:35:44 +0100

    Adding Internal EC2 module

---
+++ amazonka-ec2/src/Network/AWS/EC2/Internal.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+-- Module      : Network.AWS.EC2.Internal
+-- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+module Network.AWS.EC2.Internal where
+
+import Data.String
+import GHC.Generics
+import Network.AWS.Prelude

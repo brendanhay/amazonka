bce29a46910316b493c60608e9f58b34639e7ba7
Brendan Hay <brendan.g.hay@gmail.com>
2014-11-23 10:11:03 +0100

    Limiting imports

--- examples/src/Examples/SQS.hs
+++ examples/src/Examples/SQS.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes        #-}
 
 -- Module      : Examples.SQS
 -- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
@@ -13,8 +12,8 @@
 
 module Examples.SQS where
 
-import           Control.Applicative
-import           Control.Lens
+import           Control.Applicative     ((<$>))
+import           Control.Lens            (set)
 import           Control.Monad.Trans.AWS
 import qualified Data.Text.IO            as Text
 import qualified Network.AWS.SQS         as SQS

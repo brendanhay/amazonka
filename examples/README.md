094a806f388e39484ab764330c28f050b86c5a8c
Brendan Hay <brendan.g.hay@gmail.com>
2014-11-23 10:07:35 +0100

    Adding the beginnings of a collection of examples

---
+++ examples/README.md
@@ -0,0 +1,19 @@
+# Amazonka Examples
+
+* [Description](#description)
+* [Contribute](#contribute)
+* [Licence](#licence)
+
+## Description
+
+Basic usage examples for the various `amazonka-*` libraries.
+
+
+## Contribute
+
+For any problems, comments, or feedback please create an issue [here on GitHub](https://github.com/brendanhay/amazonka/issues).
+
+
+## Licence
+
+`amazonka-core` is released under the [Mozilla Public License Version 2.0](http://www.mozilla.org/MPL/).
---
+++ examples/src/Examples/SQS.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
+
+-- Module      : Examples.SQS
+-- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+module Examples.SQS where
+
+import           Control.Applicative
+import           Control.Lens
+import           Control.Monad.Trans.AWS
+import qualified Data.Text.IO            as Text
+import qualified Network.AWS.SQS         as SQS
+
+discoverEnv :: IO Env
+discoverEnv = set envLogger (Debug Text.putStrLn) <$> getEnv Ireland Discover
+
+sendMessage :: IO ()
+sendMessage = do
+    env <- discoverEnv
+    r   <- runAWST env $ do
+        send $ SQS.sendMessage "$MY_SQS_URL_HERE" "Testing!"
+    print r

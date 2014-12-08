fec82d67a9c37d00257fd03d66d0cf00199d1733
Brendan Hay <brendan.g.hay@gmail.com>
2014-12-08 10:26:01 +0100

    Lens documentation

--- amazonka/src/Network/AWS/Internal/Env.hs
+++ amazonka/src/Network/AWS/Internal/Env.hs
@@ -33,32 +33,32 @@ data Env = Env
     , _envAuth        :: Auth
     }
 
--- |
+-- | The current region.
 envRegion :: Lens' Env Region
 envRegion = lens _envRegion (\s a -> s { _envRegion = a })
 {-# INLINE envRegion #-}
 
--- |
+-- | The function used to output log messages.
 envLogger :: Lens' Env Logger
 envLogger = lens _envLogger (\s a -> s { _envLogger = a })
 {-# INLINE envLogger #-}
 
--- |
+-- | The function used to determine if an 'HttpException' should be retried.
 envRetryCheck :: Lens' Env (Int -> HttpException -> IO Bool)
 envRetryCheck = lens _envRetryCheck (\s a -> s { _envRetryCheck = a })
 {-# INLINE envRetryCheck #-}
 
--- |
+-- | The 'RetryPolicy' used to determine backoff/on and retry delay/growth.
 envRetryPolicy :: Lens' Env (Maybe RetryPolicy)
 envRetryPolicy = lens _envRetryPolicy (\s a -> s { _envRetryPolicy = a })
 {-# INLINE envRetryPolicy #-}
 
--- |
+-- | The 'Manager' used to create and manage open HTTP connections.
 envManager :: Lens' Env Manager
 envManager = lens _envManager (\s a -> s { _envManager = a })
 {-# INLINE envManager #-}
 
--- |
+-- | The credentials used to sign requests for authentication with AWS.
 envAuth :: Lens' Env Auth
 envAuth = lens _envAuth (\s a -> s { _envAuth = a })
 {-# INLINE envAuth #-}

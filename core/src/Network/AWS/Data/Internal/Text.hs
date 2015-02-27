f4272edb31296a2128dc338c8953f5c03bc9883a
Brendan Hay <brendan.g.hay@gmail.com>
2015-02-27 09:55:49 +0100

    Accounting for an optional leading sign when parsing non-natural numbers
    Fixes #83.

--- core/src/Network/AWS/Data/Internal/Text.hs
+++ core/src/Network/AWS/Data/Internal/Text.hs
@@ -59,11 +59,11 @@ class FromText a where
 
 instance FromText Text       where parser = AText.takeText
 instance FromText ByteString where parser = Text.encodeUtf8 <$> AText.takeText
-instance FromText Int        where parser = AText.decimal
-instance FromText Integer    where parser = AText.decimal
-instance FromText Scientific where parser = AText.scientific
+instance FromText Int        where parser = AText.signed AText.decimal
+instance FromText Integer    where parser = AText.signed AText.decimal
+instance FromText Scientific where parser = AText.signed AText.scientific
 instance FromText Natural    where parser = AText.decimal
-instance FromText Double     where parser = AText.rational
+instance FromText Double     where parser = AText.signed AText.rational
 
 instance FromText Bool where
     parser = matchCI "false" False <|> matchCI "true" True

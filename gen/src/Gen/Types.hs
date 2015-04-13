7c23b9650684b098c276a394364577ef1ff4a614
Brendan Hay <brendan.g.hay@gmail.com>
2015-04-13 21:02:51 +0200

    Bumping static versions to 0.3.4

--- gen/src/Gen/Types.hs
+++ gen/src/Gen/Types.hs
@@ -60,7 +60,7 @@ currentLibraryVersion :: Version
 currentLibraryVersion = initial
     & major .~ 0
     & minor .~ 3
-    & patch .~ 3
+    & patch .~ 4
 
 class ToFilePath a where
     toFilePath :: a -> FilePath

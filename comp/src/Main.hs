ec80555a24fcf251ed63530457264f816d89826e
Brendan Hay <brendan.g.hay@gmail.com>
2015-04-17 10:58:32 +0200

    Stripping compiler dependencies

--- comp/src/Main.hs
+++ comp/src/Main.hs
@@ -123,119 +123,25 @@ validate o = flip execStateT o $ do
     canon :: MonadIO m => FilePath -> m FilePath
     canon = liftIO . FS.canonicalizePath
 
-main :: IO ()
-main = runScript $ do
-    o <- scriptIO $ customExecParser (prefs showHelpOnError) options
-        >>= validate
+-- main :: IO ()
+-- main = runScript $ do
+--     o <- scriptIO $ customExecParser (prefs showHelpOnError) options
+--         >>= validate
 
-    t <- templates (o ^. optTemplates)
+--     t <- templates (o ^. optTemplates)
 
-    forM_ (o ^. optModels) $ \d -> do
-        s <- model d (o ^. optOverrides)
+--     forM_ (o ^. optModels) $ \d -> do
+--         s <- model d (o ^. optOverrides)
 
-        -- mapM_ (\p -> AST.pretty p >>= scriptIO . LText.putStrLn . (<> "\n"))
-        --     . mapMaybe (uncurry AST.transform)
-        --     $ Map.toList (s ^. svcShapes)
+--         -- mapM_ (\p -> AST.pretty p >>= scriptIO . LText.putStrLn . (<> "\n"))
+--         --     . mapMaybe (uncurry AST.transform)
+--         --     $ Map.toList (s ^. svcShapes)
 
-        r <- Library.tree (o ^. optOutput) t (o ^. optVersion) s
-            >>= writeTree "Write Library"
+--         r <- Library.tree (o ^. optOutput) t (o ^. optVersion) s
+--             >>= writeTree "Write Library"
 
-        copyDirectory (o ^. optAssets) (Library.root r)
+--         copyDirectory (o ^. optAssets) (Library.root r)
 
-        say "Completed" (s ^. svcName)
+--         say "Completed" (s ^. svcName)
 
-    say "Completed" (Text.pack $ show (length (o ^. optModels)) ++ " models.")
-
-model :: FilePath -> FilePath -> Script (Service (Derived Shape) (Untyped Ref))
-model d o = do
-    say "Load Model" (encode d)
-
-    v <- version d
-    x <- dec override'
-    y <- JSON.merge <$> sequence
-        [ pure x
-        , dec (normal v)
-        , JSON.def "waiters"    $ dec (waiters v)
-        , JSON.def "pagination" $ dec (pagers  v)
-        ]
-    either (throwError . msg) Override.service (JSON.parse y)
-  where
-    normal  = path "normal.json"
-    waiters = path "waiters.json"
-    pagers  = path "paginators.json"
-
-    override' = o </> basename d <.> "json"
-    path e v  = d </> v <.> e
-
-    dec = fileContents >=> hoistEither . eitherDecode
-
-    msg = mappend ("Error in " ++ show d ++ ": ")
-
-version :: FilePath -> Script FilePath
-version d = do
-    fs <- scriptIO (FS.listDirectory d)
-    vs <- NonEmpty.nonEmpty (basename `map` sortBy (flip compare) fs)
-        ?? ("Failed to find versioned model in " ++ show d)
-    say "Using Version" (msg vs)
-    return (NonEmpty.head vs)
-  where
-    msg (v :| vs) = mconcat
-        [ encode (d </> v)
-        , " from ["
-        , Text.intercalate ", " . nub $ map encode vs
-        , "]"
-        ]
-
-templates :: FilePath -> Script (Templates Protocol)
-templates d = do
-    f <- Templates
-        <$> load "cabal.ede"
-        <*> pure (error "service.ede")
-        <*> pure (error "waiters.ede")
-        <*> load "readme.ede"
-        <*> pure (error "example-cabal.ede")
-        <*> pure (error "example-makefile.ede")
-
-        -- <*> load "service.ede"
-        -- <*> load "waiters.ede"
-        -- <*> load "readme.ede"
-        -- <*> load "example-cabal.ede"
-        -- <*> load "example-makefile.ede"
-
-    -- x <- (,)
-    --     <$> load "types-xml.ede"
-    --     <*> load "operation-xml.ede"
-
-    j <- (,)
-        <$> load "types.ede"
-        <*> load "operation.ede"
-
-    -- q <- (,)
-    --     <$> load "types-query.ede"
-    --     <*> load "operation-query.ede"
-
-    let x = j
-        q = j
-
-    return $! f $ \case
-        JSON     -> j
-        RestJSON -> j
-        XML      -> x
-        RestXML  -> x
-        Query    -> q
-        EC2      -> q
-  where
-    load p = do
-        let f = encode (d </> p)
-        say "Parse Template" f
-        scriptIO (EDE.eitherParseFile (Text.unpack f)) >>= hoistEither
-
- -- * Calculate a stable 'amazonka-*' library name per service
- -- * Ensure every shape has documentation (even if 'documentation is missing' string)
- -- * Ensure every ref for a shape has documentation, same as above
- -- * Ensure timestamp format for metadata is set
- -- * Parse/create the Endpoints model
- -- * Use a post-processing step (similar to stylish-haskell) which wraps all comment lines to 80 chars
-
--- * Tag the different 'Doc' usages with their parent type and use Data.Default instances
---   to select the default documentation for that type using 'fromMaybe def'.
+--     say "Completed" (Text.pack $ show (length (o ^. optModels)) ++ " models.")

83a5c2dfd31d39e22a65fba7a935a9b8e46d129a
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-01 18:13:27 +0200

    Adding failing EC2 fixture

--- amazonka-ec2/test/Test/AWS/EC2.hs
+++ amazonka-ec2/test/Test/AWS/EC2.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 -- Module      : Test.AWS.EC2
 -- Copyright   : (c) 2013-2015 Brendan Hay <brendan.g.hay@gmail.com>
@@ -15,8 +16,11 @@ module Test.AWS.EC2
     , fixtures
     ) where
 
+import           Data.Time
 import           Network.AWS.EC2
+import           Network.AWS.Prelude
 import           Test.AWS.Gen.EC2
+import           Test.AWS.TH
 import           Test.Tasty
 
 tests :: [TestTree]
@@ -25,5 +29,29 @@ tests = []
 fixtures :: [TestTree]
 fixtures =
     [ testDescribeInstancesResponse $
-        describeInstancesResponse 200
+        describeInstancesResponse 200 & dirReservations .~
+            [ reservation "r-1a2b3c4d" "123456789012"
+                & resGroups .~
+                    [ groupIdentifier
+                        & giGroupId   ?~ "sg-1a2b3c4d"
+                        & giGroupName ?~ "my-security-group"
+                    ]
+
+                & resInstances .~
+                    [ instance'
+                        "i-1a2b3c4d"
+                        "ami-1a2b3c4d"
+                        0
+                        C1Medium
+                        $(mkTime "2014-03-18T21:47:02+0000")
+                        (placement & plaAvailabilityZone ?~ "us-east-1a"
+                                   & plaTenancy          ?~ Default)
+                        (monitoring & monState ?~ MSDisabled)
+                        X8664
+                        EBS
+                        HVM
+                        Xen
+                        (instanceState ISNRunning 16)
+                    ]
+            ]
     ]
---
+++ test/src/Test/AWS/TH.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans  #-}
+
+-- Module      : Test.AWS.TH
+-- Copyright   : (c) 2013-2015 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+module Test.AWS.TH where
+
+import           Data.Time
+import           Language.Haskell.TH
+import           Language.Haskell.TH.Syntax
+import           Network.AWS.Data.Text
+import           Network.AWS.Data.Time
+import           Network.AWS.Prelude
+
+mkTime :: Text -> Q Exp
+mkTime x =
+    case fromText x :: Either String ISO8601 of
+        Left  e -> error (show e)
+        Right t -> [|view _Time t|]
+
+instance Lift (Time a) where
+    lift (Time x) = AppE (ConE (mkName "Time")) <$> lift x
+
+instance Lift UTCTime where
+    lift (UTCTime x y) = do
+        x' <- lift x
+        y' <- lift y
+        return $ AppE (AppE (ConE (mkName "UTCTime")) x') y'
+
+instance Lift DiffTime where
+    lift x = [|toEnum $(lift (fromEnum x))|]
+
+instance Lift Day where
+    lift x = [|toEnum $(lift (fromEnum x))|]

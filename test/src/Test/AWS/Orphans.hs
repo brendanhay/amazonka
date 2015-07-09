2e2efe87a2409fdcd89118e36faf8892c28367f9
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-09 15:41:52 +0200

    Adding unordered-containers dependency to test frameworks

--- test/src/Test/AWS/Orphans.hs
+++ test/src/Test/AWS/Orphans.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 -- |
@@ -10,9 +12,19 @@
 --
 module Test.AWS.Orphans where
 
+import           Data.Bifunctor
+import           Data.HashMap.Strict            (HashMap)
+import qualified Data.HashMap.Strict            as Map
+import           Data.Monoid
 import qualified Data.Text                      as Text
+import           Network.AWS.Data.Map
 import           Network.AWS.Prelude
+import           Text.PrettyPrint
 import           Text.PrettyPrint.GenericPretty
+
+instance Out Doc where
+    docPrec _ = doc
+    doc       = id
 
 instance Out (Time a)
 
@@ -20,7 +32,22 @@ instance Out UTCTime where
     docPrec _ = doc
     doc       = doc . show
 
+instance (Out k, Out v) => Out (Map k v) where
+    docPrec _ = doc
+    doc       = doc . toMap
+
+instance (Out k, Out v) => Out (HashMap k v) where
+    docPrec _ = doc
+    doc       =
+          mappend "HashMap.fromList "
+        . doc
+        . map (bimap doc doc)
+        . Map.toList
+
 instance Out Text where
     docPrec _ = doc
     doc       = doc . Text.unpack
 
+instance Out Base64 where
+    docPrec _ = doc
+    doc       = mappend "Base64 " . doc . toText

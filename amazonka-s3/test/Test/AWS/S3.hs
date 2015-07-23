a38a3e9cb755455a2a58ea14c73317df5d35f4b7
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-23 13:37:29 +0200

    Adding ListParts fixture from the S3 documentation example
    Verifies that #122 is fixed.

--- amazonka-s3/test/Test/AWS/S3.hs
+++ amazonka-s3/test/Test/AWS/S3.hs
@@ -36,16 +36,45 @@ fixtures =
                                & rrId ?~ "rule1"
                            ])
 
-        -- FIXME: Has a JSON body, not XML, hence, serialiser error.
-        -- , testGetBucketPolicyResponse $
-        --     getBucketPolicyResponse 200
-        --         & gbprPolicy ?~ "foo"
-
         , testCopyObjectResponse $
             copyObjectResponse 200
                 & corsCopyObjectResult ?~
                     (copyObjectResult
                         & corETag         ?~ ETag "\"9b2cf535f27731c974343645a3985328\""
                         & corLastModified ?~ $(mkTime "2009-10-28T22:32:00Z"))
+
+        , testListPartsResponse $
+            listPartsResponse 200
+                & lprsBucket               ?~ "example-bucket"
+                & lprsKey                  ?~ "example-object"
+                & lprsUploadId             ?~ "XXBsb2FkIElEIGZvciBlbHZpbmcncyVcdS1tb3ZpZS5tMnRzEEEwbG9hZA"
+                & lprsStorageClass         ?~ Standard
+                & lprsPartNumberMarker     ?~ 1
+                & lprsNextPartNumberMarker ?~ 3
+                & lprsMaxParts             ?~ 2
+                & lprsIsTruncated          ?~ True
+                & lprsInitiator            ?~
+                    (initiator
+                        & iId              ?~ "arn:aws:iam::111122223333:user/some-user-11116a31-17b5-4fb7-9df5-b288870f11xx"
+                        & iDisplayName     ?~ "umat-user-11116a31-17b5-4fb7-9df5-b288870f11xx")
+                & lprsOwner                ?~
+                    (owner
+                        & oId              ?~ "75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a"
+                        & oDisplayName     ?~ "someName")
+                & lprsParts                .~
+                    [ part & pPartNumber   ?~ 2
+                           & pLastModified ?~ $(mkTime "2010-11-10T20:48:34.000Z")
+                           & pETag         ?~ "\"7778aef83f66abc1fa1e8477f296d394\""
+                           & pSize         ?~ 10485760
+                    , part & pPartNumber   ?~ 3
+                           & pLastModified ?~ $(mkTime "2010-11-10T20:48:33.000Z")
+                           & pETag         ?~ "\"aaaa18db4cc2f85cedef654fccc4a4x8\""
+                           & pSize         ?~ 10485760
+                    ]
+
+        -- FIXME: Has a JSON body, not XML, hence, serialiser error.
+        -- , testGetBucketPolicyResponse $
+        --     getBucketPolicyResponse 200
+        --         & gbprPolicy ?~ "foo"
         ]
     ]

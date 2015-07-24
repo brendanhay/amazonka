739c7a216f29411f9fbb7de029736390922e42cf
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-24 17:43:13 +0200

    Hiding Base64 use from the external lenses interface

--- gen/src/Gen/AST/Data/Syntax.hs
+++ gen/src/Gen/AST/Data/Syntax.hs
@@ -746,6 +746,7 @@ mapping t e = infixE e "." (go t)
 iso :: TType -> Maybe Exp
 iso = \case
     TLit Time        -> Just (var "_Time")
+    TLit Blob        -> Just (var "_Base64")
     TNatural         -> Just (var "_Nat")
     TSensitive   {}  -> Just (var "_Sensitive")
     TList1       {}  -> Just (var "_List1")
@@ -759,7 +760,8 @@ literal i ts = \case
     Long             -> tycon "Integer"
     Double           -> tycon "Double"
     Text             -> tycon "Text"
-    Blob             -> tycon "Base64"
+    Blob | i         -> tycon "Base64"
+         | otherwise -> tycon "ByteString"
     Bool             -> tycon "Bool"
     Time | i         -> tycon (tsToText ts)
          | otherwise -> tycon "UTCTime"

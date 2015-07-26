2e795569a674787e9280f37a61f511fb8ad7da5c
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-26 08:05:40 +0200

    Shortening license clause

--- amazonka/src/Network/AWS/EC2/Metadata.hs
+++ amazonka/src/Network/AWS/EC2/Metadata.hs
@@ -6,10 +6,7 @@
 -- |
 -- Module      : Network.AWS.EC2.Metadata
 -- Copyright   : (c) 2013-2015 Brendan Hay
--- License     : This Source Code Form is subject to the terms of
---               the Mozilla Public License, v. 2.0.
---               A copy of the MPL can be found in the LICENSE file or
---               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
 -- Stability   : experimental
 -- Portability : non-portable (GHC extensions)

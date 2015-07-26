28bb89083decc85089515ad10014281a12e4016c
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-26 20:28:35 +0200

    Explicitly URL encoding ObjectKeys when used in the path/URI

--- amazonka-s3/src/Network/AWS/S3/Internal.hs
+++ amazonka-s3/src/Network/AWS/S3/Internal.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE RankNTypes                 #-}
 
 -- Module      : Network.AWS.S3.Internal
 -- Copyright   : (c) 2013-2015 Brendan Hay
@@ -18,8 +20,12 @@ module Network.AWS.S3.Internal
     , Region
     ) where
 
+import           Control.Lens
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.Foldable         as Fold
+import           Data.Sequence         as Seq
+import           Data.Sequence.Lens
 import           Data.String
 import           Network.AWS.Data.XML
 import           Network.AWS.Prelude
@@ -85,8 +91,8 @@ newtype ETag = ETag ByteString
 type Delimiter = Char
 
 data ObjectKey
-    = DecodedKey !Delimiter [ByteString]
-    | EncodedKey ByteString
+    = DecodedKey !Delimiter (Seq ByteString)
+    | RawKey     ByteString
       deriving (Ord, Read, Show, Data, Typeable, Generic)
 
 -- FIXME: suboptimal eq complexity
@@ -94,37 +100,53 @@ instance Eq ObjectKey where
     a == b =
         case (a, b) of
             (DecodedKey x xs, DecodedKey y ys) -> x == y && xs == ys
-            _                                  -> encodedKey a == encodedKey b
+            _                                  -> encodeKey a == encodeKey b
 
-instance IsString ObjectKey where
-    fromString = objectKey '/' . fromString
+-- FIXME: cause more problems than it's worth?
+-- instance IsString ObjectKey where
+--     fromString = objectKey '/' . fromString
 
 instance FromText ObjectKey where
-    parser = EncodedKey . toBS <$> takeText
+    parser = RawKey . toBS <$> takeText
 
 instance FromXML ObjectKey where
     parseXML = parseXMLText "ObjectKey"
 
-instance ToByteString ObjectKey where toBS    = encodedKey
-instance ToText       ObjectKey where toText  = toText  . encodedKey
-instance ToQuery      ObjectKey where toQuery = toQuery . encodedKey
-instance ToBuilder    ObjectKey where build   = build   . encodedKey
-instance ToXML        ObjectKey where toXML   = toXMLText
+instance ToByteString ObjectKey where
+    toBS = \case
+        RawKey       bs -> bs
+        DecodedKey c xs -> BS8.intercalate (BS8.singleton c) (Fold.toList xs)
+
+instance ToPath ObjectKey where
+    toPath = toPath . \case
+        RawKey       bs -> bs
+        DecodedKey c xs ->
+            BS8.intercalate (BS8.singleton c) $
+                map (urlEncode True) $
+                    Fold.toList xs
+
+instance ToText    ObjectKey where toText  = toText  . toBS
+instance ToQuery   ObjectKey where toQuery = toQuery . toBS
+instance ToBuilder ObjectKey where build   = build   . toBS
+instance ToXML     ObjectKey where toXML   = toXMLText
 
 objectKey :: Delimiter -> ByteString -> ObjectKey
-objectKey c = DecodedKey c . BS8.split c
+objectKey c = DecodedKey c . splitKey c
 
-encodedKey :: ObjectKey -> ByteString
-encodedKey = \case
-    EncodedKey bs   -> bs
-    DecodedKey c xs ->
-        BS8.intercalate (BS8.singleton c) $
-            map (urlEncode True) xs
+splitKey :: Delimiter -> ByteString -> Seq ByteString
+splitKey c = Seq.fromList . BS8.split c
 
-decodedKey :: ObjectKey -> Either (Delimiter -> ByteString) ByteString
-decodedKey = \case
-    DecodedKey c xs -> Right $ BS8.intercalate (BS8.singleton c) xs
-    EncodedKey bs   -> Left  $ \c ->
-        let w = toEnum (fromEnum c)
-         in BS.intercalate (BS.singleton w) $
-                map (urlDecode True) (BS.split w bs)
+components :: Delimiter -> IndexedTraversal' Int ObjectKey ByteString
+components c f = \case
+    DecodedKey _ xs -> DecodedKey c <$> traversed f xs
+    RawKey       bs -> DecodedKey c <$> traversed f (splitKey c bs)
+
+_Last :: Delimiter -> Traversal' ObjectKey ByteString
+_Last c f = \case
+    DecodedKey _ xs -> go xs
+    RawKey       bs -> go (splitKey c bs)
+  where
+    go = fmap (DecodedKey c) . viewR right
+
+    right (xs :> x) = (xs :>) <$> f x
+    right EmptyR    = pure EmptyR

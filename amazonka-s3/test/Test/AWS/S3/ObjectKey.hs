8d598fdf5a1972a5e76af75dba6abee1187fc460
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-26 21:09:09 +0200

    Updating ObjectKey tests to latest API

--- amazonka-s3/test/Test/AWS/S3/ObjectKey.hs
+++ amazonka-s3/test/Test/AWS/S3/ObjectKey.hs
@@ -10,37 +10,27 @@
 --
 module Test.AWS.S3.ObjectKey (tests) where
 
-import           Data.String
 import           Network.AWS.Prelude
 import           Network.AWS.S3
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
--- FIXME: convert to quick/smallcheck
+-- FIXME: expand, convert to quick/smallcheck properties.
 tests :: TestTree
 tests = testGroup "object key"
     [ testGroup "encoding"
         [ testCase "without delimiters" $
-            "key" @=? encodedKey (objectKey '%' "key")
+            "key" @=? toPath (objectKey '%' "key")
 
         , testCase "without leading prefix" $
-            "some/obj%26ect" @=? encodedKey (objectKey '/' "some/obj&ect")
+            "some/obj%23ect" @=? toPath (objectKey '/' "some/obj#ect")
 
         , testCase "custom delimiter" $
-            "^some^obj%25ect^foo" @=? encodedKey (objectKey '^' "^some^obj%ect^foo")
+            "^some^obj%25ect^foo" @=?
+                 toPath (objectKey '^' "^some^obj%ect^foo")
 
         , testCase "leading prefix" $
-            decoded @=? either ($ '/') id (decodedKey encoded)
-        ]
-
-    , testGroup "text"
-        [ testCase "deserialise" $
-            Right decoded @=? (either ($ '/') id . decodedKey <$> fromText decoded)
+             "/some=1/path%20to/foo=bar/object%20here" @=?
+                 toPath (objectKey '/' "/some=1/path to/foo=bar/object here")
         ]
     ]
-
-encoded :: ObjectKey
-encoded = objectKey '/' decoded
-
-decoded :: IsString a => a
-decoded = "/some=1/path to/foo=bar/object here"

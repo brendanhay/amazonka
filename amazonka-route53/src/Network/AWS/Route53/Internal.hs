772937d061b251520a2520ed34bbce3f295ca323
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-27 19:33:29 +0200

    Updating licensing clauses

--- amazonka-route53/src/Network/AWS/Route53/Internal.hs
+++ amazonka-route53/src/Network/AWS/Route53/Internal.hs
@@ -1,15 +1,13 @@
+-- |
 -- Module      : Network.AWS.Route53.Internal
 -- Copyright   : (c) 2013-2015 Brendan Hay
--- License     : This Source Code Form is subject to the terms of
---               the Mozilla Public License, v. 2.0.
---               A copy of the MPL can be found in the LICENSE file or
---               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
 -- Stability   : experimental
 -- Portability : non-portable (GHC extensions)
-
+--
 module Network.AWS.Route53.Internal
     ( Region (..)
     ) where
 
-import Network.AWS.Types (Region(..))
+import           Network.AWS.Types (Region (..))
--- amazonka-s3/src/Network/AWS/S3/Internal.hs
+++ amazonka-s3/src/Network/AWS/S3/Internal.hs
@@ -5,16 +5,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes                 #-}
 
+-- |
 -- Module      : Network.AWS.S3.Internal
 -- Copyright   : (c) 2013-2015 Brendan Hay
--- License     : This Source Code Form is subject to the terms of
---               the Mozilla Public License, v. 2.0.
---               A copy of the MPL can be found in the LICENSE file or
---               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- License     : This Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
 -- Stability   : experimental
 -- Portability : non-portable (GHC extensions)
-
+--
 module Network.AWS.S3.Internal
     ( Region
     , BucketName      (..)

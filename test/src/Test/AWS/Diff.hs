5178319d8113e85c88262769eb1a8c08da2030b3
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-28 11:08:30 +0200

    Adding check to conditionally use colordiff for development

--- test/src/Test/AWS/Diff.hs
+++ test/src/Test/AWS/Diff.hs
@@ -17,14 +17,26 @@ import           System.Process
 import           Text.Groom
 import           Text.Printf
 
+-- | A wrapper for diff or colordiff, since CI usually doesn't have the latter.
+script :: FilePath -> FilePath -> String
+script x y =
+       "if hash colordiff 2>/dev/null; then colordiff " ++ args
+    ++ ";else diff " ++ args
+    ++ ";fi"
+  where
+     args = "-U 3 " ++ x ++ " " ++ y
+
 -- | Display the difference between two Haskell values,
 -- with control over the diff parameters.
 diff :: (Show a, Show b) => a -> b -> IO String
 diff e a =
     withSystemTempFile "diff_expect" $ \ep eh ->
         withSystemTempFile "diff_actual" $ \ap ah -> do
+
             hPutStrLn eh (groom e) >> hClose eh
             hPutStrLn ah (groom a) >> hClose ah
-            (_, !out, !err)
-                <- readProcessWithExitCode "diff" ["-U", "3", ep, ap] []
+
+            let cmd = shell (script ep ap)
+            (_, !out, !err) <- readCreateProcessWithExitCode cmd []
+
             return (out ++ "\n" ++ err)

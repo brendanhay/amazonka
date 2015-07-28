d5bbce44143923d0f0632351a8da74b01a34bb49
Brendan Hay <brendan.g.hay@gmail.com>
2015-07-28 14:38:41 +0200

    Reverting colordiff usage due to GHC7.8.4/stackage process library differences

--- test/src/Test/AWS/Diff.hs
+++ test/src/Test/AWS/Diff.hs
@@ -17,26 +17,14 @@ import           System.Process
 import           Text.Groom
 import           Text.Printf
 
--- | A wrapper for diff or colordiff, since CI usually doesn't have the latter.
-script :: FilePath -> FilePath -> String
-script x y =
-       "if hash colordiff 2>/dev/null; then colordiff " ++ args
-    ++ ";else diff " ++ args
-    ++ ";fi"
-  where
-     args = "-U 3 " ++ x ++ " " ++ y
-
 -- | Display the difference between two Haskell values,
 -- with control over the diff parameters.
 diff :: (Show a, Show b) => a -> b -> IO String
 diff e a =
     withSystemTempFile "diff_expect" $ \ep eh ->
         withSystemTempFile "diff_actual" $ \ap ah -> do
-
             hPutStrLn eh (groom e) >> hClose eh
             hPutStrLn ah (groom a) >> hClose ah
-
-            let cmd = shell (script ep ap)
-            (_, !out, !err) <- readCreateProcessWithExitCode cmd []
-
+            (_, !out, !err)
+                <- readProcessWithExitCode "diff" ["-U", "3", ep, ap] []
             return (out ++ "\n" ++ err)

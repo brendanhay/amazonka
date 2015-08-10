60368844e754b061e195d6b086af78a4301bd564
Brendan Hay <brendan.g.hay@gmail.com>
2015-08-10 20:53:21 +0200

    Formatting/consistency

--- example/SQS/RoundTrip.hs
+++ example/SQS/RoundTrip.hs
@@ -16,29 +16,29 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.AWS
 import           Data.Monoid
 import           Data.Text               (Text)
-import           Data.Text               as Text
+import qualified Data.Text               as Text
 import qualified Data.Text.IO            as Text
 import           Network.AWS.SQS
 import           System.IO
 
-roundTrip :: Text   -- ^ Name of the queue to create
+roundTrip :: Text   -- ^ Name of the queue to create.
           -> [Text] -- ^ Contents of the messages to send.
           -> IO ()
 roundTrip name xs = do
-  lgr <- newLogger Debug stdout
-  env <- newEnv Ireland Discover <&> envLogger .~ lgr
+    lgr <- newLogger Debug stdout
+    env <- newEnv Ireland Discover <&> envLogger .~ lgr
 
-  let say = liftIO . Text.putStrLn
+    let say = liftIO . Text.putStrLn
 
-  runResourceT . runAWST env $ do
-    void $ send (createQueue name)
-    url <- view gqursQueueURL <$> send (getQueueURL name)
-    say  $ "Received Queue URL: " <> url
+    runResourceT . runAWST env $ do
+        void $ send (createQueue name)
+        url <- view gqursQueueURL <$> send (getQueueURL name)
+        say  $ "Received Queue URL: " <> url
 
-    forM_ xs $ \x -> do
-        void $ send (sendMessage url x)
-        say  $ "Sent '" <> x <> "' to Queue URL: " <> url
+        forM_ xs $ \x -> do
+            void $ send (sendMessage url x)
+            say  $ "Sent '" <> x <> "' to Queue URL: " <> url
 
-    ms  <- send (receiveMessage url & rmWaitTimeSeconds ?~ 20)
-    forM_ (ms ^. rmrsMessages) $ \m ->
-        say $ "Received Message: " <> Text.pack (show m)
+        ms  <- send (receiveMessage url & rmWaitTimeSeconds ?~ 20)
+        forM_ (ms ^. rmrsMessages) $ \m ->
+            say $ "Received Message: " <> Text.pack (show m)

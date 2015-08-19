f8184b1bf82f3c6174b9f2699bc423b07d0d5f0a
Brendan Hay <brendan.g.hay@gmail.com>
2015-08-19 12:18:30 +0200

    Adding trivial EC2 describe instances example

---
+++ examples/src/Example/EC2.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- |
+-- Module      : Example.EC2
+-- Copyright   : (c) 2013-2015 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
+--
+module Example.EC2 where
+
+import           Control.Lens
+
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.AWS
+import           Data.ByteString.Builder (hPutBuilder)
+import           Data.Conduit
+import qualified Data.Conduit.List       as Conduit
+import           Data.Monoid
+import           Network.AWS.Data
+import           Network.AWS.EC2
+import           System.IO
+
+instanceOverview :: Region -> IO ()
+instanceOverview r = do
+    lgr <- newLogger Info stdout
+    env <- newEnv r Discover <&> envLogger .~ lgr
+
+    let pp x = mconcat
+          [ "[instance:" <> build (x ^. insInstanceId) <> "] {"
+          , "\n  public-dns = " <> build (x ^. insPublicDNSName)
+          , "\n  tags       = " <> build (x ^. insTags . to show)
+          , "\n  state      = " <> build (x ^. insState . isName . to toBS)
+          , "\n}\n"
+          ]
+
+    runResourceT . runAWST env $
+        paginate describeInstances
+            =$= Conduit.concatMap (view dirsReservations)
+            =$= Conduit.concatMap (view rInstances)
+             $$ Conduit.mapM_ (liftIO . hPutBuilder stdout . pp)

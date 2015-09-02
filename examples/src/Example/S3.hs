ce5f4ab7c9c8eae90c2314de5a093b8e6eb201c1
Brendan Hay <brendan.g.hay@gmail.com>
2015-09-02 10:57:13 +0200

    Adding listAll example to iterate over all buckets/object versions

--- examples/src/Example/S3.hs
+++ examples/src/Example/S3.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns      #-}
 
 -- |
 -- Module      : Example.S3
@@ -14,6 +15,8 @@ import           Control.Lens
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.AWS
+import           Data.Conduit
+import qualified Data.Conduit.List       as CL
 import qualified Data.Foldable           as Fold
 import           Data.Monoid
 import           Data.Text               (Text)
@@ -22,21 +25,47 @@ import           Network.AWS.Data
 import           Network.AWS.S3
 import           System.IO
 
-tagBucket :: BucketName          -- ^ Name of the bucket to tag.
+listAll :: Region -- ^ Region to operate in.
+        -> IO ()
+listAll r = do
+    lgr <- newLogger Debug stdout
+    env <- newEnv r Discover <&> envLogger .~ lgr
+
+    let say = liftIO . Text.putStrLn
+
+        val :: ToText a => Maybe a -> Text
+        val   = maybe "Nothing" toText
+
+        lat v = maybe mempty (mappend " - " . toText) (v ^. ovIsLatest)
+        key v = val (v ^. ovKey) <> ": " <> val (v ^. ovVersionId) <> lat v
+
+    runResourceT . runAWST env $ do
+        say "Listing Buckets .."
+        bs <- view lbrsBuckets <$> send listBuckets
+        say $ "Found " <> toText (length bs) <> " Buckets."
+
+        forM_ bs $ \(view bName -> b) -> do
+            say $ "Listing Object Versions in: " <> toText b
+            paginate (listObjectVersions b)
+                =$= CL.concatMap (view lovrsVersions)
+                 $$ CL.mapM_     (say . mappend " -> " . key)
+
+tagBucket :: Region              -- ^ Region to operate in.
+          -> BucketName          -- ^ Name of the bucket to tag.
           -> [(ObjectKey, Text)] -- ^ List of K/V pairs to apply as tags.
           -> IO ()
-tagBucket bkt xs = do
+tagBucket r b xs = do
     lgr <- newLogger Debug stdout
-    env <- newEnv Ireland Discover <&> envLogger .~ lgr
+    env <- newEnv r Discover <&> envLogger .~ lgr
 
     let say  = liftIO . Text.putStrLn
         tags = map (uncurry tag) xs
         kv t = toText (t ^. tagKey) <> "=" <> (t ^. tagValue)
 
     runResourceT . runAWST env $ do
-        void . send $ putBucketTagging bkt (tagging & tTagSet .~ tags)
+        void . send $ putBucketTagging b (tagging & tTagSet .~ tags)
         say $ "Successfully Put Tags: " <> Fold.foldMap kv tags
 
-        ts <- view gbtrsTagSet <$> send (getBucketTagging bkt)
+        ts <- view gbtrsTagSet <$> send (getBucketTagging b)
         forM_ ts $ \t ->
             say $ "Found Tag: " <> kv t

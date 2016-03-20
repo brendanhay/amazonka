0472f3c0246ccf7c470034f1c703bff4ab6a5ad6
Brendan Hay <brendan.g.hay@gmail.com>
2016-03-20 17:44:20 +0100

    Updating copyright date

--- amazonka-config/gen/Network/AWS/Config/DescribeDeliveryChannels.hs
+++ amazonka-config/gen/Network/AWS/Config/DescribeDeliveryChannels.hs
@@ -12,7 +12,7 @@
 
 -- |
 -- Module      : Network.AWS.Config.DescribeDeliveryChannels
--- Copyright   : (c) 2013-2015 Brendan Hay
+-- Copyright   : (c) 2013-2016 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
 -- Stability   : auto-generated

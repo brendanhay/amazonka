7072d97e75bac6b61691534bf1640749e103bb5e
Brendan Hay <brendan.g.hay@gmail.com>
2016-03-20 07:47:31 +0100

    Adding amazonka-certificatemanager test plumbing

---
+++ amazonka-certificatemanager/test/Test/AWS/CertificateManager.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
+-- |
+-- Module      : Test.AWS.CertificateManager
+-- Copyright   : (c) 2013-2015 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+module Test.AWS.CertificateManager
+    ( tests
+    , fixtures
+    ) where
+
+import           Test.AWS.Prelude
+
+tests :: [TestTree]
+tests = []
+
+fixtures :: [TestTree]
+fixtures = []

ba52ad1d9202b12b3d1e10fc23121859329a819c
Brendan Hay <brendan.g.hay@gmail.com>
2016-08-16 13:49:54 +0200

    Adding ServiceCatalog test plumbing

---
+++ amazonka-servicecatalog/test/Test/AWS/ServiceCatalog.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Module      : Test.AWS.ServiceCatalog
+-- Copyright   : (c) 2013-2016 Brendan Hay
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+module Test.AWS.ServiceCatalog where
+
+import Test.AWS.Prelude
+
+tests :: [TestTree]
+tests = []
+
+fixtures :: [TestTree]
+fixtures = []

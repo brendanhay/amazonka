752012d9498ae0c9fa873899750ef18d1165f74d
Brendan Hay <brendan.g.hay@gmail.com>
2016-08-16 13:16:26 +0200

    Adding new RDS operations

---
+++ amazonka-rds/gen/Network/AWS/RDS/CopyDBClusterParameterGroup.hs
@@ -0,0 +1,184 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE TypeFamilies       #-}
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds   #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.RDS.CopyDBClusterParameterGroup
+-- Copyright   : (c) 2013-2016 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Copies the specified DB cluster parameter group.
+module Network.AWS.RDS.CopyDBClusterParameterGroup
+    (
+    -- * Creating a Request
+      copyDBClusterParameterGroup
+    , CopyDBClusterParameterGroup
+    -- * Request Lenses
+    , cdbcpgTags
+    , cdbcpgSourceDBClusterParameterGroupIdentifier
+    , cdbcpgTargetDBClusterParameterGroupIdentifier
+    , cdbcpgTargetDBClusterParameterGroupDescription
+
+    -- * Destructuring the Response
+    , copyDBClusterParameterGroupResponse
+    , CopyDBClusterParameterGroupResponse
+    -- * Response Lenses
+    , cdcpgrsDBClusterParameterGroup
+    , cdcpgrsResponseStatus
+    ) where
+
+import           Network.AWS.Lens
+import           Network.AWS.Prelude
+import           Network.AWS.RDS.Types
+import           Network.AWS.RDS.Types.Product
+import           Network.AWS.Request
+import           Network.AWS.Response
+
+-- | /See:/ 'copyDBClusterParameterGroup' smart constructor.
+data CopyDBClusterParameterGroup = CopyDBClusterParameterGroup'
+    { _cdbcpgTags                                     :: !(Maybe [Tag])
+    , _cdbcpgSourceDBClusterParameterGroupIdentifier  :: !Text
+    , _cdbcpgTargetDBClusterParameterGroupIdentifier  :: !Text
+    , _cdbcpgTargetDBClusterParameterGroupDescription :: !Text
+    } deriving (Eq,Read,Show,Data,Typeable,Generic)
+
+-- | Creates a value of 'CopyDBClusterParameterGroup' with the minimum fields required to make a request.
+--
+-- Use one of the following lenses to modify other fields as desired:
+--
+-- * 'cdbcpgTags'
+--
+-- * 'cdbcpgSourceDBClusterParameterGroupIdentifier'
+--
+-- * 'cdbcpgTargetDBClusterParameterGroupIdentifier'
+--
+-- * 'cdbcpgTargetDBClusterParameterGroupDescription'
+copyDBClusterParameterGroup
+    :: Text -- ^ 'cdbcpgSourceDBClusterParameterGroupIdentifier'
+    -> Text -- ^ 'cdbcpgTargetDBClusterParameterGroupIdentifier'
+    -> Text -- ^ 'cdbcpgTargetDBClusterParameterGroupDescription'
+    -> CopyDBClusterParameterGroup
+copyDBClusterParameterGroup pSourceDBClusterParameterGroupIdentifier_ pTargetDBClusterParameterGroupIdentifier_ pTargetDBClusterParameterGroupDescription_ =
+    CopyDBClusterParameterGroup'
+    { _cdbcpgTags = Nothing
+    , _cdbcpgSourceDBClusterParameterGroupIdentifier = pSourceDBClusterParameterGroupIdentifier_
+    , _cdbcpgTargetDBClusterParameterGroupIdentifier = pTargetDBClusterParameterGroupIdentifier_
+    , _cdbcpgTargetDBClusterParameterGroupDescription = pTargetDBClusterParameterGroupDescription_
+    }
+
+-- | Undocumented member.
+cdbcpgTags :: Lens' CopyDBClusterParameterGroup [Tag]
+cdbcpgTags = lens _cdbcpgTags (\ s a -> s{_cdbcpgTags = a}) . _Default . _Coerce;
+
+-- | The identifier or Amazon Resource Name (ARN) for the source DB cluster parameter group. For information about creating an ARN, see <http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_Tagging.html#USER_Tagging.ARN Constructing an RDS Amazon Resource Name (ARN)>.
+--
+-- Constraints:
+--
+-- -   Must specify a valid DB cluster parameter group.
+--
+-- -   If the source DB cluster parameter group is in the same region as the copy, specify a valid DB parameter group identifier, for example 'my-db-cluster-param-group', or a valid ARN.
+--
+-- -   If the source DB parameter group is in a different region than the copy, specify a valid DB cluster parameter group ARN, for example 'arn:aws:rds:us-east-1:123456789012:cluster-pg:custom-cluster-group1'.
+--
+cdbcpgSourceDBClusterParameterGroupIdentifier :: Lens' CopyDBClusterParameterGroup Text
+cdbcpgSourceDBClusterParameterGroupIdentifier = lens _cdbcpgSourceDBClusterParameterGroupIdentifier (\ s a -> s{_cdbcpgSourceDBClusterParameterGroupIdentifier = a});
+
+-- | The identifier for the copied DB cluster parameter group.
+--
+-- Constraints:
+--
+-- -   Cannot be null, empty, or blank
+--
+-- -   Must contain from 1 to 255 alphanumeric characters or hyphens
+--
+-- -   First character must be a letter
+--
+-- -   Cannot end with a hyphen or contain two consecutive hyphens
+--
+-- Example: 'my-cluster-param-group1'
+cdbcpgTargetDBClusterParameterGroupIdentifier :: Lens' CopyDBClusterParameterGroup Text
+cdbcpgTargetDBClusterParameterGroupIdentifier = lens _cdbcpgTargetDBClusterParameterGroupIdentifier (\ s a -> s{_cdbcpgTargetDBClusterParameterGroupIdentifier = a});
+
+-- | A description for the copied DB cluster parameter group.
+cdbcpgTargetDBClusterParameterGroupDescription :: Lens' CopyDBClusterParameterGroup Text
+cdbcpgTargetDBClusterParameterGroupDescription = lens _cdbcpgTargetDBClusterParameterGroupDescription (\ s a -> s{_cdbcpgTargetDBClusterParameterGroupDescription = a});
+
+instance AWSRequest CopyDBClusterParameterGroup where
+        type Rs CopyDBClusterParameterGroup =
+             CopyDBClusterParameterGroupResponse
+        request = postQuery rds
+        response
+          = receiveXMLWrapper
+              "CopyDBClusterParameterGroupResult"
+              (\ s h x ->
+                 CopyDBClusterParameterGroupResponse' <$>
+                   (x .@? "DBClusterParameterGroup") <*>
+                     (pure (fromEnum s)))
+
+instance Hashable CopyDBClusterParameterGroup
+
+instance NFData CopyDBClusterParameterGroup
+
+instance ToHeaders CopyDBClusterParameterGroup where
+        toHeaders = const mempty
+
+instance ToPath CopyDBClusterParameterGroup where
+        toPath = const "/"
+
+instance ToQuery CopyDBClusterParameterGroup where
+        toQuery CopyDBClusterParameterGroup'{..}
+          = mconcat
+              ["Action" =:
+                 ("CopyDBClusterParameterGroup" :: ByteString),
+               "Version" =: ("2014-10-31" :: ByteString),
+               "Tags" =:
+                 toQuery (toQueryList "Tag" <$> _cdbcpgTags),
+               "SourceDBClusterParameterGroupIdentifier" =:
+                 _cdbcpgSourceDBClusterParameterGroupIdentifier,
+               "TargetDBClusterParameterGroupIdentifier" =:
+                 _cdbcpgTargetDBClusterParameterGroupIdentifier,
+               "TargetDBClusterParameterGroupDescription" =:
+                 _cdbcpgTargetDBClusterParameterGroupDescription]
+
+-- | /See:/ 'copyDBClusterParameterGroupResponse' smart constructor.
+data CopyDBClusterParameterGroupResponse = CopyDBClusterParameterGroupResponse'
+    { _cdcpgrsDBClusterParameterGroup :: !(Maybe DBClusterParameterGroup)
+    , _cdcpgrsResponseStatus          :: !Int
+    } deriving (Eq,Read,Show,Data,Typeable,Generic)
+
+-- | Creates a value of 'CopyDBClusterParameterGroupResponse' with the minimum fields required to make a request.
+--
+-- Use one of the following lenses to modify other fields as desired:
+--
+-- * 'cdcpgrsDBClusterParameterGroup'
+--
+-- * 'cdcpgrsResponseStatus'
+copyDBClusterParameterGroupResponse
+    :: Int -- ^ 'cdcpgrsResponseStatus'
+    -> CopyDBClusterParameterGroupResponse
+copyDBClusterParameterGroupResponse pResponseStatus_ =
+    CopyDBClusterParameterGroupResponse'
+    { _cdcpgrsDBClusterParameterGroup = Nothing
+    , _cdcpgrsResponseStatus = pResponseStatus_
+    }
+
+-- | Undocumented member.
+cdcpgrsDBClusterParameterGroup :: Lens' CopyDBClusterParameterGroupResponse (Maybe DBClusterParameterGroup)
+cdcpgrsDBClusterParameterGroup = lens _cdcpgrsDBClusterParameterGroup (\ s a -> s{_cdcpgrsDBClusterParameterGroup = a});
+
+-- | The response status code.
+cdcpgrsResponseStatus :: Lens' CopyDBClusterParameterGroupResponse Int
+cdcpgrsResponseStatus = lens _cdcpgrsResponseStatus (\ s a -> s{_cdcpgrsResponseStatus = a});
+
+instance NFData CopyDBClusterParameterGroupResponse
---
+++ amazonka-rds/gen/Network/AWS/RDS/ModifyDBClusterSnapshotAttribute.hs
@@ -0,0 +1,183 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE TypeFamilies       #-}
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds   #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.RDS.ModifyDBClusterSnapshotAttribute
+-- Copyright   : (c) 2013-2016 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Adds an attribute and values to, or removes an attribute and values from, a manual DB cluster snapshot.
+--
+-- To share a manual DB cluster snapshot with other AWS accounts, specify 'restore' as the 'AttributeName' and use the 'ValuesToAdd' parameter to add a list of IDs of the AWS accounts that are authorized to restore the manual DB cluster snapshot. Use the value 'all' to make the manual DB cluster snapshot public, which means that it can be copied or restored by all AWS accounts. Do not add the 'all' value for any manual DB cluster snapshots that contain private information that you don\'t want available to all AWS accounts.
+--
+-- To view which AWS accounts have access to copy or restore a manual DB cluster snapshot, or whether a manual DB cluster snapshot public or private, use the < DescribeDBClusterSnapshotAttributes> API action.
+--
+-- If a manual DB cluster snapshot is encrypted, it cannot be shared.
+module Network.AWS.RDS.ModifyDBClusterSnapshotAttribute
+    (
+    -- * Creating a Request
+      modifyDBClusterSnapshotAttribute
+    , ModifyDBClusterSnapshotAttribute
+    -- * Request Lenses
+    , mdcsaValuesToAdd
+    , mdcsaValuesToRemove
+    , mdcsaDBClusterSnapshotIdentifier
+    , mdcsaAttributeName
+
+    -- * Destructuring the Response
+    , modifyDBClusterSnapshotAttributeResponse
+    , ModifyDBClusterSnapshotAttributeResponse
+    -- * Response Lenses
+    , mdcsarsDBClusterSnapshotAttributesResult
+    , mdcsarsResponseStatus
+    ) where
+
+import           Network.AWS.Lens
+import           Network.AWS.Prelude
+import           Network.AWS.RDS.Types
+import           Network.AWS.RDS.Types.Product
+import           Network.AWS.Request
+import           Network.AWS.Response
+
+-- |
+--
+-- /See:/ 'modifyDBClusterSnapshotAttribute' smart constructor.
+data ModifyDBClusterSnapshotAttribute = ModifyDBClusterSnapshotAttribute'
+    { _mdcsaValuesToAdd                 :: !(Maybe [Text])
+    , _mdcsaValuesToRemove              :: !(Maybe [Text])
+    , _mdcsaDBClusterSnapshotIdentifier :: !Text
+    , _mdcsaAttributeName               :: !Text
+    } deriving (Eq,Read,Show,Data,Typeable,Generic)
+
+-- | Creates a value of 'ModifyDBClusterSnapshotAttribute' with the minimum fields required to make a request.
+--
+-- Use one of the following lenses to modify other fields as desired:
+--
+-- * 'mdcsaValuesToAdd'
+--
+-- * 'mdcsaValuesToRemove'
+--
+-- * 'mdcsaDBClusterSnapshotIdentifier'
+--
+-- * 'mdcsaAttributeName'
+modifyDBClusterSnapshotAttribute
+    :: Text -- ^ 'mdcsaDBClusterSnapshotIdentifier'
+    -> Text -- ^ 'mdcsaAttributeName'
+    -> ModifyDBClusterSnapshotAttribute
+modifyDBClusterSnapshotAttribute pDBClusterSnapshotIdentifier_ pAttributeName_ =
+    ModifyDBClusterSnapshotAttribute'
+    { _mdcsaValuesToAdd = Nothing
+    , _mdcsaValuesToRemove = Nothing
+    , _mdcsaDBClusterSnapshotIdentifier = pDBClusterSnapshotIdentifier_
+    , _mdcsaAttributeName = pAttributeName_
+    }
+
+-- | A list of DB cluster snapshot attributes to add to the attribute specified by 'AttributeName'.
+--
+-- To authorize other AWS accounts to copy or restore a manual DB cluster snapshot, set this list to include one or more AWS account IDs, or 'all' to make the manual DB cluster snapshot restorable by any AWS account. Do not add the 'all' value for any manual DB cluster snapshots that contain private information that you don\'t want available to all AWS accounts.
+mdcsaValuesToAdd :: Lens' ModifyDBClusterSnapshotAttribute [Text]
+mdcsaValuesToAdd = lens _mdcsaValuesToAdd (\ s a -> s{_mdcsaValuesToAdd = a}) . _Default . _Coerce;
+
+-- | A list of DB cluster snapshot attributes to remove from the attribute specified by 'AttributeName'.
+--
+-- To remove authorization for other AWS accounts to copy or restore a manual DB cluster snapshot, set this list to include one or more AWS account identifiers, or 'all' to remove authorization for any AWS account to copy or restore the DB cluster snapshot. If you specify 'all', an AWS account whose account ID is explicitly added to the 'restore' attribute can still copy or restore a manual DB cluster snapshot.
+mdcsaValuesToRemove :: Lens' ModifyDBClusterSnapshotAttribute [Text]
+mdcsaValuesToRemove = lens _mdcsaValuesToRemove (\ s a -> s{_mdcsaValuesToRemove = a}) . _Default . _Coerce;
+
+-- | The identifier for the DB cluster snapshot to modify the attributes for.
+mdcsaDBClusterSnapshotIdentifier :: Lens' ModifyDBClusterSnapshotAttribute Text
+mdcsaDBClusterSnapshotIdentifier = lens _mdcsaDBClusterSnapshotIdentifier (\ s a -> s{_mdcsaDBClusterSnapshotIdentifier = a});
+
+-- | The name of the DB cluster snapshot attribute to modify.
+--
+-- To manage authorization for other AWS accounts to copy or restore a manual DB cluster snapshot, set this value to 'restore'.
+mdcsaAttributeName :: Lens' ModifyDBClusterSnapshotAttribute Text
+mdcsaAttributeName = lens _mdcsaAttributeName (\ s a -> s{_mdcsaAttributeName = a});
+
+instance AWSRequest ModifyDBClusterSnapshotAttribute
+         where
+        type Rs ModifyDBClusterSnapshotAttribute =
+             ModifyDBClusterSnapshotAttributeResponse
+        request = postQuery rds
+        response
+          = receiveXMLWrapper
+              "ModifyDBClusterSnapshotAttributeResult"
+              (\ s h x ->
+                 ModifyDBClusterSnapshotAttributeResponse' <$>
+                   (x .@? "DBClusterSnapshotAttributesResult") <*>
+                     (pure (fromEnum s)))
+
+instance Hashable ModifyDBClusterSnapshotAttribute
+
+instance NFData ModifyDBClusterSnapshotAttribute
+
+instance ToHeaders ModifyDBClusterSnapshotAttribute
+         where
+        toHeaders = const mempty
+
+instance ToPath ModifyDBClusterSnapshotAttribute
+         where
+        toPath = const "/"
+
+instance ToQuery ModifyDBClusterSnapshotAttribute
+         where
+        toQuery ModifyDBClusterSnapshotAttribute'{..}
+          = mconcat
+              ["Action" =:
+                 ("ModifyDBClusterSnapshotAttribute" :: ByteString),
+               "Version" =: ("2014-10-31" :: ByteString),
+               "ValuesToAdd" =:
+                 toQuery
+                   (toQueryList "AttributeValue" <$> _mdcsaValuesToAdd),
+               "ValuesToRemove" =:
+                 toQuery
+                   (toQueryList "AttributeValue" <$>
+                      _mdcsaValuesToRemove),
+               "DBClusterSnapshotIdentifier" =:
+                 _mdcsaDBClusterSnapshotIdentifier,
+               "AttributeName" =: _mdcsaAttributeName]
+
+-- | /See:/ 'modifyDBClusterSnapshotAttributeResponse' smart constructor.
+data ModifyDBClusterSnapshotAttributeResponse = ModifyDBClusterSnapshotAttributeResponse'
+    { _mdcsarsDBClusterSnapshotAttributesResult :: !(Maybe DBClusterSnapshotAttributesResult)
+    , _mdcsarsResponseStatus                    :: !Int
+    } deriving (Eq,Read,Show,Data,Typeable,Generic)
+
+-- | Creates a value of 'ModifyDBClusterSnapshotAttributeResponse' with the minimum fields required to make a request.
+--
+-- Use one of the following lenses to modify other fields as desired:
+--
+-- * 'mdcsarsDBClusterSnapshotAttributesResult'
+--
+-- * 'mdcsarsResponseStatus'
+modifyDBClusterSnapshotAttributeResponse
+    :: Int -- ^ 'mdcsarsResponseStatus'
+    -> ModifyDBClusterSnapshotAttributeResponse
+modifyDBClusterSnapshotAttributeResponse pResponseStatus_ =
+    ModifyDBClusterSnapshotAttributeResponse'
+    { _mdcsarsDBClusterSnapshotAttributesResult = Nothing
+    , _mdcsarsResponseStatus = pResponseStatus_
+    }
+
+-- | Undocumented member.
+mdcsarsDBClusterSnapshotAttributesResult :: Lens' ModifyDBClusterSnapshotAttributeResponse (Maybe DBClusterSnapshotAttributesResult)
+mdcsarsDBClusterSnapshotAttributesResult = lens _mdcsarsDBClusterSnapshotAttributesResult (\ s a -> s{_mdcsarsDBClusterSnapshotAttributesResult = a});
+
+-- | The response status code.
+mdcsarsResponseStatus :: Lens' ModifyDBClusterSnapshotAttributeResponse Int
+mdcsarsResponseStatus = lens _mdcsarsResponseStatus (\ s a -> s{_mdcsarsResponseStatus = a});
+
+instance NFData
+         ModifyDBClusterSnapshotAttributeResponse

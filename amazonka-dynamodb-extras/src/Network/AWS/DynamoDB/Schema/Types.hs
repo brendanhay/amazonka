c739ad6825c28118178c88d4682f453abd4d2be7
Brendan Hay <brendan.g.hay@gmail.com>
2016-08-26 21:23:19 +0200

    Preliminary schema type documentation

--- amazonka-dynamodb-extras/src/Network/AWS/DynamoDB/Schema/Types.hs
+++ amazonka-dynamodb-extras/src/Network/AWS/DynamoDB/Schema/Types.hs
@@ -17,15 +17,40 @@ import Network.AWS.DynamoDB (ProjectionType, StreamViewType)
 
 import qualified Data.Text as Text
 
+-- | A DynamoDB table schema.
+--
+-- The parameters are the table name, the attribute schema, and any additional
+-- configuration options.
 data Table (name :: Symbol) schema options
 
 data PartitionKey (name :: Symbol) hash
 data SortKey      (name :: Symbol) range
 data Attribute    (name :: Symbol) value
 
-data ReadCapacity  (read  :: Nat)
+-- | One read capacity unit represents one strongly consistent read per second, or
+-- two eventually consistent reads per second, for items up to 4 KB in size.
+--
+-- If you need to read an item that is larger than 4 KB, DynamoDB will need to
+-- consume additional read capacity units. The total number of read capacity units
+-- required depends on the item size, and whether you want an eventually
+-- consistent or strongly consistent read.
+data ReadCapacity (read  :: Nat)
+
+-- | One write capacity unit represents one write per second for items up
+-- to 1 KB in size.
+--
+-- If you need to write an item that is larger than 1 KB, DynamoDB will need
+-- to consume additional write capacity units. The total number of write capacity
+-- units required depends on the item size.
 data WriteCapacity (write :: Nat)
 
+-- | The provisioned throughput capacity you want to reserve for reads and writes.
+--
+-- DynamoDB will reserve the necessary resources to meet your throughput needs
+-- while ensuring consistent, low-latency performance. You can also change your
+-- provisioned throughput settings, increasing or decreasing capacity as needed.
+--
+-- The parameters are 'ReadCapacity' and 'WriteCapacity', respectively.
 data Throughput read write
 
 data Stream (view :: StreamViewType)
@@ -33,8 +58,9 @@ data Stream (view :: StreamViewType)
 data IndexPartitionKey (key :: Symbol)
 data IndexSortKey      (key :: Symbol)
 
--- Every global secondary index must have a partition key, and can have an optional
--- sort key. The index key schema can be different from the table schema
+-- Every global secondary index must have a partition key and can also
+-- have an optional sort key.
+-- The index key schema can be different from the table schema
 data GlobalSecondaryIndex (name :: Symbol) options
 
 -- Every local secondary index must meet the following conditions:

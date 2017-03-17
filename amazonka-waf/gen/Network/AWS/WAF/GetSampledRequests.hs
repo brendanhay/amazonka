8b369080db039a86e7fee25aa02ed4d3fc7a156e
Brendan Hay <brendan.g.hay@gmail.com>
2017-03-17 23:22:37 +0100

    Regenerating amazonka-waf from latest service definition

--- amazonka-waf/gen/Network/AWS/WAF/GetSampledRequests.hs
+++ amazonka-waf/gen/Network/AWS/WAF/GetSampledRequests.hs
@@ -18,7 +18,7 @@
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
--- Gets detailed information about a specified number of requests--a sample--that AWS WAF randomly selects from among the first 5,000 requests that your AWS resource received during a time range that you choose. You can specify a sample size of up to 100 requests, and you can specify any time range in the previous three hours.
+-- Gets detailed information about a specified number of requests--a sample--that AWS WAF randomly selects from among the first 5,000 requests that your AWS resource received during a time range that you choose. You can specify a sample size of up to 500 requests, and you can specify any time range in the previous three hours.
 --
 --
 -- @GetSampledRequests@ returns a time range, which is usually the time range that you specified. However, if your resource (such as a CloudFront distribution) received 5,000 requests before the specified time range elapsed, @GetSampledRequests@ returns an updated time range. This new time range indicates the actual period during which AWS WAF selected the requests in the sample.

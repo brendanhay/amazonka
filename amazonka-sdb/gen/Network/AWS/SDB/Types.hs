144857cfeefca9a8f025a2706e1c60ebb2ef5605
Brendan Hay <brendan.g.hay@gmail.com>
2017-08-18 12:56:39 +0200

    Regenerating amazonka-sdb from latest service definition

--- amazonka-sdb/gen/Network/AWS/SDB/Types.hs
+++ amazonka-sdb/gen/Network/AWS/SDB/Types.hs
@@ -105,6 +105,8 @@ sdb =
         , _retryCheck = check
         }
     check e
+      | has (hasCode "ThrottledException" . hasStatus 400) e =
+          Just "throttled_exception"
       | has (hasStatus 429) e = Just "too_many_requests"
       | has (hasCode "ThrottlingException" . hasStatus 400) e =
           Just "throttling_exception"

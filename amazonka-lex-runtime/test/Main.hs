4e7c420573769c745c51a98eab52a114accfdf72
Nick Hibberd <nhibberd@gmail.com>
2019-03-24 11:40:43 -0700

    Updating amazonka-lex-runtime service definition

--- amazonka-lex-runtime/test/Main.hs
+++ amazonka-lex-runtime/test/Main.hs
@@ -10,9 +10,9 @@
 --
 module Main (main) where
 
-import Test.Tasty
 import Test.AWS.LexRuntime
 import Test.AWS.LexRuntime.Internal
+import Test.Tasty
 
 main :: IO ()
 main = defaultMain $ testGroup "LexRuntime"

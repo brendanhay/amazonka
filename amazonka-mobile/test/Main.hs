d560233a2802212264306b588eef7874228de06b
Nick Hibberd <nhibberd@gmail.com>
2019-03-24 11:40:45 -0700

    Updating amazonka-mobile service definition

--- amazonka-mobile/test/Main.hs
+++ amazonka-mobile/test/Main.hs
@@ -10,9 +10,9 @@
 --
 module Main (main) where
 
-import Test.Tasty
 import Test.AWS.Mobile
 import Test.AWS.Mobile.Internal
+import Test.Tasty
 
 main :: IO ()
 main = defaultMain $ testGroup "Mobile"

89962cf682a75ddb8d56c93242c1f4a5f5788346
Nick Hibberd <nhibberd@gmail.com>
2019-03-24 11:40:47 -0700

    Updating amazonka-pricing service definition

--- amazonka-pricing/test/Main.hs
+++ amazonka-pricing/test/Main.hs
@@ -10,9 +10,9 @@
 --
 module Main (main) where
 
-import Test.Tasty
 import Test.AWS.Pricing
 import Test.AWS.Pricing.Internal
+import Test.Tasty
 
 main :: IO ()
 main = defaultMain $ testGroup "Pricing"

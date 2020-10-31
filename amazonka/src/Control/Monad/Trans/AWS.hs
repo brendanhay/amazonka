f0a3bc557a5da616c79c61fa43f12e04e978adf8
Peter Becich <peter@simspace.com>
2020-10-31 01:07:21 -0700

    unliftio-core upgrade

--- amazonka/src/Control/Monad/Trans/AWS.hs
+++ amazonka/src/Control/Monad/Trans/AWS.hs
@@ -241,10 +241,6 @@ instance MonadBaseControl b m => MonadBaseControl b (AWST' r m) where
     liftBaseWith = defaultLiftBaseWith
     restoreM     = defaultRestoreM
 
-instance MonadUnliftIO m => MonadUnliftIO (AWST' r m) where
-    askUnliftIO = AWST' $ (\(UnliftIO f) -> UnliftIO $ f . unAWST)
-        <$> askUnliftIO
-
 instance MonadResource m => MonadResource (AWST' r m) where
     liftResourceT = lift . liftResourceT
 

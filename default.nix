92ffdb41be55e8206bbce56af906c0831c1e19af
Brendan Hay <brendan.g.hay@gmail.com>
2020-11-22 19:33:38 +0100

    Adding nix configuration

---
+++ default.nix
@@ -0,0 +1,53 @@
+# The build system where packages will be _built_.
+{ system ? builtins.currentSystem
+  # The host system where packages will _run_.
+, crossSystem ? null
+  # Additional sources.json overrides.
+, sources ? { }
+  # Additional nixpkgs.config overrides.
+, config ? { }
+  # Additional nixpkgs.overlays.
+, overlays ? [ ]
+  # Overlays to apply to the last package set in cross compilation.
+, crossOverlays ? [ ] }:
+
+let
+
+  pkgs = import ./nix/default.nix {
+    inherit system sources config overlays crossOverlays;
+  };
+
+  components = pkgs.libLocal.collectHaskellComponents pkgs.cabalProject;
+
+in {
+  inherit (pkgs) cabalProject;
+  inherit (components) library checks exes;
+
+  shell = pkgs.cabalProject.shellFor {
+    withHoogle = true;
+    exactDeps = true;
+
+    packages = ps: 
+      with ps; [
+        amazonka
+        amazonka-core
+        amazonka-gen
+      ];
+
+      # pkgs.lib.attrValues ps;
+
+    tools = {
+      cabal = "3.2.0.0";
+      ormolu = "0.1.3.0";
+      shellcheck = "0.7.1";
+    };
+
+    buildInputs = [
+      pkgs.nixfmt
+      pkgs.shfmt
+
+      # sources.json
+      (import pkgs.sources.niv { }).niv
+    ];
+  };
+}
---
+++ nix/default.nix
@@ -0,0 +1,40 @@
+# The build system where packages will be _built_.
+{ system ? builtins.currentSystem
+  # The host system where packages will _run_.
+, crossSystem ? null
+  # Additional sources.json overrides.
+, sources ? { }
+  # Additional nixpkgs.config overrides.
+, config ? { }
+  # Additional nixpkgs.overlays.
+, overlays ? [ ]
+  # Overlays to apply to the last package set in cross compilation.
+, crossOverlays ? [ ] }:
+
+let
+
+  finalSources = import ./sources.nix { inherit pkgs; } // sources;
+
+  haskellNix = import finalSources."haskell.nix" {
+    sourcesOverride = { hackage = finalSources."hackage.nix"; };
+  };
+
+  finalOverlays = haskellNix.overlays ++ [
+    # Add top-level `.sources` attribute.
+    (_final: _prev: { sources = finalSources; })
+
+    # Basic library functions.
+    (import ./overlays/lib-local.nix)
+
+    # Local package sets.
+    (import ./overlays/haskell.nix)
+  ] ++ overlays;
+
+  pkgs = import finalSources.nixpkgs {
+    inherit system crossSystem crossOverlays;
+
+    config = haskellNix.config // config;
+    overlays = finalOverlays;
+  };
+
+in pkgs

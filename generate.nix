0555a70fd2cdc56e19dd976e8029ada4bcb19495
Brendan Hay <brendan.g.hay@gmail.com>
2020-12-02 15:53:17 +0100

    Trimming output paths during code generation/formatting

--- generate.nix
+++ generate.nix
@@ -66,11 +66,10 @@ in pkgs.stdenvNoCC.mkDerivation {
     mkdir -p $out
     cp -R $src/amazonka-* $out/
     chmod -R u+rw $out
-
-    echo "Starting code generation"
+    cd $out
 
     amazonka-gen \
-      --out=$out \
+      --out=. \
       --library-version=${libraryVersion} \
       --client-version=${clientVersion} \
       --core-version=${coreVersion} \
@@ -85,16 +84,16 @@ in pkgs.stdenvNoCC.mkDerivation {
   formatPhase = ''
     export LC_ALL=C.UTF-8
 
-    echo "Starting formatting"
+    cd $out
 
-    for dir in $out/amazonka*; do 
+    for dir in amazonka*; do 
       echo " -> Formatting $dir"
 
-      find $dir -type f -name '*.cabal' \
-        | xargs cabal-fmt --inplace --indent=2
+      find $dir -type f -name '*.cabal' -print0 \
+        | xargs -0 cabal-fmt --inplace --indent=2
 
-      find $dir -type f -name '*.hs' \
-        | xargs ormolu --mode=inplace
+      find $dir -type f -name '*.hs' -print0 \
+        | xargs -0 ormolu --mode=inplace
     done
   '';
 }

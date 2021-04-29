566a414e645b9695c5cae27496c023758a2f85e9
Brendan Hay <brendan.g.hay@gmail.com>
2021-04-29 12:18:43 +0200

    Regenerating amazonka-marketplace-entitlement

--- amazonka-marketplace-entitlement/gen/Network/AWS/MarketplaceEntitlement/Types.hs
+++ amazonka-marketplace-entitlement/gen/Network/AWS/MarketplaceEntitlement/Types.hs
@@ -1,111 +1,144 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- Derived from AWS service descriptions, licensed under Apache 2.0.
 
 -- |
 -- Module      : Network.AWS.MarketplaceEntitlement.Types
--- Copyright   : (c) 2013-2018 Brendan Hay
+-- Copyright   : (c) 2013-2021 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
---
 module Network.AWS.MarketplaceEntitlement.Types
-    (
-    -- * Service Configuration
-      marketplaceEntitlement
+  ( -- * Service Configuration
+    defaultService,
 
     -- * Errors
-    , _InvalidParameterException
-    , _ThrottlingException
-    , _InternalServiceErrorException
+    _ThrottlingException,
+    _InvalidParameterException,
+    _InternalServiceErrorException,
 
     -- * GetEntitlementFilterName
-    , GetEntitlementFilterName (..)
+    GetEntitlementFilterName (..),
 
     -- * Entitlement
-    , Entitlement
-    , entitlement
-    , eDimension
-    , eValue
-    , eExpirationDate
-    , eCustomerIdentifier
-    , eProductCode
+    Entitlement (..),
+    newEntitlement,
+    entitlement_expirationDate,
+    entitlement_customerIdentifier,
+    entitlement_productCode,
+    entitlement_value,
+    entitlement_dimension,
 
     -- * EntitlementValue
-    , EntitlementValue
-    , entitlementValue
-    , evIntegerValue
-    , evDoubleValue
-    , evStringValue
-    , evBooleanValue
-    ) where
+    EntitlementValue (..),
+    newEntitlementValue,
+    entitlementValue_doubleValue,
+    entitlementValue_stringValue,
+    entitlementValue_booleanValue,
+    entitlementValue_integerValue,
+  )
+where
 
-import Network.AWS.Lens
-import Network.AWS.MarketplaceEntitlement.Types.Product
-import Network.AWS.MarketplaceEntitlement.Types.Sum
-import Network.AWS.Prelude
-import Network.AWS.Sign.V4
+import qualified Network.AWS.Lens as Lens
+import Network.AWS.MarketplaceEntitlement.Types.Entitlement
+import Network.AWS.MarketplaceEntitlement.Types.EntitlementValue
+import Network.AWS.MarketplaceEntitlement.Types.GetEntitlementFilterName
+import qualified Network.AWS.Prelude as Prelude
+import qualified Network.AWS.Sign.V4 as Sign
 
 -- | API version @2017-01-11@ of the Amazon Marketplace Entitlement Service SDK configuration.
-marketplaceEntitlement :: Service
-marketplaceEntitlement =
-  Service
-    { _svcAbbrev = "MarketplaceEntitlement"
-    , _svcSigner = v4
-    , _svcPrefix = "entitlement.marketplace"
-    , _svcVersion = "2017-01-11"
-    , _svcEndpoint = defaultEndpoint marketplaceEntitlement
-    , _svcTimeout = Just 70
-    , _svcCheck = statusSuccess
-    , _svcError = parseJSONError "MarketplaceEntitlement"
-    , _svcRetry = retry
+defaultService :: Prelude.Service
+defaultService =
+  Prelude.Service
+    { Prelude._svcAbbrev =
+        "MarketplaceEntitlement",
+      Prelude._svcSigner = Sign.v4,
+      Prelude._svcPrefix = "entitlement.marketplace",
+      Prelude._svcVersion = "2017-01-11",
+      Prelude._svcEndpoint =
+        Prelude.defaultEndpoint defaultService,
+      Prelude._svcTimeout = Prelude.Just 70,
+      Prelude._svcCheck = Prelude.statusSuccess,
+      Prelude._svcError =
+        Prelude.parseJSONError "MarketplaceEntitlement",
+      Prelude._svcRetry = retry
     }
   where
     retry =
-      Exponential
-        { _retryBase = 5.0e-2
-        , _retryGrowth = 2
-        , _retryAttempts = 5
-        , _retryCheck = check
+      Prelude.Exponential
+        { Prelude._retryBase = 5.0e-2,
+          Prelude._retryGrowth = 2,
+          Prelude._retryAttempts = 5,
+          Prelude._retryCheck = check
         }
     check e
-      | has (hasCode "ThrottledException" . hasStatus 400) e =
-        Just "throttled_exception"
-      | has (hasStatus 429) e = Just "too_many_requests"
-      | has (hasCode "ThrottlingException" . hasStatus 400) e =
-        Just "throttling_exception"
-      | has (hasCode "Throttling" . hasStatus 400) e = Just "throttling"
-      | has (hasStatus 504) e = Just "gateway_timeout"
-      | has (hasCode "RequestThrottledException" . hasStatus 400) e =
-        Just "request_throttled_exception"
-      | has (hasStatus 502) e = Just "bad_gateway"
-      | has (hasStatus 503) e = Just "service_unavailable"
-      | has (hasStatus 500) e = Just "general_server_error"
-      | has (hasStatus 509) e = Just "limit_exceeded"
-      | otherwise = Nothing
-
-
--- | One or more parameters in your request was invalid.
---
---
-_InvalidParameterException :: AsError a => Getting (First ServiceError) a ServiceError
-_InvalidParameterException =
-  _MatchServiceError marketplaceEntitlement "InvalidParameterException"
-
+      | Lens.has (Prelude.hasStatus 504) e =
+        Prelude.Just "gateway_timeout"
+      | Lens.has
+          ( Prelude.hasCode
+              "ProvisionedThroughputExceededException"
+              Prelude.. Prelude.hasStatus 400
+          )
+          e =
+        Prelude.Just "throughput_exceeded"
+      | Lens.has (Prelude.hasStatus 503) e =
+        Prelude.Just "service_unavailable"
+      | Lens.has (Prelude.hasStatus 502) e =
+        Prelude.Just "bad_gateway"
+      | Lens.has (Prelude.hasStatus 429) e =
+        Prelude.Just "too_many_requests"
+      | Lens.has
+          ( Prelude.hasCode "RequestThrottledException"
+              Prelude.. Prelude.hasStatus 400
+          )
+          e =
+        Prelude.Just "request_throttled_exception"
+      | Lens.has
+          ( Prelude.hasCode "ThrottledException"
+              Prelude.. Prelude.hasStatus 400
+          )
+          e =
+        Prelude.Just "throttled_exception"
+      | Lens.has (Prelude.hasStatus 509) e =
+        Prelude.Just "limit_exceeded"
+      | Lens.has (Prelude.hasStatus 500) e =
+        Prelude.Just "general_server_error"
+      | Lens.has
+          ( Prelude.hasCode "ThrottlingException"
+              Prelude.. Prelude.hasStatus 400
+          )
+          e =
+        Prelude.Just "throttling_exception"
+      | Lens.has
+          ( Prelude.hasCode "Throttling"
+              Prelude.. Prelude.hasStatus 400
+          )
+          e =
+        Prelude.Just "throttling"
+      | Prelude.otherwise = Prelude.Nothing
 
 -- | The calls to the GetEntitlements API are throttled.
---
---
-_ThrottlingException :: AsError a => Getting (First ServiceError) a ServiceError
+_ThrottlingException :: Prelude.AsError a => Lens.Getting (Prelude.First Prelude.ServiceError) a Prelude.ServiceError
 _ThrottlingException =
-  _MatchServiceError marketplaceEntitlement "ThrottlingException"
+  Prelude._MatchServiceError
+    defaultService
+    "ThrottlingException"
 
+-- | One or more parameters in your request was invalid.
+_InvalidParameterException :: Prelude.AsError a => Lens.Getting (Prelude.First Prelude.ServiceError) a Prelude.ServiceError
+_InvalidParameterException =
+  Prelude._MatchServiceError
+    defaultService
+    "InvalidParameterException"
 
--- | An internal error has occurred. Retry your request. If the problem persists, post a message with details on the AWS forums.
---
---
-_InternalServiceErrorException :: AsError a => Getting (First ServiceError) a ServiceError
+-- | An internal error has occurred. Retry your request. If the problem
+-- persists, post a message with details on the AWS forums.
+_InternalServiceErrorException :: Prelude.AsError a => Lens.Getting (Prelude.First Prelude.ServiceError) a Prelude.ServiceError
 _InternalServiceErrorException =
-  _MatchServiceError marketplaceEntitlement "InternalServiceErrorException"
-
+  Prelude._MatchServiceError
+    defaultService
+    "InternalServiceErrorException"

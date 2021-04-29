d9586315a39a8bfd6536aff8906aedcebcb82d65
Brendan Hay <brendan.g.hay@gmail.com>
2021-04-29 12:18:23 +0200

    Regenerating amazonka-cloudtrail

--- amazonka-cloudtrail/gen/Network/AWS/CloudTrail/StopLogging.hs
+++ amazonka-cloudtrail/gen/Network/AWS/CloudTrail/StopLogging.hs
@@ -1,129 +1,168 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE RecordWildCards    #-}
-{-# LANGUAGE TypeFamilies       #-}
-
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
-{-# OPTIONS_GHC -fno-warn-unused-binds   #-}
 {-# OPTIONS_GHC -fno-warn-unused-matches #-}
 
 -- Derived from AWS service descriptions, licensed under Apache 2.0.
 
 -- |
 -- Module      : Network.AWS.CloudTrail.StopLogging
--- Copyright   : (c) 2013-2018 Brendan Hay
+-- Copyright   : (c) 2013-2021 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --
--- Suspends the recording of AWS API calls and log file delivery for the specified trail. Under most circumstances, there is no need to use this action. You can update a trail without stopping it first. This action is the only way to stop recording. For a trail enabled in all regions, this operation must be called from the region in which the trail was created, or an @InvalidHomeRegionException@ will occur. This operation cannot be called on the shadow trails (replicated trails in other regions) of a trail enabled in all regions.
---
---
+-- Suspends the recording of AWS API calls and log file delivery for the
+-- specified trail. Under most circumstances, there is no need to use this
+-- action. You can update a trail without stopping it first. This action is
+-- the only way to stop recording. For a trail enabled in all regions, this
+-- operation must be called from the region in which the trail was created,
+-- or an @InvalidHomeRegionException@ will occur. This operation cannot be
+-- called on the shadow trails (replicated trails in other regions) of a
+-- trail enabled in all regions.
 module Network.AWS.CloudTrail.StopLogging
-    (
-    -- * Creating a Request
-      stopLogging
-    , StopLogging
+  ( -- * Creating a Request
+    StopLogging (..),
+    newStopLogging,
+
     -- * Request Lenses
-    , slName
+    stopLogging_name,
 
     -- * Destructuring the Response
-    , stopLoggingResponse
-    , StopLoggingResponse
+    StopLoggingResponse (..),
+    newStopLoggingResponse,
+
     -- * Response Lenses
-    , slrsResponseStatus
-    ) where
+    stopLoggingResponse_httpStatus,
+  )
+where
 
 import Network.AWS.CloudTrail.Types
-import Network.AWS.CloudTrail.Types.Product
-import Network.AWS.Lens
-import Network.AWS.Prelude
-import Network.AWS.Request
-import Network.AWS.Response
+import qualified Network.AWS.Lens as Lens
+import qualified Network.AWS.Prelude as Prelude
+import qualified Network.AWS.Request as Request
+import qualified Network.AWS.Response as Response
 
--- | Passes the request to CloudTrail to stop logging AWS API calls for the specified account.
+-- | Passes the request to CloudTrail to stop logging AWS API calls for the
+-- specified account.
 --
+-- /See:/ 'newStopLogging' smart constructor.
+data StopLogging = StopLogging'
+  { -- | Specifies the name or the CloudTrail ARN of the trail for which
+    -- CloudTrail will stop logging AWS API calls. The format of a trail ARN
+    -- is:
+    --
+    -- @arn:aws:cloudtrail:us-east-2:123456789012:trail\/MyTrail@
+    name :: Prelude.Text
+  }
+  deriving (Prelude.Eq, Prelude.Read, Prelude.Show, Prelude.Data, Prelude.Typeable, Prelude.Generic)
+
+-- |
+-- Create a value of 'StopLogging' with all optional fields omitted.
 --
+-- Use <https://hackage.haskell.org/package/generic-lens generic-lens> or <https://hackage.haskell.org/package/optics optics> to modify other optional fields.
 --
--- /See:/ 'stopLogging' smart constructor.
-newtype StopLogging = StopLogging'
-  { _slName :: Text
-  } deriving (Eq, Read, Show, Data, Typeable, Generic)
-
-
--- | Creates a value of 'StopLogging' with the minimum fields required to make a request.
+-- The following record fields are available, with the corresponding lenses provided
+-- for backwards compatibility:
 --
--- Use one of the following lenses to modify other fields as desired:
+-- 'name', 'stopLogging_name' - Specifies the name or the CloudTrail ARN of the trail for which
+-- CloudTrail will stop logging AWS API calls. The format of a trail ARN
+-- is:
 --
--- * 'slName' - Specifies the name or the CloudTrail ARN of the trail for which CloudTrail will stop logging AWS API calls. The format of a trail ARN is: @arn:aws:cloudtrail:us-east-1:123456789012:trail/MyTrail@
-stopLogging
-    :: Text -- ^ 'slName'
-    -> StopLogging
-stopLogging pName_ = StopLogging' {_slName = pName_}
+-- @arn:aws:cloudtrail:us-east-2:123456789012:trail\/MyTrail@
+newStopLogging ::
+  -- | 'name'
+  Prelude.Text ->
+  StopLogging
+newStopLogging pName_ = StopLogging' {name = pName_}
 
-
--- | Specifies the name or the CloudTrail ARN of the trail for which CloudTrail will stop logging AWS API calls. The format of a trail ARN is: @arn:aws:cloudtrail:us-east-1:123456789012:trail/MyTrail@
-slName :: Lens' StopLogging Text
-slName = lens _slName (\ s a -> s{_slName = a})
-
-instance AWSRequest StopLogging where
-        type Rs StopLogging = StopLoggingResponse
-        request = postJSON cloudTrail
-        response
-          = receiveEmpty
-              (\ s h x ->
-                 StopLoggingResponse' <$> (pure (fromEnum s)))
-
-instance Hashable StopLogging where
-
-instance NFData StopLogging where
-
-instance ToHeaders StopLogging where
-        toHeaders
-          = const
-              (mconcat
-                 ["X-Amz-Target" =#
-                    ("com.amazonaws.cloudtrail.v20131101.CloudTrail_20131101.StopLogging"
-                       :: ByteString),
-                  "Content-Type" =#
-                    ("application/x-amz-json-1.1" :: ByteString)])
-
-instance ToJSON StopLogging where
-        toJSON StopLogging'{..}
-          = object (catMaybes [Just ("Name" .= _slName)])
-
-instance ToPath StopLogging where
-        toPath = const "/"
-
-instance ToQuery StopLogging where
-        toQuery = const mempty
-
--- | Returns the objects or data listed below if successful. Otherwise, returns an error.
+-- | Specifies the name or the CloudTrail ARN of the trail for which
+-- CloudTrail will stop logging AWS API calls. The format of a trail ARN
+-- is:
 --
+-- @arn:aws:cloudtrail:us-east-2:123456789012:trail\/MyTrail@
+stopLogging_name :: Lens.Lens' StopLogging Prelude.Text
+stopLogging_name = Lens.lens (\StopLogging' {name} -> name) (\s@StopLogging' {} a -> s {name = a} :: StopLogging)
+
+instance Prelude.AWSRequest StopLogging where
+  type Rs StopLogging = StopLoggingResponse
+  request = Request.postJSON defaultService
+  response =
+    Response.receiveEmpty
+      ( \s h x ->
+          StopLoggingResponse'
+            Prelude.<$> (Prelude.pure (Prelude.fromEnum s))
+      )
+
+instance Prelude.Hashable StopLogging
+
+instance Prelude.NFData StopLogging
+
+instance Prelude.ToHeaders StopLogging where
+  toHeaders =
+    Prelude.const
+      ( Prelude.mconcat
+          [ "X-Amz-Target"
+              Prelude.=# ( "com.amazonaws.cloudtrail.v20131101.CloudTrail_20131101.StopLogging" ::
+                             Prelude.ByteString
+                         ),
+            "Content-Type"
+              Prelude.=# ( "application/x-amz-json-1.1" ::
+                             Prelude.ByteString
+                         )
+          ]
+      )
+
+instance Prelude.ToJSON StopLogging where
+  toJSON StopLogging' {..} =
+    Prelude.object
+      ( Prelude.catMaybes
+          [Prelude.Just ("Name" Prelude..= name)]
+      )
+
+instance Prelude.ToPath StopLogging where
+  toPath = Prelude.const "/"
+
+instance Prelude.ToQuery StopLogging where
+  toQuery = Prelude.const Prelude.mempty
+
+-- | Returns the objects or data listed below if successful. Otherwise,
+-- returns an error.
 --
+-- /See:/ 'newStopLoggingResponse' smart constructor.
+data StopLoggingResponse = StopLoggingResponse'
+  { -- | The response's http status code.
+    httpStatus :: Prelude.Int
+  }
+  deriving (Prelude.Eq, Prelude.Read, Prelude.Show, Prelude.Data, Prelude.Typeable, Prelude.Generic)
+
+-- |
+-- Create a value of 'StopLoggingResponse' with all optional fields omitted.
 --
--- /See:/ 'stopLoggingResponse' smart constructor.
-newtype StopLoggingResponse = StopLoggingResponse'
-  { _slrsResponseStatus :: Int
-  } deriving (Eq, Read, Show, Data, Typeable, Generic)
-
-
--- | Creates a value of 'StopLoggingResponse' with the minimum fields required to make a request.
+-- Use <https://hackage.haskell.org/package/generic-lens generic-lens> or <https://hackage.haskell.org/package/optics optics> to modify other optional fields.
 --
--- Use one of the following lenses to modify other fields as desired:
+-- The following record fields are available, with the corresponding lenses provided
+-- for backwards compatibility:
 --
--- * 'slrsResponseStatus' - -- | The response status code.
-stopLoggingResponse
-    :: Int -- ^ 'slrsResponseStatus'
-    -> StopLoggingResponse
-stopLoggingResponse pResponseStatus_ =
-  StopLoggingResponse' {_slrsResponseStatus = pResponseStatus_}
+-- 'httpStatus', 'stopLoggingResponse_httpStatus' - The response's http status code.
+newStopLoggingResponse ::
+  -- | 'httpStatus'
+  Prelude.Int ->
+  StopLoggingResponse
+newStopLoggingResponse pHttpStatus_ =
+  StopLoggingResponse' {httpStatus = pHttpStatus_}
 
+-- | The response's http status code.
+stopLoggingResponse_httpStatus :: Lens.Lens' StopLoggingResponse Prelude.Int
+stopLoggingResponse_httpStatus = Lens.lens (\StopLoggingResponse' {httpStatus} -> httpStatus) (\s@StopLoggingResponse' {} a -> s {httpStatus = a} :: StopLoggingResponse)
 
--- | -- | The response status code.
-slrsResponseStatus :: Lens' StopLoggingResponse Int
-slrsResponseStatus = lens _slrsResponseStatus (\ s a -> s{_slrsResponseStatus = a})
-
-instance NFData StopLoggingResponse where
+instance Prelude.NFData StopLoggingResponse

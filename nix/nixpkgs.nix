012ea199c3df7255c926b5a198a956277e32747b
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-09 18:54:05 +0200

    bazel: remove ghc865 support due to complexity

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,21 +1,6 @@
 { system ? builtins.currentSystem, config ? { }, overlays ? [ ]
 , sources ? import ./sources.nix { } }:
 
-let
-
-  nixpkgs-2009 = import sources.nixpkgs-2009 { inherit system; };
-
-in import sources.nixpkgs-latest {
-  inherit system config;
-
-  overlays = [
-    (final: prev: {
-      inherit sources;
-
-      haskell = prev.lib.recursiveUpdate prev.haskell {
-        compiler.ghc865 = nixpkgs-2009.haskell.compiler.ghc865;
-        packages.ghc865 = nixpkgs-2009.haskell.packages.ghc865;
-      };
-    })
-  ] ++ overlays;
+import sources.nixpkgs-latest {
+  inherit system config overlays;
 }

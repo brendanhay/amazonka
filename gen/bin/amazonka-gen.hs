938d7e325276576375043b3575d26ec642422817
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-10 12:04:12 +0200

    amazonka-gen-configs: replace bash tool with haskell

--- gen/bin/amazonka-gen.hs
+++ gen/bin/amazonka-gen.hs
@@ -3,16 +3,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
 
--- Module      : Main
--- Copyright   : (c) 2013-2021 Brendan Hay
--- License     : This Source Code Form is subject to the terms of
---               the Mozilla Public License, v. 2.0.
---               A copy of the MPL can be found in the LICENSE file or
---               you can obtain it at http://mozilla.org/MPL/2.0/.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
--- Stability   : provisional
--- Portability : non-portable (GHC extensions)
-
 module Main (main) where
 
 import Control.Lens (Lens', (^.))

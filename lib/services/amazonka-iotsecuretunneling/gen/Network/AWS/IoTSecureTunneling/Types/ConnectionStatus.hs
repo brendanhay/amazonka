f33134fa744ef491514d3165a12d93167127a28e
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-27 11:59:50 +0200

    amazonka-iotsecuretunneling: regenerating service

---
+++ lib/services/amazonka-iotsecuretunneling/gen/Network/AWS/IoTSecureTunneling/Types/ConnectionStatus.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.IoTSecureTunneling.Types.ConnectionStatus
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Network.AWS.IoTSecureTunneling.Types.ConnectionStatus
+  ( ConnectionStatus
+      ( ..,
+        ConnectionStatus_CONNECTED,
+        ConnectionStatus_DISCONNECTED
+      ),
+  )
+where
+
+import qualified Network.AWS.Core as Core
+import qualified Network.AWS.Prelude as Prelude
+
+newtype ConnectionStatus = ConnectionStatus'
+  { fromConnectionStatus ::
+      Core.Text
+  }
+  deriving stock
+    ( Prelude.Show,
+      Prelude.Read,
+      Prelude.Eq,
+      Prelude.Ord,
+      Prelude.Generic
+    )
+  deriving newtype
+    ( Prelude.Hashable,
+      Prelude.NFData,
+      Core.FromText,
+      Core.ToText,
+      Core.ToByteString,
+      Core.ToLog,
+      Core.ToHeader,
+      Core.ToQuery,
+      Core.FromJSON,
+      Core.FromJSONKey,
+      Core.ToJSON,
+      Core.ToJSONKey,
+      Core.FromXML,
+      Core.ToXML
+    )
+
+pattern ConnectionStatus_CONNECTED :: ConnectionStatus
+pattern ConnectionStatus_CONNECTED = ConnectionStatus' "CONNECTED"
+
+pattern ConnectionStatus_DISCONNECTED :: ConnectionStatus
+pattern ConnectionStatus_DISCONNECTED = ConnectionStatus' "DISCONNECTED"
+
+{-# COMPLETE
+  ConnectionStatus_CONNECTED,
+  ConnectionStatus_DISCONNECTED,
+  ConnectionStatus'
+  #-}
---
+++ lib/services/amazonka-iotsecuretunneling/gen/Network/AWS/IoTSecureTunneling/Types/TunnelSummary.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.IoTSecureTunneling.Types.TunnelSummary
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Network.AWS.IoTSecureTunneling.Types.TunnelSummary where
+
+import qualified Network.AWS.Core as Core
+import Network.AWS.IoTSecureTunneling.Types.TunnelStatus
+import qualified Network.AWS.Lens as Lens
+import qualified Network.AWS.Prelude as Prelude
+
+-- | Information about the tunnel.
+--
+-- /See:/ 'newTunnelSummary' smart constructor.
+data TunnelSummary = TunnelSummary'
+  { -- | The status of a tunnel. Valid values are: Open and Closed.
+    status :: Prelude.Maybe TunnelStatus,
+    -- | The time the tunnel was last updated.
+    lastUpdatedAt :: Prelude.Maybe Core.POSIX,
+    -- | The time the tunnel was created.
+    createdAt :: Prelude.Maybe Core.POSIX,
+    -- | The Amazon Resource Name of the tunnel. The tunnel ARN format is
+    -- @arn:aws:tunnel:\<region>:\<account-id>:tunnel\/\<tunnel-id>@
+    tunnelArn :: Prelude.Maybe Prelude.Text,
+    -- | The unique alpha-numeric identifier for the tunnel.
+    tunnelId :: Prelude.Maybe Prelude.Text,
+    -- | A description of the tunnel.
+    description :: Prelude.Maybe Prelude.Text
+  }
+  deriving (Prelude.Eq, Prelude.Read, Prelude.Show, Prelude.Generic)
+
+-- |
+-- Create a value of 'TunnelSummary' with all optional fields omitted.
+--
+-- Use <https://hackage.haskell.org/package/generic-lens generic-lens> or <https://hackage.haskell.org/package/optics optics> to modify other optional fields.
+--
+-- The following record fields are available, with the corresponding lenses provided
+-- for backwards compatibility:
+--
+-- 'status', 'tunnelSummary_status' - The status of a tunnel. Valid values are: Open and Closed.
+--
+-- 'lastUpdatedAt', 'tunnelSummary_lastUpdatedAt' - The time the tunnel was last updated.
+--
+-- 'createdAt', 'tunnelSummary_createdAt' - The time the tunnel was created.
+--
+-- 'tunnelArn', 'tunnelSummary_tunnelArn' - The Amazon Resource Name of the tunnel. The tunnel ARN format is
+-- @arn:aws:tunnel:\<region>:\<account-id>:tunnel\/\<tunnel-id>@
+--
+-- 'tunnelId', 'tunnelSummary_tunnelId' - The unique alpha-numeric identifier for the tunnel.
+--
+-- 'description', 'tunnelSummary_description' - A description of the tunnel.
+newTunnelSummary ::
+  TunnelSummary
+newTunnelSummary =
+  TunnelSummary'
+    { status = Prelude.Nothing,
+      lastUpdatedAt = Prelude.Nothing,
+      createdAt = Prelude.Nothing,
+      tunnelArn = Prelude.Nothing,
+      tunnelId = Prelude.Nothing,
+      description = Prelude.Nothing
+    }
+
+-- | The status of a tunnel. Valid values are: Open and Closed.
+tunnelSummary_status :: Lens.Lens' TunnelSummary (Prelude.Maybe TunnelStatus)
+tunnelSummary_status = Lens.lens (\TunnelSummary' {status} -> status) (\s@TunnelSummary' {} a -> s {status = a} :: TunnelSummary)
+
+-- | The time the tunnel was last updated.
+tunnelSummary_lastUpdatedAt :: Lens.Lens' TunnelSummary (Prelude.Maybe Prelude.UTCTime)
+tunnelSummary_lastUpdatedAt = Lens.lens (\TunnelSummary' {lastUpdatedAt} -> lastUpdatedAt) (\s@TunnelSummary' {} a -> s {lastUpdatedAt = a} :: TunnelSummary) Prelude.. Lens.mapping Core._Time
+
+-- | The time the tunnel was created.
+tunnelSummary_createdAt :: Lens.Lens' TunnelSummary (Prelude.Maybe Prelude.UTCTime)
+tunnelSummary_createdAt = Lens.lens (\TunnelSummary' {createdAt} -> createdAt) (\s@TunnelSummary' {} a -> s {createdAt = a} :: TunnelSummary) Prelude.. Lens.mapping Core._Time
+
+-- | The Amazon Resource Name of the tunnel. The tunnel ARN format is
+-- @arn:aws:tunnel:\<region>:\<account-id>:tunnel\/\<tunnel-id>@
+tunnelSummary_tunnelArn :: Lens.Lens' TunnelSummary (Prelude.Maybe Prelude.Text)
+tunnelSummary_tunnelArn = Lens.lens (\TunnelSummary' {tunnelArn} -> tunnelArn) (\s@TunnelSummary' {} a -> s {tunnelArn = a} :: TunnelSummary)
+
+-- | The unique alpha-numeric identifier for the tunnel.
+tunnelSummary_tunnelId :: Lens.Lens' TunnelSummary (Prelude.Maybe Prelude.Text)
+tunnelSummary_tunnelId = Lens.lens (\TunnelSummary' {tunnelId} -> tunnelId) (\s@TunnelSummary' {} a -> s {tunnelId = a} :: TunnelSummary)
+
+-- | A description of the tunnel.
+tunnelSummary_description :: Lens.Lens' TunnelSummary (Prelude.Maybe Prelude.Text)
+tunnelSummary_description = Lens.lens (\TunnelSummary' {description} -> description) (\s@TunnelSummary' {} a -> s {description = a} :: TunnelSummary)
+
+instance Core.FromJSON TunnelSummary where
+  parseJSON =
+    Core.withObject
+      "TunnelSummary"
+      ( \x ->
+          TunnelSummary'
+            Prelude.<$> (x Core..:? "status")
+            Prelude.<*> (x Core..:? "lastUpdatedAt")
+            Prelude.<*> (x Core..:? "createdAt")
+            Prelude.<*> (x Core..:? "tunnelArn")
+            Prelude.<*> (x Core..:? "tunnelId")
+            Prelude.<*> (x Core..:? "description")
+      )
+
+instance Prelude.Hashable TunnelSummary
+
+instance Prelude.NFData TunnelSummary

580d6838f0139c38fea10ad13855500bd5976d43
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-27 11:59:32 +0200

    amazonka-account: regenerating service

---
+++ lib/services/amazonka-account/gen/Network/AWS/Account/Types/AlternateContact.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.Account.Types.AlternateContact
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Network.AWS.Account.Types.AlternateContact where
+
+import Network.AWS.Account.Types.AlternateContactType
+import qualified Network.AWS.Core as Core
+import qualified Network.AWS.Lens as Lens
+import qualified Network.AWS.Prelude as Prelude
+
+-- | A structure that contains the details of an alternate contact associated
+-- with an Amazon Web Services account
+--
+-- /See:/ 'newAlternateContact' smart constructor.
+data AlternateContact = AlternateContact'
+  { -- | The type of alternate contact.
+    alternateContactType :: Prelude.Maybe AlternateContactType,
+    -- | The phone number associated with this alternate contact.
+    phoneNumber :: Prelude.Maybe (Core.Sensitive Prelude.Text),
+    -- | The name associated with this alternate contact.
+    name :: Prelude.Maybe (Core.Sensitive Prelude.Text),
+    -- | The email address associated with this alternate contact.
+    emailAddress :: Prelude.Maybe (Core.Sensitive Prelude.Text),
+    -- | The title associated with this alternate contact.
+    title :: Prelude.Maybe (Core.Sensitive Prelude.Text)
+  }
+  deriving (Prelude.Eq, Prelude.Show, Prelude.Generic)
+
+-- |
+-- Create a value of 'AlternateContact' with all optional fields omitted.
+--
+-- Use <https://hackage.haskell.org/package/generic-lens generic-lens> or <https://hackage.haskell.org/package/optics optics> to modify other optional fields.
+--
+-- The following record fields are available, with the corresponding lenses provided
+-- for backwards compatibility:
+--
+-- 'alternateContactType', 'alternateContact_alternateContactType' - The type of alternate contact.
+--
+-- 'phoneNumber', 'alternateContact_phoneNumber' - The phone number associated with this alternate contact.
+--
+-- 'name', 'alternateContact_name' - The name associated with this alternate contact.
+--
+-- 'emailAddress', 'alternateContact_emailAddress' - The email address associated with this alternate contact.
+--
+-- 'title', 'alternateContact_title' - The title associated with this alternate contact.
+newAlternateContact ::
+  AlternateContact
+newAlternateContact =
+  AlternateContact'
+    { alternateContactType =
+        Prelude.Nothing,
+      phoneNumber = Prelude.Nothing,
+      name = Prelude.Nothing,
+      emailAddress = Prelude.Nothing,
+      title = Prelude.Nothing
+    }
+
+-- | The type of alternate contact.
+alternateContact_alternateContactType :: Lens.Lens' AlternateContact (Prelude.Maybe AlternateContactType)
+alternateContact_alternateContactType = Lens.lens (\AlternateContact' {alternateContactType} -> alternateContactType) (\s@AlternateContact' {} a -> s {alternateContactType = a} :: AlternateContact)
+
+-- | The phone number associated with this alternate contact.
+alternateContact_phoneNumber :: Lens.Lens' AlternateContact (Prelude.Maybe Prelude.Text)
+alternateContact_phoneNumber = Lens.lens (\AlternateContact' {phoneNumber} -> phoneNumber) (\s@AlternateContact' {} a -> s {phoneNumber = a} :: AlternateContact) Prelude.. Lens.mapping Core._Sensitive
+
+-- | The name associated with this alternate contact.
+alternateContact_name :: Lens.Lens' AlternateContact (Prelude.Maybe Prelude.Text)
+alternateContact_name = Lens.lens (\AlternateContact' {name} -> name) (\s@AlternateContact' {} a -> s {name = a} :: AlternateContact) Prelude.. Lens.mapping Core._Sensitive
+
+-- | The email address associated with this alternate contact.
+alternateContact_emailAddress :: Lens.Lens' AlternateContact (Prelude.Maybe Prelude.Text)
+alternateContact_emailAddress = Lens.lens (\AlternateContact' {emailAddress} -> emailAddress) (\s@AlternateContact' {} a -> s {emailAddress = a} :: AlternateContact) Prelude.. Lens.mapping Core._Sensitive
+
+-- | The title associated with this alternate contact.
+alternateContact_title :: Lens.Lens' AlternateContact (Prelude.Maybe Prelude.Text)
+alternateContact_title = Lens.lens (\AlternateContact' {title} -> title) (\s@AlternateContact' {} a -> s {title = a} :: AlternateContact) Prelude.. Lens.mapping Core._Sensitive
+
+instance Core.FromJSON AlternateContact where
+  parseJSON =
+    Core.withObject
+      "AlternateContact"
+      ( \x ->
+          AlternateContact'
+            Prelude.<$> (x Core..:? "AlternateContactType")
+            Prelude.<*> (x Core..:? "PhoneNumber")
+            Prelude.<*> (x Core..:? "Name")
+            Prelude.<*> (x Core..:? "EmailAddress")
+            Prelude.<*> (x Core..:? "Title")
+      )
+
+instance Prelude.Hashable AlternateContact
+
+instance Prelude.NFData AlternateContact
---
+++ lib/services/amazonka-account/test/Test/AWS/Gen/Account.hs
@@ -0,0 +1,98 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Test.AWS.Gen.Account
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Test.AWS.Gen.Account where
+
+import Data.Proxy
+import Network.AWS.Account
+import Test.AWS.Account.Internal
+import Test.AWS.Fixture
+import Test.AWS.Prelude
+import Test.Tasty
+
+-- Auto-generated: the actual test selection needs to be manually placed into
+-- the top-level so that real test data can be incrementally added.
+--
+-- This commented snippet is what the entire set should look like:
+
+-- fixtures :: TestTree
+-- fixtures =
+--     [ testGroup "request"
+--         [ requestDeleteAlternateContact $
+--             newDeleteAlternateContact
+--
+--         , requestPutAlternateContact $
+--             newPutAlternateContact
+--
+--         , requestGetAlternateContact $
+--             newGetAlternateContact
+--
+--           ]
+
+--     , testGroup "response"
+--         [ responseDeleteAlternateContact $
+--             newDeleteAlternateContactResponse
+--
+--         , responsePutAlternateContact $
+--             newPutAlternateContactResponse
+--
+--         , responseGetAlternateContact $
+--             newGetAlternateContactResponse
+--
+--           ]
+--     ]
+
+-- Requests
+
+requestDeleteAlternateContact :: DeleteAlternateContact -> TestTree
+requestDeleteAlternateContact =
+  req
+    "DeleteAlternateContact"
+    "fixture/DeleteAlternateContact.yaml"
+
+requestPutAlternateContact :: PutAlternateContact -> TestTree
+requestPutAlternateContact =
+  req
+    "PutAlternateContact"
+    "fixture/PutAlternateContact.yaml"
+
+requestGetAlternateContact :: GetAlternateContact -> TestTree
+requestGetAlternateContact =
+  req
+    "GetAlternateContact"
+    "fixture/GetAlternateContact.yaml"
+
+-- Responses
+
+responseDeleteAlternateContact :: DeleteAlternateContactResponse -> TestTree
+responseDeleteAlternateContact =
+  res
+    "DeleteAlternateContactResponse"
+    "fixture/DeleteAlternateContactResponse.proto"
+    defaultService
+    (Proxy :: Proxy DeleteAlternateContact)
+
+responsePutAlternateContact :: PutAlternateContactResponse -> TestTree
+responsePutAlternateContact =
+  res
+    "PutAlternateContactResponse"
+    "fixture/PutAlternateContactResponse.proto"
+    defaultService
+    (Proxy :: Proxy PutAlternateContact)
+
+responseGetAlternateContact :: GetAlternateContactResponse -> TestTree
+responseGetAlternateContact =
+  res
+    "GetAlternateContactResponse"
+    "fixture/GetAlternateContactResponse.proto"
+    defaultService
+    (Proxy :: Proxy GetAlternateContact)

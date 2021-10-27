27400d843ff00845d68f42551f4475305f515a3d
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-27 12:00:01 +0200

    amazonka-rds-data: regenerating service

---
+++ lib/services/amazonka-rds-data/gen/Network/AWS/RDSData.hs
@@ -0,0 +1,152 @@
+{-# OPTIONS_GHC -fno-warn-duplicate-exports #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- |
+-- Module      : Network.AWS.RDSData
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Derived from API version @2018-08-01@ of the AWS service descriptions, licensed under Apache 2.0.
+--
+-- Amazon RDS Data Service
+--
+-- Amazon RDS provides an HTTP endpoint to run SQL statements on an Amazon
+-- Aurora Serverless DB cluster. To run these statements, you work with the
+-- Data Service API.
+--
+-- For more information about the Data Service API, see
+-- <https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/data-api.html Using the Data API for Aurora Serverless>
+-- in the /Amazon Aurora User Guide/.
+module Network.AWS.RDSData
+  ( -- * Service Configuration
+    defaultService,
+
+    -- * Errors
+    -- $errors
+
+    -- ** StatementTimeoutException
+    _StatementTimeoutException,
+
+    -- ** ForbiddenException
+    _ForbiddenException,
+
+    -- ** NotFoundException
+    _NotFoundException,
+
+    -- ** ServiceUnavailableError
+    _ServiceUnavailableError,
+
+    -- ** InternalServerErrorException
+    _InternalServerErrorException,
+
+    -- ** BadRequestException
+    _BadRequestException,
+
+    -- * Waiters
+    -- $waiters
+
+    -- * Operations
+    -- $operations
+
+    -- ** RollbackTransaction
+    RollbackTransaction (RollbackTransaction'),
+    newRollbackTransaction,
+    RollbackTransactionResponse (RollbackTransactionResponse'),
+    newRollbackTransactionResponse,
+
+    -- ** BeginTransaction
+    BeginTransaction (BeginTransaction'),
+    newBeginTransaction,
+    BeginTransactionResponse (BeginTransactionResponse'),
+    newBeginTransactionResponse,
+
+    -- ** BatchExecuteStatement
+    BatchExecuteStatement (BatchExecuteStatement'),
+    newBatchExecuteStatement,
+    BatchExecuteStatementResponse (BatchExecuteStatementResponse'),
+    newBatchExecuteStatementResponse,
+
+    -- ** ExecuteStatement
+    ExecuteStatement (ExecuteStatement'),
+    newExecuteStatement,
+    ExecuteStatementResponse (ExecuteStatementResponse'),
+    newExecuteStatementResponse,
+
+    -- ** CommitTransaction
+    CommitTransaction (CommitTransaction'),
+    newCommitTransaction,
+    CommitTransactionResponse (CommitTransactionResponse'),
+    newCommitTransactionResponse,
+
+    -- * Types
+
+    -- ** DecimalReturnType
+    DecimalReturnType (..),
+
+    -- ** TypeHint
+    TypeHint (..),
+
+    -- ** ArrayValue
+    ArrayValue (ArrayValue'),
+    newArrayValue,
+
+    -- ** ColumnMetadata
+    ColumnMetadata (ColumnMetadata'),
+    newColumnMetadata,
+
+    -- ** Field
+    Field (Field'),
+    newField,
+
+    -- ** ResultSetOptions
+    ResultSetOptions (ResultSetOptions'),
+    newResultSetOptions,
+
+    -- ** SqlParameter
+    SqlParameter (SqlParameter'),
+    newSqlParameter,
+
+    -- ** UpdateResult
+    UpdateResult (UpdateResult'),
+    newUpdateResult,
+  )
+where
+
+import Network.AWS.RDSData.BatchExecuteStatement
+import Network.AWS.RDSData.BeginTransaction
+import Network.AWS.RDSData.CommitTransaction
+import Network.AWS.RDSData.ExecuteStatement
+import Network.AWS.RDSData.Lens
+import Network.AWS.RDSData.RollbackTransaction
+import Network.AWS.RDSData.Types
+import Network.AWS.RDSData.Waiters
+
+-- $errors
+-- Error matchers are designed for use with the functions provided by
+-- <http://hackage.haskell.org/package/lens/docs/Control-Exception-Lens.html Control.Exception.Lens>.
+-- This allows catching (and rethrowing) service specific errors returned
+-- by 'RDSData'.
+
+-- $operations
+-- Some AWS operations return results that are incomplete and require subsequent
+-- requests in order to obtain the entire result set. The process of sending
+-- subsequent requests to continue where a previous request left off is called
+-- pagination. For example, the 'ListObjects' operation of Amazon S3 returns up to
+-- 1000 objects at a time, and you must send subsequent requests with the
+-- appropriate Marker in order to retrieve the next page of results.
+--
+-- Operations that have an 'AWSPager' instance can transparently perform subsequent
+-- requests, correctly setting Markers and other request facets to iterate through
+-- the entire result set of a truncated API operation. Operations which support
+-- this have an additional note in the documentation.
+--
+-- Many operations have the ability to filter results on the server side. See the
+-- individual operation parameters for details.
+
+-- $waiters
+-- Waiters poll by repeatedly sending a request until some remote success condition
+-- configured by the 'Wait' specification is fulfilled. The 'Wait' specification
+-- determines how many attempts should be made, in addition to delay and retry strategies.
---
+++ lib/services/amazonka-rds-data/test/Test/AWS/Gen/RDSData.hs
@@ -0,0 +1,138 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Test.AWS.Gen.RDSData
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Test.AWS.Gen.RDSData where
+
+import Data.Proxy
+import Network.AWS.RDSData
+import Test.AWS.Fixture
+import Test.AWS.Prelude
+import Test.AWS.RDSData.Internal
+import Test.Tasty
+
+-- Auto-generated: the actual test selection needs to be manually placed into
+-- the top-level so that real test data can be incrementally added.
+--
+-- This commented snippet is what the entire set should look like:
+
+-- fixtures :: TestTree
+-- fixtures =
+--     [ testGroup "request"
+--         [ requestRollbackTransaction $
+--             newRollbackTransaction
+--
+--         , requestBeginTransaction $
+--             newBeginTransaction
+--
+--         , requestBatchExecuteStatement $
+--             newBatchExecuteStatement
+--
+--         , requestExecuteStatement $
+--             newExecuteStatement
+--
+--         , requestCommitTransaction $
+--             newCommitTransaction
+--
+--           ]
+
+--     , testGroup "response"
+--         [ responseRollbackTransaction $
+--             newRollbackTransactionResponse
+--
+--         , responseBeginTransaction $
+--             newBeginTransactionResponse
+--
+--         , responseBatchExecuteStatement $
+--             newBatchExecuteStatementResponse
+--
+--         , responseExecuteStatement $
+--             newExecuteStatementResponse
+--
+--         , responseCommitTransaction $
+--             newCommitTransactionResponse
+--
+--           ]
+--     ]
+
+-- Requests
+
+requestRollbackTransaction :: RollbackTransaction -> TestTree
+requestRollbackTransaction =
+  req
+    "RollbackTransaction"
+    "fixture/RollbackTransaction.yaml"
+
+requestBeginTransaction :: BeginTransaction -> TestTree
+requestBeginTransaction =
+  req
+    "BeginTransaction"
+    "fixture/BeginTransaction.yaml"
+
+requestBatchExecuteStatement :: BatchExecuteStatement -> TestTree
+requestBatchExecuteStatement =
+  req
+    "BatchExecuteStatement"
+    "fixture/BatchExecuteStatement.yaml"
+
+requestExecuteStatement :: ExecuteStatement -> TestTree
+requestExecuteStatement =
+  req
+    "ExecuteStatement"
+    "fixture/ExecuteStatement.yaml"
+
+requestCommitTransaction :: CommitTransaction -> TestTree
+requestCommitTransaction =
+  req
+    "CommitTransaction"
+    "fixture/CommitTransaction.yaml"
+
+-- Responses
+
+responseRollbackTransaction :: RollbackTransactionResponse -> TestTree
+responseRollbackTransaction =
+  res
+    "RollbackTransactionResponse"
+    "fixture/RollbackTransactionResponse.proto"
+    defaultService
+    (Proxy :: Proxy RollbackTransaction)
+
+responseBeginTransaction :: BeginTransactionResponse -> TestTree
+responseBeginTransaction =
+  res
+    "BeginTransactionResponse"
+    "fixture/BeginTransactionResponse.proto"
+    defaultService
+    (Proxy :: Proxy BeginTransaction)
+
+responseBatchExecuteStatement :: BatchExecuteStatementResponse -> TestTree
+responseBatchExecuteStatement =
+  res
+    "BatchExecuteStatementResponse"
+    "fixture/BatchExecuteStatementResponse.proto"
+    defaultService
+    (Proxy :: Proxy BatchExecuteStatement)
+
+responseExecuteStatement :: ExecuteStatementResponse -> TestTree
+responseExecuteStatement =
+  res
+    "ExecuteStatementResponse"
+    "fixture/ExecuteStatementResponse.proto"
+    defaultService
+    (Proxy :: Proxy ExecuteStatement)
+
+responseCommitTransaction :: CommitTransactionResponse -> TestTree
+responseCommitTransaction =
+  res
+    "CommitTransactionResponse"
+    "fixture/CommitTransactionResponse.proto"
+    defaultService
+    (Proxy :: Proxy CommitTransaction)

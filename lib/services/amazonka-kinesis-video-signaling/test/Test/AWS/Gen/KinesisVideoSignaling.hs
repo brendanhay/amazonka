f6491363a6ef91bae38bfce3e7da9fb6b59bce15
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-27 11:59:52 +0200

    amazonka-kinesis-video-signaling: regenerating service

---
+++ lib/services/amazonka-kinesis-video-signaling/test/Test/AWS/Gen/KinesisVideoSignaling.hs
@@ -0,0 +1,78 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Test.AWS.Gen.KinesisVideoSignaling
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Test.AWS.Gen.KinesisVideoSignaling where
+
+import Data.Proxy
+import Network.AWS.KinesisVideoSignaling
+import Test.AWS.Fixture
+import Test.AWS.KinesisVideoSignaling.Internal
+import Test.AWS.Prelude
+import Test.Tasty
+
+-- Auto-generated: the actual test selection needs to be manually placed into
+-- the top-level so that real test data can be incrementally added.
+--
+-- This commented snippet is what the entire set should look like:
+
+-- fixtures :: TestTree
+-- fixtures =
+--     [ testGroup "request"
+--         [ requestSendAlexaOfferToMaster $
+--             newSendAlexaOfferToMaster
+--
+--         , requestGetIceServerConfig $
+--             newGetIceServerConfig
+--
+--           ]
+
+--     , testGroup "response"
+--         [ responseSendAlexaOfferToMaster $
+--             newSendAlexaOfferToMasterResponse
+--
+--         , responseGetIceServerConfig $
+--             newGetIceServerConfigResponse
+--
+--           ]
+--     ]
+
+-- Requests
+
+requestSendAlexaOfferToMaster :: SendAlexaOfferToMaster -> TestTree
+requestSendAlexaOfferToMaster =
+  req
+    "SendAlexaOfferToMaster"
+    "fixture/SendAlexaOfferToMaster.yaml"
+
+requestGetIceServerConfig :: GetIceServerConfig -> TestTree
+requestGetIceServerConfig =
+  req
+    "GetIceServerConfig"
+    "fixture/GetIceServerConfig.yaml"
+
+-- Responses
+
+responseSendAlexaOfferToMaster :: SendAlexaOfferToMasterResponse -> TestTree
+responseSendAlexaOfferToMaster =
+  res
+    "SendAlexaOfferToMasterResponse"
+    "fixture/SendAlexaOfferToMasterResponse.proto"
+    defaultService
+    (Proxy :: Proxy SendAlexaOfferToMaster)
+
+responseGetIceServerConfig :: GetIceServerConfigResponse -> TestTree
+responseGetIceServerConfig =
+  res
+    "GetIceServerConfigResponse"
+    "fixture/GetIceServerConfigResponse.proto"
+    defaultService
+    (Proxy :: Proxy GetIceServerConfig)

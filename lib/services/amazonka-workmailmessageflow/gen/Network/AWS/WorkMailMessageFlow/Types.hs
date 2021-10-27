fd16c603aaaf3035c5952cdce4dd53c09f6d40cc
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-27 12:00:11 +0200

    amazonka-workmailmessageflow: regenerating service

---
+++ lib/services/amazonka-workmailmessageflow/gen/Network/AWS/WorkMailMessageFlow/Types.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.WorkMailMessageFlow.Types
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Network.AWS.WorkMailMessageFlow.Types
+  ( -- * Service Configuration
+    defaultService,
+
+    -- * Errors
+    _MessageRejected,
+    _InvalidContentLocation,
+    _ResourceNotFoundException,
+    _MessageFrozen,
+
+    -- * RawMessageContent
+    RawMessageContent (..),
+    newRawMessageContent,
+    rawMessageContent_s3Reference,
+
+    -- * S3Reference
+    S3Reference (..),
+    newS3Reference,
+    s3Reference_objectVersion,
+    s3Reference_bucket,
+    s3Reference_key,
+  )
+where
+
+import qualified Network.AWS.Core as Core
+import qualified Network.AWS.Lens as Lens
+import qualified Network.AWS.Prelude as Prelude
+import qualified Network.AWS.Sign.V4 as Sign
+import Network.AWS.WorkMailMessageFlow.Types.RawMessageContent
+import Network.AWS.WorkMailMessageFlow.Types.S3Reference
+
+-- | API version @2019-05-01@ of the Amazon WorkMail Message Flow SDK configuration.
+defaultService :: Core.Service
+defaultService =
+  Core.Service
+    { Core._serviceAbbrev =
+        "WorkMailMessageFlow",
+      Core._serviceSigner = Sign.v4,
+      Core._serviceEndpointPrefix = "workmailmessageflow",
+      Core._serviceSigningName = "workmailmessageflow",
+      Core._serviceVersion = "2019-05-01",
+      Core._serviceEndpoint =
+        Core.defaultEndpoint defaultService,
+      Core._serviceTimeout = Prelude.Just 70,
+      Core._serviceCheck = Core.statusSuccess,
+      Core._serviceError =
+        Core.parseJSONError "WorkMailMessageFlow",
+      Core._serviceRetry = retry
+    }
+  where
+    retry =
+      Core.Exponential
+        { Core._retryBase = 5.0e-2,
+          Core._retryGrowth = 2,
+          Core._retryAttempts = 5,
+          Core._retryCheck = check
+        }
+    check e
+      | Lens.has
+          ( Core.hasCode "ThrottledException"
+              Prelude.. Core.hasStatus 400
+          )
+          e =
+        Prelude.Just "throttled_exception"
+      | Lens.has (Core.hasStatus 429) e =
+        Prelude.Just "too_many_requests"
+      | Lens.has
+          ( Core.hasCode "ThrottlingException"
+              Prelude.. Core.hasStatus 400
+          )
+          e =
+        Prelude.Just "throttling_exception"
+      | Lens.has
+          ( Core.hasCode "Throttling"
+              Prelude.. Core.hasStatus 400
+          )
+          e =
+        Prelude.Just "throttling"
+      | Lens.has
+          ( Core.hasCode
+              "ProvisionedThroughputExceededException"
+              Prelude.. Core.hasStatus 400
+          )
+          e =
+        Prelude.Just "throughput_exceeded"
+      | Lens.has (Core.hasStatus 504) e =
+        Prelude.Just "gateway_timeout"
+      | Lens.has
+          ( Core.hasCode "RequestThrottledException"
+              Prelude.. Core.hasStatus 400
+          )
+          e =
+        Prelude.Just "request_throttled_exception"
+      | Lens.has (Core.hasStatus 502) e =
+        Prelude.Just "bad_gateway"
+      | Lens.has (Core.hasStatus 503) e =
+        Prelude.Just "service_unavailable"
+      | Lens.has (Core.hasStatus 500) e =
+        Prelude.Just "general_server_error"
+      | Lens.has (Core.hasStatus 509) e =
+        Prelude.Just "limit_exceeded"
+      | Prelude.otherwise = Prelude.Nothing
+
+-- | The requested email could not be updated due to an error in the MIME
+-- content. Check the error message for more information about what caused
+-- the error.
+_MessageRejected :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_MessageRejected =
+  Core._MatchServiceError
+    defaultService
+    "MessageRejected"
+
+-- | WorkMail could not access the updated email content. Possible reasons:
+--
+-- -   You made the request in a region other than your S3 bucket region.
+--
+-- -   The
+--     <https://docs.aws.amazon.com/AmazonS3/latest/dev/bucket-owner-condition.html S3 bucket owner>
+--     is not the same as the calling AWS account.
+--
+-- -   You have an incomplete or missing S3 bucket policy. For more
+--     information about policies, see
+--     <https://docs.aws.amazon.com/workmail/latest/adminguide/update-with-lambda.html Updating message content with AWS Lambda>
+--     in the /WorkMail Administrator Guide/.
+_InvalidContentLocation :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_InvalidContentLocation =
+  Core._MatchServiceError
+    defaultService
+    "InvalidContentLocation"
+
+-- | The requested email message is not found.
+_ResourceNotFoundException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ResourceNotFoundException =
+  Core._MatchServiceError
+    defaultService
+    "ResourceNotFoundException"
+    Prelude.. Core.hasStatus 404
+
+-- | The requested email is not eligible for update. This is usually the case
+-- for a redirected email.
+_MessageFrozen :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_MessageFrozen =
+  Core._MatchServiceError
+    defaultService
+    "MessageFrozen"

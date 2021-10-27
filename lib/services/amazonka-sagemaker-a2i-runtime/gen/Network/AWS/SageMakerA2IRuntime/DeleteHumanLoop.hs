857137ed73df092f66d6572b0d69e8f0002bf367
Brendan Hay <brendan.g.hay@gmail.com>
2021-10-27 12:00:04 +0200

    amazonka-sagemaker-a2i-runtime: regenerating service

---
+++ lib/services/amazonka-sagemaker-a2i-runtime/gen/Network/AWS/SageMakerA2IRuntime/DeleteHumanLoop.hs
@@ -0,0 +1,140 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-unused-matches #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Network.AWS.SageMakerA2IRuntime.DeleteHumanLoop
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+--
+-- Deletes the specified human loop for a flow definition.
+--
+-- If the human loop was deleted, this operation will return a
+-- @ResourceNotFoundException@.
+module Network.AWS.SageMakerA2IRuntime.DeleteHumanLoop
+  ( -- * Creating a Request
+    DeleteHumanLoop (..),
+    newDeleteHumanLoop,
+
+    -- * Request Lenses
+    deleteHumanLoop_humanLoopName,
+
+    -- * Destructuring the Response
+    DeleteHumanLoopResponse (..),
+    newDeleteHumanLoopResponse,
+
+    -- * Response Lenses
+    deleteHumanLoopResponse_httpStatus,
+  )
+where
+
+import qualified Network.AWS.Core as Core
+import qualified Network.AWS.Lens as Lens
+import qualified Network.AWS.Prelude as Prelude
+import qualified Network.AWS.Request as Request
+import qualified Network.AWS.Response as Response
+import Network.AWS.SageMakerA2IRuntime.Types
+
+-- | /See:/ 'newDeleteHumanLoop' smart constructor.
+data DeleteHumanLoop = DeleteHumanLoop'
+  { -- | The name of the human loop that you want to delete.
+    humanLoopName :: Prelude.Text
+  }
+  deriving (Prelude.Eq, Prelude.Read, Prelude.Show, Prelude.Generic)
+
+-- |
+-- Create a value of 'DeleteHumanLoop' with all optional fields omitted.
+--
+-- Use <https://hackage.haskell.org/package/generic-lens generic-lens> or <https://hackage.haskell.org/package/optics optics> to modify other optional fields.
+--
+-- The following record fields are available, with the corresponding lenses provided
+-- for backwards compatibility:
+--
+-- 'humanLoopName', 'deleteHumanLoop_humanLoopName' - The name of the human loop that you want to delete.
+newDeleteHumanLoop ::
+  -- | 'humanLoopName'
+  Prelude.Text ->
+  DeleteHumanLoop
+newDeleteHumanLoop pHumanLoopName_ =
+  DeleteHumanLoop' {humanLoopName = pHumanLoopName_}
+
+-- | The name of the human loop that you want to delete.
+deleteHumanLoop_humanLoopName :: Lens.Lens' DeleteHumanLoop Prelude.Text
+deleteHumanLoop_humanLoopName = Lens.lens (\DeleteHumanLoop' {humanLoopName} -> humanLoopName) (\s@DeleteHumanLoop' {} a -> s {humanLoopName = a} :: DeleteHumanLoop)
+
+instance Core.AWSRequest DeleteHumanLoop where
+  type
+    AWSResponse DeleteHumanLoop =
+      DeleteHumanLoopResponse
+  request = Request.delete defaultService
+  response =
+    Response.receiveEmpty
+      ( \s h x ->
+          DeleteHumanLoopResponse'
+            Prelude.<$> (Prelude.pure (Prelude.fromEnum s))
+      )
+
+instance Prelude.Hashable DeleteHumanLoop
+
+instance Prelude.NFData DeleteHumanLoop
+
+instance Core.ToHeaders DeleteHumanLoop where
+  toHeaders =
+    Prelude.const
+      ( Prelude.mconcat
+          [ "Content-Type"
+              Core.=# ( "application/x-amz-json-1.1" ::
+                          Prelude.ByteString
+                      )
+          ]
+      )
+
+instance Core.ToPath DeleteHumanLoop where
+  toPath DeleteHumanLoop' {..} =
+    Prelude.mconcat
+      ["/human-loops/", Core.toBS humanLoopName]
+
+instance Core.ToQuery DeleteHumanLoop where
+  toQuery = Prelude.const Prelude.mempty
+
+-- | /See:/ 'newDeleteHumanLoopResponse' smart constructor.
+data DeleteHumanLoopResponse = DeleteHumanLoopResponse'
+  { -- | The response's http status code.
+    httpStatus :: Prelude.Int
+  }
+  deriving (Prelude.Eq, Prelude.Read, Prelude.Show, Prelude.Generic)
+
+-- |
+-- Create a value of 'DeleteHumanLoopResponse' with all optional fields omitted.
+--
+-- Use <https://hackage.haskell.org/package/generic-lens generic-lens> or <https://hackage.haskell.org/package/optics optics> to modify other optional fields.
+--
+-- The following record fields are available, with the corresponding lenses provided
+-- for backwards compatibility:
+--
+-- 'httpStatus', 'deleteHumanLoopResponse_httpStatus' - The response's http status code.
+newDeleteHumanLoopResponse ::
+  -- | 'httpStatus'
+  Prelude.Int ->
+  DeleteHumanLoopResponse
+newDeleteHumanLoopResponse pHttpStatus_ =
+  DeleteHumanLoopResponse' {httpStatus = pHttpStatus_}
+
+-- | The response's http status code.
+deleteHumanLoopResponse_httpStatus :: Lens.Lens' DeleteHumanLoopResponse Prelude.Int
+deleteHumanLoopResponse_httpStatus = Lens.lens (\DeleteHumanLoopResponse' {httpStatus} -> httpStatus) (\s@DeleteHumanLoopResponse' {} a -> s {httpStatus = a} :: DeleteHumanLoopResponse)
+
+instance Prelude.NFData DeleteHumanLoopResponse
---
+++ lib/services/amazonka-sagemaker-a2i-runtime/test/Test/AWS/Gen/SageMakerA2IRuntime.hs
@@ -0,0 +1,138 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+-- Derived from AWS service descriptions, licensed under Apache 2.0.
+
+-- |
+-- Module      : Test.AWS.Gen.SageMakerA2IRuntime
+-- Copyright   : (c) 2013-2021 Brendan Hay
+-- License     : Mozilla Public License, v. 2.0.
+-- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Stability   : auto-generated
+-- Portability : non-portable (GHC extensions)
+module Test.AWS.Gen.SageMakerA2IRuntime where
+
+import Data.Proxy
+import Network.AWS.SageMakerA2IRuntime
+import Test.AWS.Fixture
+import Test.AWS.Prelude
+import Test.AWS.SageMakerA2IRuntime.Internal
+import Test.Tasty
+
+-- Auto-generated: the actual test selection needs to be manually placed into
+-- the top-level so that real test data can be incrementally added.
+--
+-- This commented snippet is what the entire set should look like:
+
+-- fixtures :: TestTree
+-- fixtures =
+--     [ testGroup "request"
+--         [ requestListHumanLoops $
+--             newListHumanLoops
+--
+--         , requestDeleteHumanLoop $
+--             newDeleteHumanLoop
+--
+--         , requestStartHumanLoop $
+--             newStartHumanLoop
+--
+--         , requestStopHumanLoop $
+--             newStopHumanLoop
+--
+--         , requestDescribeHumanLoop $
+--             newDescribeHumanLoop
+--
+--           ]
+
+--     , testGroup "response"
+--         [ responseListHumanLoops $
+--             newListHumanLoopsResponse
+--
+--         , responseDeleteHumanLoop $
+--             newDeleteHumanLoopResponse
+--
+--         , responseStartHumanLoop $
+--             newStartHumanLoopResponse
+--
+--         , responseStopHumanLoop $
+--             newStopHumanLoopResponse
+--
+--         , responseDescribeHumanLoop $
+--             newDescribeHumanLoopResponse
+--
+--           ]
+--     ]
+
+-- Requests
+
+requestListHumanLoops :: ListHumanLoops -> TestTree
+requestListHumanLoops =
+  req
+    "ListHumanLoops"
+    "fixture/ListHumanLoops.yaml"
+
+requestDeleteHumanLoop :: DeleteHumanLoop -> TestTree
+requestDeleteHumanLoop =
+  req
+    "DeleteHumanLoop"
+    "fixture/DeleteHumanLoop.yaml"
+
+requestStartHumanLoop :: StartHumanLoop -> TestTree
+requestStartHumanLoop =
+  req
+    "StartHumanLoop"
+    "fixture/StartHumanLoop.yaml"
+
+requestStopHumanLoop :: StopHumanLoop -> TestTree
+requestStopHumanLoop =
+  req
+    "StopHumanLoop"
+    "fixture/StopHumanLoop.yaml"
+
+requestDescribeHumanLoop :: DescribeHumanLoop -> TestTree
+requestDescribeHumanLoop =
+  req
+    "DescribeHumanLoop"
+    "fixture/DescribeHumanLoop.yaml"
+
+-- Responses
+
+responseListHumanLoops :: ListHumanLoopsResponse -> TestTree
+responseListHumanLoops =
+  res
+    "ListHumanLoopsResponse"
+    "fixture/ListHumanLoopsResponse.proto"
+    defaultService
+    (Proxy :: Proxy ListHumanLoops)
+
+responseDeleteHumanLoop :: DeleteHumanLoopResponse -> TestTree
+responseDeleteHumanLoop =
+  res
+    "DeleteHumanLoopResponse"
+    "fixture/DeleteHumanLoopResponse.proto"
+    defaultService
+    (Proxy :: Proxy DeleteHumanLoop)
+
+responseStartHumanLoop :: StartHumanLoopResponse -> TestTree
+responseStartHumanLoop =
+  res
+    "StartHumanLoopResponse"
+    "fixture/StartHumanLoopResponse.proto"
+    defaultService
+    (Proxy :: Proxy StartHumanLoop)
+
+responseStopHumanLoop :: StopHumanLoopResponse -> TestTree
+responseStopHumanLoop =
+  res
+    "StopHumanLoopResponse"
+    "fixture/StopHumanLoopResponse.proto"
+    defaultService
+    (Proxy :: Proxy StopHumanLoop)
+
+responseDescribeHumanLoop :: DescribeHumanLoopResponse -> TestTree
+responseDescribeHumanLoop =
+  res
+    "DescribeHumanLoopResponse"
+    "fixture/DescribeHumanLoopResponse.proto"
+    defaultService
+    (Proxy :: Proxy DescribeHumanLoop)

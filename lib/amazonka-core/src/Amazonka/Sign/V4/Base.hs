335be358ea47db1f6764d5f5cf3c890211651c21
Alex Mason <Axman6@gmail.com>
2022-01-06 22:24:50 +1100

    Use Builders in canonicalHeaders

--- lib/amazonka-core/src/Amazonka/Sign/V4/Base.hs
+++ lib/amazonka-core/src/Amazonka/Sign/V4/Base.hs
@@ -15,7 +15,9 @@ import Amazonka.Prelude
 import Amazonka.Request
 import Amazonka.Types
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder as BSB
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.CaseInsensitive as CI
 import qualified Data.Foldable as Foldable
 import qualified Data.Map.Strict as Map
@@ -272,9 +274,9 @@ canonicalQuery = Tag . toBS
 -- all internal whitespace, replacing with a single space char,
 -- unless quoted with \"...\"
 canonicalHeaders :: NormalisedHeaders -> CanonicalHeaders
-canonicalHeaders = Tag . Foldable.foldMap (uncurry f) . untag
+canonicalHeaders = Tag . BSL.toStrict . BSB.toLazyByteString . Foldable.foldMap (uncurry f) . untag
   where
-    f k v = k <> ":" <> stripBS v <> "\n"
+    f k v = BSB.byteString k <> BSB.char7 ':' <> BSB.byteString (stripBS v) <> BSB.char7 '\n'
 
 signedHeaders :: NormalisedHeaders -> SignedHeaders
 signedHeaders = Tag . BS8.intercalate ";" . map fst . untag

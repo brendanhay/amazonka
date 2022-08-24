2a39b7e4c8701abf1b1a1d7e23d153276a91da0a
Jack Kelly <jack@jackkelly.name>
2022-08-25 09:23:28 +1000

    amazonka-dynamodb-streams: regenerating service

--- lib/services/amazonka-dynamodb-streams/gen/Amazonka/DynamoDBStreams/Types.hs
+++ lib/services/amazonka-dynamodb-streams/gen/Amazonka/DynamoDBStreams/Types.hs
@@ -212,17 +212,17 @@ _ResourceNotFoundException =
 -- | There is no limit to the number of daily on-demand backups that can be
 -- taken.
 --
--- Up to 50 simultaneous table operations are allowed per account. These
+-- Up to 500 simultaneous table operations are allowed per account. These
 -- operations include @CreateTable@, @UpdateTable@,
 -- @DeleteTable@,@UpdateTimeToLive@, @RestoreTableFromBackup@, and
 -- @RestoreTableToPointInTime@.
 --
 -- The only exception is when you are creating a table with one or more
--- secondary indexes. You can have up to 25 such requests running at a
+-- secondary indexes. You can have up to 250 such requests running at a
 -- time; however, if the table or index specifications are complex,
 -- DynamoDB might temporarily reduce the number of concurrent operations.
 --
--- There is a soft account quota of 256 tables.
+-- There is a soft account quota of 2,500 tables.
 _LimitExceededException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
 _LimitExceededException =
   Core._MatchServiceError

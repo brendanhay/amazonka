23c486ae8f293bd870e8ca49d6d4cbf87a7efa29
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:52 +1000

    amazonka-sso: regenerating service

--- lib/services/amazonka-sso/gen/Amazonka/SSO/GetRoleCredentials.hs
+++ lib/services/amazonka-sso/gen/Amazonka/SSO/GetRoleCredentials.hs
@@ -116,7 +116,8 @@ instance Core.AWSRequest GetRoleCredentials where
   type
     AWSResponse GetRoleCredentials =
       GetRoleCredentialsResponse
-  request = Request.get defaultService
+  service _ = defaultService
+  request srv = Request.get srv
   response =
     Response.receiveJSON
       ( \s h x ->
--- lib/services/amazonka-sso/gen/Amazonka/SSO/Types.hs
+++ lib/services/amazonka-sso/gen/Amazonka/SSO/Types.hs
@@ -62,6 +62,8 @@ defaultService =
       Core._serviceEndpointPrefix = "portal.sso",
       Core._serviceSigningName = "awsssoportal",
       Core._serviceVersion = "2019-06-10",
+      Core._serviceS3AddressingStyle =
+        Core.S3AddressingStyleAuto,
       Core._serviceEndpoint =
         Core.defaultEndpoint defaultService,
       Core._serviceTimeout = Prelude.Just 70,

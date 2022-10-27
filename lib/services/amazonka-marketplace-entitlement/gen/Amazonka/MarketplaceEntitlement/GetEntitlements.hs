15d01916074a11d967c93f7e388249b90097ebe4
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:41 +1000

    amazonka-marketplace-entitlement: regenerating service

--- lib/services/amazonka-marketplace-entitlement/gen/Amazonka/MarketplaceEntitlement/GetEntitlements.hs
+++ lib/services/amazonka-marketplace-entitlement/gen/Amazonka/MarketplaceEntitlement/GetEntitlements.hs
@@ -163,7 +163,8 @@ instance Core.AWSRequest GetEntitlements where
   type
     AWSResponse GetEntitlements =
       GetEntitlementsResponse
-  request = Request.postJSON defaultService
+  service _ = defaultService
+  request srv = Request.postJSON srv
   response =
     Response.receiveJSON
       ( \s h x ->

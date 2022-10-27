35372f193753dc0c329d8a43632b3306a2e09d99
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:39 +1000

    amazonka-kinesis-video-signaling: regenerating service

--- lib/services/amazonka-kinesis-video-signaling/gen/Amazonka/KinesisVideoSignaling/SendAlexaOfferToMaster.hs
+++ lib/services/amazonka-kinesis-video-signaling/gen/Amazonka/KinesisVideoSignaling/SendAlexaOfferToMaster.hs
@@ -115,7 +115,8 @@ instance Core.AWSRequest SendAlexaOfferToMaster where
   type
     AWSResponse SendAlexaOfferToMaster =
       SendAlexaOfferToMasterResponse
-  request = Request.postJSON defaultService
+  service _ = defaultService
+  request srv = Request.postJSON srv
   response =
     Response.receiveJSON
       ( \s h x ->

da37452061bc04727b39262ce8deea9eaeab2e2a
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:49 +1000

    amazonka-route53-recovery-cluster: regenerating service

--- lib/services/amazonka-route53-recovery-cluster/gen/Amazonka/Route53RecoveryCluster/GetRoutingControlState.hs
+++ lib/services/amazonka-route53-recovery-cluster/gen/Amazonka/Route53RecoveryCluster/GetRoutingControlState.hs
@@ -111,7 +111,8 @@ instance Core.AWSRequest GetRoutingControlState where
   type
     AWSResponse GetRoutingControlState =
       GetRoutingControlStateResponse
-  request = Request.postJSON defaultService
+  service _ = defaultService
+  request srv = Request.postJSON srv
   response =
     Response.receiveJSON
       ( \s h x ->

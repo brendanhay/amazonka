c575597698a91396796de0cb6a56f7ba9dbc76f0
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:46 +1000

    amazonka-pinpoint-sms-voice: regenerating service

--- lib/services/amazonka-pinpoint-sms-voice/gen/Amazonka/PinpointSMSVoice/GetConfigurationSetEventDestinations.hs
+++ lib/services/amazonka-pinpoint-sms-voice/gen/Amazonka/PinpointSMSVoice/GetConfigurationSetEventDestinations.hs
@@ -86,7 +86,8 @@ instance
   type
     AWSResponse GetConfigurationSetEventDestinations =
       GetConfigurationSetEventDestinationsResponse
-  request = Request.get defaultService
+  service _ = defaultService
+  request srv = Request.get srv
   response =
     Response.receiveJSON
       ( \s h x ->

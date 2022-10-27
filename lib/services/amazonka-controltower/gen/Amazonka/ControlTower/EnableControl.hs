39dd4522a8feb79d2a22bafdff348d2519816fce
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:27 +1000

    amazonka-controltower: regenerating service

--- lib/services/amazonka-controltower/gen/Amazonka/ControlTower/EnableControl.hs
+++ lib/services/amazonka-controltower/gen/Amazonka/ControlTower/EnableControl.hs
@@ -103,7 +103,8 @@ instance Core.AWSRequest EnableControl where
   type
     AWSResponse EnableControl =
       EnableControlResponse
-  request = Request.postJSON defaultService
+  service _ = defaultService
+  request srv = Request.postJSON srv
   response =
     Response.receiveJSON
       ( \s h x ->

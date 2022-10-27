bf75a301ffefdf484d0d5f8f0a36c61183f5d29a
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:41 +1000

    amazonka-macie: regenerating service

--- lib/services/amazonka-macie/gen/Amazonka/Macie/DisassociateS3Resources.hs
+++ lib/services/amazonka-macie/gen/Amazonka/Macie/DisassociateS3Resources.hs
@@ -99,7 +99,8 @@ instance Core.AWSRequest DisassociateS3Resources where
   type
     AWSResponse DisassociateS3Resources =
       DisassociateS3ResourcesResponse
-  request = Request.postJSON defaultService
+  service _ = defaultService
+  request srv = Request.postJSON srv
   response =
     Response.receiveJSON
       ( \s h x ->

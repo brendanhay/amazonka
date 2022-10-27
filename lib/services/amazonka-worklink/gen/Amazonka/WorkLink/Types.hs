feda3d3ea443cc2f6bd8c1fe5fd9e92ce0f96b5a
Jack Kelly <jack@jackkelly.name>
2022-10-27 13:25:56 +1000

    amazonka-worklink: regenerating service

--- lib/services/amazonka-worklink/gen/Amazonka/WorkLink/Types.hs
+++ lib/services/amazonka-worklink/gen/Amazonka/WorkLink/Types.hs
@@ -40,6 +40,8 @@ defaultService =
       Core._serviceEndpointPrefix = "worklink",
       Core._serviceSigningName = "worklink",
       Core._serviceVersion = "2018-09-25",
+      Core._serviceS3AddressingStyle =
+        Core.S3AddressingStyleAuto,
       Core._serviceEndpoint =
         Core.defaultEndpoint defaultService,
       Core._serviceTimeout = Prelude.Just 70,

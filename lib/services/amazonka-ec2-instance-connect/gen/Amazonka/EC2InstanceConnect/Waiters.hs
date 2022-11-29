6d1d396e098835ad92012b644a6b4cb3e3b0f21a
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:19 +1000

    amazonka-ec2-instance-connect: regenerating service

--- lib/services/amazonka-ec2-instance-connect/gen/Amazonka/EC2InstanceConnect/Waiters.hs
+++ lib/services/amazonka-ec2-instance-connect/gen/Amazonka/EC2InstanceConnect/Waiters.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -16,7 +17,7 @@
 module Amazonka.EC2InstanceConnect.Waiters where
 
 import qualified Amazonka.Core as Core
+import qualified Amazonka.Core.Lens.Internal as Lens
 import Amazonka.EC2InstanceConnect.Lens
 import Amazonka.EC2InstanceConnect.Types
-import qualified Amazonka.Lens as Lens
 import qualified Amazonka.Prelude as Prelude

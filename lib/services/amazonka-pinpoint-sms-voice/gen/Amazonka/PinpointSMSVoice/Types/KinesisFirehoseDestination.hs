2a6c09eb94be892e186baae6e48ac0ff0e009fee
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:39 +1000

    amazonka-pinpoint-sms-voice: regenerating service

--- lib/services/amazonka-pinpoint-sms-voice/gen/Amazonka/PinpointSMSVoice/Types/KinesisFirehoseDestination.hs
+++ lib/services/amazonka-pinpoint-sms-voice/gen/Amazonka/PinpointSMSVoice/Types/KinesisFirehoseDestination.hs
@@ -20,7 +20,7 @@
 module Amazonka.PinpointSMSVoice.Types.KinesisFirehoseDestination where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import qualified Amazonka.Prelude as Prelude
 
 -- | An object that contains information about an event destination that

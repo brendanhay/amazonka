b130ebf8b1174185eb7bd8870356bf6b9f713981
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:45 +1000

    amazonka-serverlessrepo: regenerating service

--- lib/services/amazonka-serverlessrepo/gen/Amazonka/ServerlessApplicationRepository/Types/ApplicationSummary.hs
+++ lib/services/amazonka-serverlessrepo/gen/Amazonka/ServerlessApplicationRepository/Types/ApplicationSummary.hs
@@ -20,7 +20,7 @@
 module Amazonka.ServerlessApplicationRepository.Types.ApplicationSummary where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import qualified Amazonka.Prelude as Prelude
 
 -- | Summary of details about the application.
--- lib/services/amazonka-serverlessrepo/gen/Amazonka/ServerlessApplicationRepository/Types/ParameterValue.hs
+++ lib/services/amazonka-serverlessrepo/gen/Amazonka/ServerlessApplicationRepository/Types/ParameterValue.hs
@@ -20,7 +20,7 @@
 module Amazonka.ServerlessApplicationRepository.Types.ParameterValue where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import qualified Amazonka.Prelude as Prelude
 
 -- | Parameter value of the application.

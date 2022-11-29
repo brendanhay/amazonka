bc3c0f6d2a160a424a1b8817ef0a6794a56ff740
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:27 +1000

    amazonka-iot-jobs-dataplane: regenerating service

--- lib/services/amazonka-iot-jobs-dataplane/gen/Amazonka/IoTJobsData/DescribeJobExecution.hs
+++ lib/services/amazonka-iot-jobs-dataplane/gen/Amazonka/IoTJobsData/DescribeJobExecution.hs
@@ -43,8 +43,8 @@ module Amazonka.IoTJobsData.DescribeJobExecution
 where
 
 import qualified Amazonka.Core as Core
+import qualified Amazonka.Core.Lens.Internal as Lens
 import Amazonka.IoTJobsData.Types
-import qualified Amazonka.Lens as Lens
 import qualified Amazonka.Prelude as Prelude
 import qualified Amazonka.Request as Request
 import qualified Amazonka.Response as Response
@@ -124,8 +124,8 @@ instance Core.AWSRequest DescribeJobExecution where
   type
     AWSResponse DescribeJobExecution =
       DescribeJobExecutionResponse
-  service _ = defaultService
-  request srv = Request.get srv
+  request overrides =
+    Request.get (overrides defaultService)
   response =
     Response.receiveJSON
       ( \s h x ->

c3610c6abacf8e18ad63837654162a977751065c
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:37 +1000

    amazonka-mobile: regenerating service

--- lib/services/amazonka-mobile/gen/Amazonka/Mobile/DeleteProject.hs
+++ lib/services/amazonka-mobile/gen/Amazonka/Mobile/DeleteProject.hs
@@ -41,7 +41,7 @@ module Amazonka.Mobile.DeleteProject
 where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import Amazonka.Mobile.Types
 import qualified Amazonka.Prelude as Prelude
 import qualified Amazonka.Request as Request
@@ -80,8 +80,8 @@ instance Core.AWSRequest DeleteProject where
   type
     AWSResponse DeleteProject =
       DeleteProjectResponse
-  service _ = defaultService
-  request srv = Request.delete srv
+  request overrides =
+    Request.delete (overrides defaultService)
   response =
     Response.receiveJSON
       ( \s h x ->
--- lib/services/amazonka-mobile/gen/Amazonka/Mobile/ExportProject.hs
+++ lib/services/amazonka-mobile/gen/Amazonka/Mobile/ExportProject.hs
@@ -45,7 +45,7 @@ module Amazonka.Mobile.ExportProject
 where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import Amazonka.Mobile.Types
 import qualified Amazonka.Prelude as Prelude
 import qualified Amazonka.Request as Request
@@ -85,8 +85,8 @@ instance Core.AWSRequest ExportProject where
   type
     AWSResponse ExportProject =
       ExportProjectResponse
-  service _ = defaultService
-  request srv = Request.postJSON srv
+  request overrides =
+    Request.postJSON (overrides defaultService)
   response =
     Response.receiveJSON
       ( \s h x ->

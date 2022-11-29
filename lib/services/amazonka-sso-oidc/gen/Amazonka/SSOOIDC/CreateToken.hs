ba301371db0dc939a2fc940d4d7cd8f11f2d6109
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:48 +1000

    amazonka-sso-oidc: regenerating service

--- lib/services/amazonka-sso-oidc/gen/Amazonka/SSOOIDC/CreateToken.hs
+++ lib/services/amazonka-sso-oidc/gen/Amazonka/SSOOIDC/CreateToken.hs
@@ -53,7 +53,7 @@ module Amazonka.SSOOIDC.CreateToken
 where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import qualified Amazonka.Prelude as Prelude
 import qualified Amazonka.Request as Request
 import qualified Amazonka.Response as Response
@@ -226,8 +226,8 @@ createToken_grantType = Lens.lens (\CreateToken' {grantType} -> grantType) (\s@C
 
 instance Core.AWSRequest CreateToken where
   type AWSResponse CreateToken = CreateTokenResponse
-  service _ = defaultService
-  request srv = Request.postJSON srv
+  request overrides =
+    Request.postJSON (overrides defaultService)
   response =
     Response.receiveJSON
       ( \s h x ->

9acee4d921a2dcbb876cc2c853508ad697b82630
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:06 +1000

    amazonka-appconfigdata: regenerating service

--- lib/services/amazonka-appconfigdata/gen/Amazonka/AppConfigData/StartConfigurationSession.hs
+++ lib/services/amazonka-appconfigdata/gen/Amazonka/AppConfigData/StartConfigurationSession.hs
@@ -49,7 +49,7 @@ where
 
 import Amazonka.AppConfigData.Types
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import qualified Amazonka.Prelude as Prelude
 import qualified Amazonka.Request as Request
 import qualified Amazonka.Response as Response
@@ -129,8 +129,8 @@ instance Core.AWSRequest StartConfigurationSession where
   type
     AWSResponse StartConfigurationSession =
       StartConfigurationSessionResponse
-  service _ = defaultService
-  request srv = Request.postJSON srv
+  request overrides =
+    Request.postJSON (overrides defaultService)
   response =
     Response.receiveJSON
       ( \s h x ->

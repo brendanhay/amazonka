3d981818fba2838d029152be6b8a4bd33d13d003
Jack Kelly <jack@jackkelly.name>
2022-11-29 15:24:43 +1000

    amazonka-s3outposts: regenerating service

--- lib/services/amazonka-s3outposts/gen/Amazonka/S3Outposts/Types/Endpoint.hs
+++ lib/services/amazonka-s3outposts/gen/Amazonka/S3Outposts/Types/Endpoint.hs
@@ -20,7 +20,7 @@
 module Amazonka.S3Outposts.Types.Endpoint where
 
 import qualified Amazonka.Core as Core
-import qualified Amazonka.Lens as Lens
+import qualified Amazonka.Core.Lens.Internal as Lens
 import qualified Amazonka.Prelude as Prelude
 import Amazonka.S3Outposts.Types.EndpointAccessType
 import Amazonka.S3Outposts.Types.EndpointStatus

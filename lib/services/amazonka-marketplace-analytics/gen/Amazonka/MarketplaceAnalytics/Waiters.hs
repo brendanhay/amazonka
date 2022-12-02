5e848d1da1c8f6c1141383cac1b22651f3fd2171
Jack Kelly <jack@jackkelly.name>
2022-12-02 10:59:19 +1000

    amazonka-marketplace-analytics: regenerating service

--- lib/services/amazonka-marketplace-analytics/gen/Amazonka/MarketplaceAnalytics/Waiters.hs
+++ lib/services/amazonka-marketplace-analytics/gen/Amazonka/MarketplaceAnalytics/Waiters.hs
@@ -18,6 +18,7 @@ module Amazonka.MarketplaceAnalytics.Waiters where
 
 import qualified Amazonka.Core as Core
 import qualified Amazonka.Core.Lens.Internal as Lens
+import qualified Amazonka.Data as Data
 import Amazonka.MarketplaceAnalytics.Lens
 import Amazonka.MarketplaceAnalytics.Types
 import qualified Amazonka.Prelude as Prelude

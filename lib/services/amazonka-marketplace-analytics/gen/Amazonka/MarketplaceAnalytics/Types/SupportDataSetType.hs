0955ec87e52b087ac7f22f37068a3260f0707ce1
Jack Kelly <jack@jackkelly.name>
2023-01-11 21:14:06 +1000

    amazonka-marketplace-analytics: regenerating service

--- lib/services/amazonka-marketplace-analytics/gen/Amazonka/MarketplaceAnalytics/Types/SupportDataSetType.hs
+++ lib/services/amazonka-marketplace-analytics/gen/Amazonka/MarketplaceAnalytics/Types/SupportDataSetType.hs
@@ -12,7 +12,7 @@
 
 -- |
 -- Module      : Amazonka.MarketplaceAnalytics.Types.SupportDataSetType
--- Copyright   : (c) 2013-2022 Brendan Hay
+-- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated

598cf3bb0ea54c225ed005be719b5072d5c1171e
Jack Kelly <jack@jackkelly.name>
2023-01-11 21:14:30 +1000

    amazonka-s3outposts: regenerating service

--- lib/services/amazonka-s3outposts/gen/Amazonka/S3Outposts/Types.hs
+++ lib/services/amazonka-s3outposts/gen/Amazonka/S3Outposts/Types.hs
@@ -8,7 +8,7 @@
 
 -- |
 -- Module      : Amazonka.S3Outposts.Types
--- Copyright   : (c) 2013-2022 Brendan Hay
+-- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
@@ -132,7 +132,7 @@ defaultService =
       | Prelude.otherwise = Prelude.Nothing
 
 -- | Access was denied for this action.
-_AccessDeniedException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_AccessDeniedException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _AccessDeniedException =
   Core._MatchServiceError
     defaultService
@@ -140,7 +140,7 @@ _AccessDeniedException =
     Prelude.. Core.hasStatus 403
 
 -- | There was a conflict with this action, and it could not be completed.
-_ConflictException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ConflictException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ConflictException =
   Core._MatchServiceError
     defaultService
@@ -148,7 +148,7 @@ _ConflictException =
     Prelude.. Core.hasStatus 409
 
 -- | There was an exception with the internal server.
-_InternalServerException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_InternalServerException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _InternalServerException =
   Core._MatchServiceError
     defaultService
@@ -156,7 +156,7 @@ _InternalServerException =
     Prelude.. Core.hasStatus 500
 
 -- | The requested resource was not found.
-_ResourceNotFoundException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ResourceNotFoundException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ResourceNotFoundException =
   Core._MatchServiceError
     defaultService
@@ -164,7 +164,7 @@ _ResourceNotFoundException =
     Prelude.. Core.hasStatus 404
 
 -- | There was an exception validating this data.
-_ValidationException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ValidationException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ValidationException =
   Core._MatchServiceError
     defaultService

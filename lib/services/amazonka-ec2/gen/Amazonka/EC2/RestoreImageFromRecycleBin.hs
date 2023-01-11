6a2bef75391e9b8d0267b79151e8ab41d4e12c82
Jack Kelly <jack@jackkelly.name>
2023-01-11 21:13:18 +1000

    amazonka-ec2: regenerating service

--- lib/services/amazonka-ec2/gen/Amazonka/EC2/RestoreImageFromRecycleBin.hs
+++ lib/services/amazonka-ec2/gen/Amazonka/EC2/RestoreImageFromRecycleBin.hs
@@ -14,7 +14,7 @@
 
 -- |
 -- Module      : Amazonka.EC2.RestoreImageFromRecycleBin
--- Copyright   : (c) 2013-2022 Brendan Hay
+-- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
@@ -22,7 +22,7 @@
 --
 -- Restores an AMI from the Recycle Bin. For more information, see
 -- <https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/recycle-bin.html Recycle Bin>
--- in the Amazon Elastic Compute Cloud User Guide.
+-- in the /Amazon EC2 User Guide/.
 module Amazonka.EC2.RestoreImageFromRecycleBin
   ( -- * Creating a Request
     RestoreImageFromRecycleBin (..),

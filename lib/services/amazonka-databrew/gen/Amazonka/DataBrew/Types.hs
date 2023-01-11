f2293c30f262f1a5efb90ec7fd887342386ad259
Jack Kelly <jack@jackkelly.name>
2023-01-11 21:13:05 +1000

    amazonka-databrew: regenerating service

--- lib/services/amazonka-databrew/gen/Amazonka/DataBrew/Types.hs
+++ lib/services/amazonka-databrew/gen/Amazonka/DataBrew/Types.hs
@@ -8,7 +8,7 @@
 
 -- |
 -- Module      : Amazonka.DataBrew.Types
--- Copyright   : (c) 2013-2022 Brendan Hay
+-- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
@@ -622,7 +622,7 @@ defaultService =
       | Prelude.otherwise = Prelude.Nothing
 
 -- | Access to the specified resource was denied.
-_AccessDeniedException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_AccessDeniedException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _AccessDeniedException =
   Core._MatchServiceError
     defaultService
@@ -630,7 +630,7 @@ _AccessDeniedException =
     Prelude.. Core.hasStatus 403
 
 -- | Updating or deleting a resource can cause an inconsistent state.
-_ConflictException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ConflictException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ConflictException =
   Core._MatchServiceError
     defaultService
@@ -638,7 +638,7 @@ _ConflictException =
     Prelude.. Core.hasStatus 409
 
 -- | An internal service failure occurred.
-_InternalServerException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_InternalServerException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _InternalServerException =
   Core._MatchServiceError
     defaultService
@@ -646,7 +646,7 @@ _InternalServerException =
     Prelude.. Core.hasStatus 500
 
 -- | One or more resources can\'t be found.
-_ResourceNotFoundException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ResourceNotFoundException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ResourceNotFoundException =
   Core._MatchServiceError
     defaultService
@@ -654,7 +654,7 @@ _ResourceNotFoundException =
     Prelude.. Core.hasStatus 404
 
 -- | A service quota is exceeded.
-_ServiceQuotaExceededException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ServiceQuotaExceededException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ServiceQuotaExceededException =
   Core._MatchServiceError
     defaultService
@@ -662,7 +662,7 @@ _ServiceQuotaExceededException =
     Prelude.. Core.hasStatus 402
 
 -- | The input parameters for this request failed validation.
-_ValidationException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ValidationException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ValidationException =
   Core._MatchServiceError
     defaultService

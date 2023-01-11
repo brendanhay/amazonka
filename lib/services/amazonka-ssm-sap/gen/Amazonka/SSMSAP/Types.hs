f9c464da980a322f3082ce007735eb1d5a519fc3
Jack Kelly <jack@jackkelly.name>
2023-01-11 21:14:37 +1000

    amazonka-ssm-sap: regenerating service

--- lib/services/amazonka-ssm-sap/gen/Amazonka/SSMSAP/Types.hs
+++ lib/services/amazonka-ssm-sap/gen/Amazonka/SSMSAP/Types.hs
@@ -8,7 +8,7 @@
 
 -- |
 -- Module      : Amazonka.SSMSAP.Types
--- Copyright   : (c) 2013-2022 Brendan Hay
+-- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
 -- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
 -- Stability   : auto-generated
@@ -245,7 +245,7 @@ defaultService =
       | Prelude.otherwise = Prelude.Nothing
 
 -- |
-_ConflictException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ConflictException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ConflictException =
   Core._MatchServiceError
     defaultService
@@ -253,7 +253,7 @@ _ConflictException =
     Prelude.. Core.hasStatus 409
 
 -- |
-_InternalServerException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_InternalServerException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _InternalServerException =
   Core._MatchServiceError
     defaultService
@@ -261,7 +261,7 @@ _InternalServerException =
     Prelude.. Core.hasStatus 500
 
 -- |
-_ResourceNotFoundException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ResourceNotFoundException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ResourceNotFoundException =
   Core._MatchServiceError
     defaultService
@@ -269,7 +269,7 @@ _ResourceNotFoundException =
     Prelude.. Core.hasStatus 404
 
 -- |
-_ValidationException :: Core.AsError a => Lens.Getting (Prelude.First Core.ServiceError) a Core.ServiceError
+_ValidationException :: Core.AsError a => Lens.Fold a Core.ServiceError
 _ValidationException =
   Core._MatchServiceError
     defaultService

47781c9b8d57a866a425d3f4c4c89ad6ca98fdaf
Brendan Hay <brendan.g.hay@gmail.com>
2023-03-24 08:44:16 +0100

    build: move botocore dependency into flake.nix and update scripts

--- flake.nix
+++ flake.nix
@@ -13,22 +13,32 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+
+    botocore = {
+      url = "github:boto/botocore";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, botocore }:
     flake-utils.lib.eachDefaultSystem (system:
       let
 
         pre-commit = pre-commit-hooks.lib.${system}.run {
           src = self;
           hooks = {
+            cabal-fmt.enable = true;
             nixpkgs-fmt.enable = true;
+            ormolu.enable = true;
             shellcheck.enable = true;
             shfmt.enable = true;
           };
         };
 
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowBroken = true;
+        };
 
         renameVersion = version: "ghc" + (pkgs.lib.replaceStrings [ "." ] [ "" ] version);
 
@@ -67,17 +77,51 @@
           shellHook = pre-commit.shellHook;
         };
 
+        amazonka-gen =
+          pkgs.haskellPackages.developPackage {
+            root = ./gen;
+            overrides = _hsFinal: hsPrev: {
+              ede = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.dontHaddock hsPrev.ede);
+            };
+          };
+
+        botocore-data = pkgs.stdenvNoCC.mkDerivation {
+          name = "botocore";
+          src = botocore;
+          phases = [ "installPhase" ];
+          installPhase = ''
+            cp -R $src/botocore/data $out
+          '';
+        };
+
       in
       {
+        apps = {
+          gen = {
+            type = "app";
+            program = "${amazonka-gen}/bin/gen";
+          };
+
+          gen-configs = {
+            type = "app";
+            program = "${amazonka-gen}/bin/gen-configs";
+          };
+        };
+
+        checks = {
+          inherit pre-commit;
+        };
+
+        packages = {
+          botocore = botocore-data;
+          default = amazonka-gen;
+        };
+
         devShells = {
           ghc902 = mkDevShell ghc902;
           ghc944 = mkDevShell ghc944;
           ghc961 = mkDevShell ghc961;
           default = mkDevShell ghc927;
-        };
-
-        checks = {
-          inherit pre-commit;
         };
       });
 }

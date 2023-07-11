75fc5bdbefc5a38d0a53f64fc6d973b9cf923b24
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:54 +1200

    amazonka-servicecatalog: regenerating service

--- lib/services/amazonka-servicecatalog/gen/Amazonka/ServiceCatalog/Types/SourceConnectionDetail.hs
+++ lib/services/amazonka-servicecatalog/gen/Amazonka/ServiceCatalog/Types/SourceConnectionDetail.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.ServiceCatalog.Types.SourceConnectionDetail
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.ServiceCatalog.Types.SourceConnectionDetail where

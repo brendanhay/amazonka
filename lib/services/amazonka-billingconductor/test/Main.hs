47d28d92f24513ffaca78c5e7d86c60442e3e3fb
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:04 +1200

    amazonka-billingconductor: regenerating service

--- lib/services/amazonka-billingconductor/test/Main.hs
+++ lib/services/amazonka-billingconductor/test/Main.hs
@@ -4,7 +4,7 @@
 -- Module      : Main
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Main (main) where

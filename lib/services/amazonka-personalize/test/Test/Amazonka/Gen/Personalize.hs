b61d1751eed4ac0c7fab2675e073360648af3079
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:44 +1200

    amazonka-personalize: regenerating service

--- lib/services/amazonka-personalize/test/Test/Amazonka/Gen/Personalize.hs
+++ lib/services/amazonka-personalize/test/Test/Amazonka/Gen/Personalize.hs
@@ -7,7 +7,7 @@
 -- Module      : Test.Amazonka.Gen.Personalize
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Test.Amazonka.Gen.Personalize where

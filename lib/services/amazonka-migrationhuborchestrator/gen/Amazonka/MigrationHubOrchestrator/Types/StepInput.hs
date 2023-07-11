907e99bb669cfcad7de62d6313d8efec10ea90fa
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:40 +1200

    amazonka-migrationhuborchestrator: regenerating service

--- lib/services/amazonka-migrationhuborchestrator/gen/Amazonka/MigrationHubOrchestrator/Types/StepInput.hs
+++ lib/services/amazonka-migrationhuborchestrator/gen/Amazonka/MigrationHubOrchestrator/Types/StepInput.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.MigrationHubOrchestrator.Types.StepInput
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.MigrationHubOrchestrator.Types.StepInput where

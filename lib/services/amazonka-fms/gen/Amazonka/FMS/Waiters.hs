8efae0891e67a31de3b6c7e9ba2c60b54792e13d
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:23 +1200

    amazonka-fms: regenerating service

--- lib/services/amazonka-fms/gen/Amazonka/FMS/Waiters.hs
+++ lib/services/amazonka-fms/gen/Amazonka/FMS/Waiters.hs
@@ -11,7 +11,7 @@
 -- Module      : Amazonka.FMS.Waiters
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.FMS.Waiters where

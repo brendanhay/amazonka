02e186745d40f9c5aaca106023b805e01791f3bf
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:48 +1200

    amazonka-rds-data: regenerating service

--- lib/services/amazonka-rds-data/gen/Amazonka/RDSData/Types/ResultSetOptions.hs
+++ lib/services/amazonka-rds-data/gen/Amazonka/RDSData/Types/ResultSetOptions.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.RDSData.Types.ResultSetOptions
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.RDSData.Types.ResultSetOptions where

5006fddee9d38bfbbfb77e5a3e67fd41b436260a
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:23 +1200

    amazonka-fis: regenerating service

--- lib/services/amazonka-fis/gen/Amazonka/FIS/Types/ExperimentActionState.hs
+++ lib/services/amazonka-fis/gen/Amazonka/FIS/Types/ExperimentActionState.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.FIS.Types.ExperimentActionState
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.FIS.Types.ExperimentActionState where

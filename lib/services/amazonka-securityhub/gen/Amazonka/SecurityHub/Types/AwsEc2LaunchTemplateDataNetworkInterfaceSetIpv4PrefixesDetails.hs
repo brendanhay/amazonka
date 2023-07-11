f29e072581629ddce9c5c022c01014b5bafe5861
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:53 +1200

    amazonka-securityhub: regenerating service

--- lib/services/amazonka-securityhub/gen/Amazonka/SecurityHub/Types/AwsEc2LaunchTemplateDataNetworkInterfaceSetIpv4PrefixesDetails.hs
+++ lib/services/amazonka-securityhub/gen/Amazonka/SecurityHub/Types/AwsEc2LaunchTemplateDataNetworkInterfaceSetIpv4PrefixesDetails.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.SecurityHub.Types.AwsEc2LaunchTemplateDataNetworkInterfaceSetIpv4PrefixesDetails
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.SecurityHub.Types.AwsEc2LaunchTemplateDataNetworkInterfaceSetIpv4PrefixesDetails where

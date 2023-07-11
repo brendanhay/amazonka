f6e40f05815b81f7944de46f951a70519a6e9d84
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:36 +1200

    amazonka-lightsail: regenerating service

--- lib/services/amazonka-lightsail/gen/Amazonka/Lightsail/Types/AccountLevelBpaSync.hs
+++ lib/services/amazonka-lightsail/gen/Amazonka/Lightsail/Types/AccountLevelBpaSync.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.Lightsail.Types.AccountLevelBpaSync
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.Lightsail.Types.AccountLevelBpaSync where

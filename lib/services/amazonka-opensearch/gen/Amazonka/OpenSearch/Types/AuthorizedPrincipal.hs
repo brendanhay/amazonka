ed69324b2547c73f0368707404f4ef1f48661ade
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:43 +1200

    amazonka-opensearch: regenerating service

--- lib/services/amazonka-opensearch/gen/Amazonka/OpenSearch/Types/AuthorizedPrincipal.hs
+++ lib/services/amazonka-opensearch/gen/Amazonka/OpenSearch/Types/AuthorizedPrincipal.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.OpenSearch.Types.AuthorizedPrincipal
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.OpenSearch.Types.AuthorizedPrincipal where

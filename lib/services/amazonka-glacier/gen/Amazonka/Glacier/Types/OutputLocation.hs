459220547aa06bce149c331cecd2712eecd1fd57
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:25 +1200

    amazonka-glacier: regenerating service

--- lib/services/amazonka-glacier/gen/Amazonka/Glacier/Types/OutputLocation.hs
+++ lib/services/amazonka-glacier/gen/Amazonka/Glacier/Types/OutputLocation.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.Glacier.Types.OutputLocation
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.Glacier.Types.OutputLocation where

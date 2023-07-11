f09c665f06650e1371c034439f620896ab96c5c9
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:31 +1200

    amazonka-iottwinmaker: regenerating service

--- lib/services/amazonka-iottwinmaker/gen/Amazonka/IotTwinMaker/Types/PropertyFilter.hs
+++ lib/services/amazonka-iottwinmaker/gen/Amazonka/IotTwinMaker/Types/PropertyFilter.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.IotTwinMaker.Types.PropertyFilter
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.IotTwinMaker.Types.PropertyFilter where

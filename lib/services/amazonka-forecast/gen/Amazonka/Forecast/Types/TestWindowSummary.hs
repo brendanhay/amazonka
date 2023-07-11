353bc21d9119d1b282f1b6e499a8c49dbec94fa4
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:23 +1200

    amazonka-forecast: regenerating service

--- lib/services/amazonka-forecast/gen/Amazonka/Forecast/Types/TestWindowSummary.hs
+++ lib/services/amazonka-forecast/gen/Amazonka/Forecast/Types/TestWindowSummary.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.Forecast.Types.TestWindowSummary
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.Forecast.Types.TestWindowSummary where

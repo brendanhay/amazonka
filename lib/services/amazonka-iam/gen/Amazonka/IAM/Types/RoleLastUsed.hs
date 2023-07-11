391eac8de265c455488d117afaa13f7977b7956b
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:27 +1200

    amazonka-iam: regenerating service

--- lib/services/amazonka-iam/gen/Amazonka/IAM/Types/RoleLastUsed.hs
+++ lib/services/amazonka-iam/gen/Amazonka/IAM/Types/RoleLastUsed.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.IAM.Types.RoleLastUsed
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.IAM.Types.RoleLastUsed where

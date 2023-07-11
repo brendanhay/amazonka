52a4eb60d57bf343b8fe899c44e5dfd41707954e
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:19 +1200

    amazonka-ec2: regenerating service

--- lib/services/amazonka-ec2/gen/Amazonka/EC2/ModifyIdFormat.hs
+++ lib/services/amazonka-ec2/gen/Amazonka/EC2/ModifyIdFormat.hs
@@ -16,7 +16,7 @@
 -- Module      : Amazonka.EC2.ModifyIdFormat
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 --

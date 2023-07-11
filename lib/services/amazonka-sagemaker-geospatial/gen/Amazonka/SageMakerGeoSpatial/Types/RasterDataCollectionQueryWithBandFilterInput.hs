7e3e70090384c13dca68a57fa1c1434a19871d0b
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:52 +1200

    amazonka-sagemaker-geospatial: regenerating service

--- lib/services/amazonka-sagemaker-geospatial/gen/Amazonka/SageMakerGeoSpatial/Types/RasterDataCollectionQueryWithBandFilterInput.hs
+++ lib/services/amazonka-sagemaker-geospatial/gen/Amazonka/SageMakerGeoSpatial/Types/RasterDataCollectionQueryWithBandFilterInput.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.SageMakerGeoSpatial.Types.RasterDataCollectionQueryWithBandFilterInput
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.SageMakerGeoSpatial.Types.RasterDataCollectionQueryWithBandFilterInput where

521dcbfaa864619930d7cb8aaa58538d8d86b50a
Brendan Hay <os@brendanhay.nz>
2023-07-12 10:08:37 +1200

    amazonka-maciev2: regenerating service

--- lib/services/amazonka-maciev2/gen/Amazonka/MacieV2/Types/IpCity.hs
+++ lib/services/amazonka-maciev2/gen/Amazonka/MacieV2/Types/IpCity.hs
@@ -14,7 +14,7 @@
 -- Module      : Amazonka.MacieV2.Types.IpCity
 -- Copyright   : (c) 2013-2023 Brendan Hay
 -- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Brendan Hay <brendan.g.hay+amazonka@gmail.com>
+-- Maintainer  : Brendan Hay
 -- Stability   : auto-generated
 -- Portability : non-portable (GHC extensions)
 module Amazonka.MacieV2.Types.IpCity where

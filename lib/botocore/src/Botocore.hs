f39b6adc9c1568e3c8f2a6982bec83b4976bb437
Jack Kelly <jack@jackkelly.name>
2024-01-06 12:20:22 +1000

    botocore: remove empty top-level module

--- lib/botocore/src/Botocore.hs
+++
@@ -1,8 +0,0 @@
--- |
--- Module      : Botocore
--- Copyright   : (c) 2023 Bellroy Pty Ltd
--- License     : Mozilla Public License, v. 2.0.
--- Maintainer  : Jack Kelly <jack@jackkelly.name>
--- Stability   : provisional
--- Portability : non-portable (GHC extensions)
-module Botocore where
